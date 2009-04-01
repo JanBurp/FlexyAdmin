c92edd75a94934826276a73f6084694af849aa44
Jan den Besten <info@jandenbesten.net>
2009-04-01 10:30:30 +0000

    - content class (p and img tags gets classes now)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@198 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/libraries/content.php
+++ sys/flexyadmin/libraries/content.php
@@ -16,21 +16,42 @@
 class Content {
 
 	var $safeEmail;
-	var $table;
-	var $fields;
+	var $addClasses;
 
 	function Content() {
 		$this->option_safe_email();
+		$this->add_classes();
 	}
 
 	function option_safe_email($safe=TRUE) {
 		$this->safeEmail=$safe;
 	}
+	
+	function add_classes($classes=TRUE) {
+		$this->addClasses=$classes;
+	}
 
-/**
- *TODO: !content rendering (p,img classes etc)
- */
 	function render($txt) {
+		
+		if ($this->addClasses) {
+			// callback for class replacing
+			function countCallBack($matches) {
+				static $img_count=1;
+				static $p_count=1;
+				if ($matches[1]=="p") {
+					$class="p$p_count";
+					if ($p_count++%2) $class.=" odd"; else $class.=" even";
+				}
+				else {
+					$class="img$img_count";
+					if ($img_count++%2) $class.=" odd"; else $class.=" even";
+				}
+				return "<".$matches[1]." class=\"$class ".$matches[4]."\" ".$matches[2]." ".$matches[5].">";
+			}
+		 	// add classes (odd even nrs to p and img tags)
+			$txt=preg_replace_callback("/<(img|p)(.*?)(class=\"(.*?)\")?(.*?)>/","countCallBack",$txt);
+		}
+		
 		if ($this->safeEmail) {
 			if (preg_match_all("/<a[\s]*href=\"mailto:(.*)\">(.*)<\/a>/",$txt,$matches)) { 	//<a[\s]*href="(.*)">(.*)</a>
 				$search=array();
@@ -44,8 +65,14 @@ class Content {
 				$txt=str_replace($search,$replace,$txt);
 			}
 		}
+		
 		return $txt;
 	}
+	
+	
+	
+	
+	
 }
 
 ?>
