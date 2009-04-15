21a2acba1320467693be42738083767e86b4980d
Jan den Besten <info@jandenbesten.net>
2009-04-15 14:08:12 +0000

    - language selection - language setting per user
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@212 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/language/nederlands/update_delete_lang.php
+++ sys/flexyadmin/language/nederlands/update_delete_lang.php
@@ -6,7 +6,7 @@ $lang['update_succes']		= "Het item van %s is aangepast.";
 $lang['update_error']			= "Fout tijdens toevoegen/aanpassen van '%0s' (%1s).";
 $lang['update_user_changed']	="Jouw instellingen zijn aangepast.";
 
-$lang['delete_confirm']		= "Weet u zeker dat u de volgende gegevens van %s wilt verwijderen?";
+$lang['delete_confirm']		= "Weet u zeker dat u de volgende gegevens van '%s' wilt verwijderen?";
 $lang['delete_data']			= "<p class=\"title\">Samenvatting:</p><p class=\"data\">%s</p>";
 $lang['delete_join']			= "<p class=\"title\">Er zijn ook verwijzingen naar:</p><p class=\"data\">%s</p>";
 $lang['delete_succes']		= "Het item van %s is verwijderd.";
