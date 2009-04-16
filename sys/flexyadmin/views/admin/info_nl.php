1634fbdd4f1200e67c9b33551001ce2823dd46b5
Jan den Besten <info@jandenbesten.net>
2009-04-16 14:38:00 +0000

    - info and (c) added
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@223 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ sys/flexyadmin/views/admin/info_nl.php
@@ -0,0 +1,16 @@
+<h1>FlexyAdmin 2009</h1>
+<p>Een flexibel content management systeem.</p>
+
+<h2>Door</h2>
+<p><a href="http://www.jandenbesten.net" target="_blank">Jan den Besten</a></p>
+
+<h2>Met</h2>
+<p><a href="http://codeigniter.com/" target="_blank">Code Igniter</a><br /><a href="http://tinymce.moxiecode.com/" target="_blank">TinyMCE</a><br /><a href="http://jquery.com/" target="_blank">jQuery</a></p>
+
+<h2>Met dank aan</h2>
+<p><a href="http://www.arnoudvandenheuvel.nl/" target="_blank">Arnoud van den Heuvel</a> voor de flexibele blokjes.<br /><a href="http://www.nervo.nl/" target="_blank">Emanuel Nervo</a> e.a. voor tips.</p>
+
+<p>&nbsp;</p>
+<p><a href="http://www.flexyadmin.com" target="_blank">FlexyAdmin 2009 r<?=$revision?></a></p>
+
+<h1>&nbsp;</h1>
