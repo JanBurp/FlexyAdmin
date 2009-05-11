0c80c20959a4fe72e47ee59d7939693bb15754f1
Jan den Besten <info@jandenbesten.net>
2009-05-11 10:02:15 +0000

    - removing sys from tinymce (error)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@263 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/assets/js/tinymce/sys/flexyadmin/language/nl/rights_lang.php
+++
@@ -1,6 +0,0 @@
-<?
-
-$lang['ritghs_no_rights'] 			= "Sorry, je hebt geen rechten om dit te doen.";
-
-
-?>
--- sys/flexyadmin/assets/js/tinymce/sys/flexyadmin/views/admin/content.php
+++
@@ -1,5 +0,0 @@
-<div id="content" class="<?=$show_type?>">
-
-<? if (isset($content)) echo $content; ?>
-
-</div>
