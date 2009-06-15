56d4ffd4691690ee42b3189d97a0e03879048cb2
Jan den Besten <info@jandenbesten.net>
2009-06-15 13:41:31 +0000

    - from validation messages
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@345 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/language/nl/form_validation_lang.php
+++ sys/flexyadmin/language/nl/form_validation_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['required']         = "Het veld %s is verplicht.";
-$lang['isset']            = "Het veld %s moet een waarde hebben.";
-$lang['valid_email']    	= "Het veld %s moet een geldig emailadres bevatten.";
-$lang['valid_emails'] 		= "Het veld %s moet geldige emaialadressen bevatten.";
-$lang['valid_url']        = "Het veld %s moet een geldige URL bevatten.";
-$lang['valid_ip'] 				= "Het veld %s moet een geldig IP adres bevatten.";
-$lang['min_length']       = "Het veld %s moet tenminste %s karakters lang zijn.";
-$lang['max_length']       = "Het veld %s mag niet langer zijn dan %s karakters.";
-$lang['exact_length']    	= "Het veld %s moet exact %s karakters zijn.";
-$lang['alpha']            = "Het veld %s mag alleen letters bevatten.";
-$lang['alpha_numeric']    = "Het veld %s mag alleen letters en cijfers bevatten.";
-$lang['alpha_dash']       = "Het veld %s mag alleen letters, cijfers en streepjes ( _ of - )bevatten.";
-$lang['numeric']        	= "Het veld %s mag alleen cijfers bevatten.";
+$lang['required']         = "'%s' is verplicht.";
+$lang['isset']            = "'%s' moet een waarde hebben.";
+$lang['valid_email']    	= "'%s' moet een geldig emailadres bevatten.";
+$lang['valid_emails'] 		= "'%s' moet geldige emaialadressen bevatten.";
+$lang['valid_url']        = "'%s' moet een geldige URL bevatten.";
+$lang['valid_ip'] 				= "'%s' moet een geldig IP adres bevatten.";
+$lang['min_length']       = "'%s' moet tenminste %s karakters lang zijn.";
+$lang['max_length']       = "'%s' mag niet langer zijn dan %s karakters.";
+$lang['exact_length']    	= "'%s' moet exact %s karakters zijn.";
+$lang['alpha']            = "'%s' mag alleen letters bevatten.";
+$lang['alpha_numeric']    = "'%s' mag alleen letters en cijfers bevatten.";
+$lang['alpha_dash']       = "'%s' mag alleen letters, cijfers en streepjes ( _ of - )bevatten.";
+$lang['numeric']        	= "'%s' mag alleen cijfers bevatten.";
 $lang['is_numeric']				= $lang["numeric"];
-$lang['integer']					= "Het veld %s mag alleen gehele getallen bevatten.";
-$lang['matches']        	= "Het veld %s is niet gelijk aan het veld %s.";
-$lang['is_natural']					= "Het veld %s mag alleen positieve getallen bevatten.";
-$lang['is_natural_no_zero']	= "Het veld %s mag alleen getallen bevatten groter dan nul.";
+$lang['integer']					= "'%s' mag alleen gehele getallen bevatten.";
+$lang['matches']        	= "'%s' is niet gelijk aan het veld %s.";
+$lang['is_natural']					= "'%s' mag alleen positieve getallen bevatten.";
+$lang['is_natural_no_zero']	= "'%s' mag alleen getallen bevatten groter dan nul.";
 
 $lang['valid_rgb']	= "Geen juiste kleurcode in '%s'";
 
