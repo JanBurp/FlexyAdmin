046a7bd434bd9138fe5525eccbe0451b47a33db6
Jan den Besten <info@jandenbesten.net>
2009-06-19 12:12:22 +0000

    - nice html helper (echo_if_set)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@365 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/helpers/MY_html_helper.php
+++ sys/flexyadmin/helpers/MY_html_helper.php
@@ -1,5 +1,13 @@
 <?
 
+function echo_if_set(&$var,$a=array(),$tag="p") {
+	return if_set(&$var,$a=array(),$tag="p");
+}
+function if_set(&$var,$a=array(),$tag="p") {
+	if (isset($var)) return html($tag,$a).$var._html($tag);
+ return "";
+}
+
 function html($tag,$a=array(),$end=FALSE) {
 	if (!is_array($a)) $a=array("class"=>$a);
 	$attr="";
