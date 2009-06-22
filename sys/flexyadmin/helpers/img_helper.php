ad6517556fb51bee140db9fe162eeedee5028723
Jan den Besten <info@jandenbesten.net>
2009-06-22 13:05:23 +0000

    - img_helper (check exist before get size)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@367 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/helpers/img_helper.php
+++ sys/flexyadmin/helpers/img_helper.php
@@ -5,8 +5,11 @@ function icon($class="",$s="",$extraClass="",$a="") {
 }
 
 function popup_img($img) {
-	$imgSize=getimagesize($this->map."/".$name);
-	$a=array("src"=>$img, "class"=>"zoom", "zwidth"=>$imgSize[0], "zheight"=>$imgSize[1] );
+	$imgSize=get_img_size($this->map."/".$name);
+	if ($imgSize)
+		$a=array("src"=>$img, "class"=>"zoom", "zwidth"=>$imgSize[0], "zheight"=>$imgSize[1] );
+	else
+		$a=NULL;
 	return img($a);
 }
 
@@ -14,22 +17,30 @@ function show_thumb($attr) {
 	$a=array();
 	if (!is_array($attr)) $a["src"]=$attr; else $a=$attr;
 	$ext=get_file_extension($a["src"]);
-	$imgSize=getimagesize($a["src"]);
-	$a["zwidth"]=$imgSize[0];
-	$a["zheight"]=$imgSize[1];
-	if (!isset($a["title"])) $a["title"]=$a["src"];
-	if (!isset($a["class"])) $a["class"]="zoom"; else $a["class"].=" zoom";
-	if ($ext=="swf") {
-		$src=$a["src"];
-		unset($a["src"]);
-		return flash($src,$a);
+	$imgSize=get_img_size($a["src"]);
+	if ($imgSize) {
+		$a["zwidth"]=$imgSize[0];
+		$a["zheight"]=$imgSize[1];
+		if (!isset($a["title"])) $a["title"]=$a["src"];
+		if (!isset($a["class"])) $a["class"]="zoom"; else $a["class"].=" zoom";
+		if ($ext=="swf") {
+			$src=$a["src"];
+			unset($a["src"]);
+			return flash($src,$a);
+		}
+		else {
+			if (!isset($a["alt"])) $a["alt"]=$a["src"];
+			$CI=& get_instance();
+			$cachedThumb=$CI->config->item('THUMBCACHE').pathencode($a['src']);
+			if (file_exists($cachedThumb)) $a['src']=$cachedThumb;
+			return img($a);
+		}
 	}
-	else {
-		if (!isset($a["alt"])) $a["alt"]=$a["src"];
-		$CI=& get_instance();
-		$cachedThumb=$CI->config->item('THUMBCACHE').pathencode($a['src']);
-		if (file_exists($cachedThumb)) $a['src']=$cachedThumb;
-		return img($a);
-	}
+	return '';
 }
 
+function get_img_size($i) {
+	$size=FALSE;
+	if (file_exists($i)) $size=getimagesize($i);
+	return $size;
+}
