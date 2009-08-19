37bc03878ded12b84ad6d4067943a06d7e52b304
Jan den Besten <info@jandenbesten.net>
2009-08-19 14:53:11 +0000

    - MY upload, upload & resizing are two methods now
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@431 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/libraries/MY_Upload.php
+++ sys/flexyadmin/libraries/MY_Upload.php
@@ -6,11 +6,13 @@ class MY_Upload extends CI_Upload {
 	var $error;
 	var $result;
 	var $file_name;
+	var $CI;
 
 	function MY_Upload($config=NULL)
 	{
 		parent::CI_Upload($config);
 		$this->config($config);
+		$this->CI=NULL;
 	}
 
 	function config($config) {
@@ -32,8 +34,6 @@ class MY_Upload extends CI_Upload {
 		return $this->file_name;
 	}
 
-
-
 	function _setMemory($imageInfo) {
 		// trace_($imageInfo);
 		if (isset($imageInfo['channels']) and isset($imageInfo['bits']) ) {
@@ -52,17 +52,9 @@ class MY_Upload extends CI_Upload {
 			else
 				return false;
 		}
-		// ini_set('memory_limit', '128M');
 		return true;
 	}
 
-/**
- * function upload_file()
- *
- * Uploads a file and if a image resize if asked for
- *
- * @return result
- */
 	function upload_file($file="userfile") {
 		$config=$this->config;
 		// trace_($config);
@@ -85,103 +77,106 @@ class MY_Upload extends CI_Upload {
 			}
 			else {
 				log_("info","[UPLOAD] uploaded: '$this->file_name'");
-				/**
-				 * Is image and need to be resized/copied?
-				 */
-				$CI =& get_instance();
-				$CI->load->library('image_lib');
-				$uPath=str_replace($CI->config->item('ASSETS'),"",$config["upload_path"]);
-				$cfg=$CI->cfg->get('CFG_img_info',$uPath);
-				// first resize copies
-				$currentSizes=getimagesize($config["upload_path"]."/".$this->file_name);
-				// trace_($currentSizes);
-				$memSet=FALSE;
-				$nr=1;
-				while (isset($cfg["b_create_$nr"])) {
-					if ($cfg["b_create_$nr"]!=FALSE) {
-						// check if resize is not bigger than original (that would be strange)
-						if ($currentSizes[0]<$cfg["int_width_$nr"] and $currentSizes[1]<$cfg["int_height_$nr"] ) {
-							$cfg["int_width_$nr"]=$currentSizes[0];
-							$cfg["int_height_$nr"]=$currentSizes[1];
-						}
-						$pre=$cfg["str_prefix_$nr"];
-						$post=$cfg["str_postfix_$nr"];
-						$ext=get_file_extension($this->file_name);
-						$name=str_replace(".$ext","",$this->file_name);
-						$copyName=$pre.$name.$post.".".$ext;
-						$configResize['source_image'] 	= $config["upload_path"]."/".$this->file_name;
-						$configResize['maintain_ratio'] = TRUE;
-						$configResize['width'] 					= $cfg["int_width_$nr"];
-						$configResize['height'] 				= $cfg["int_height_$nr"];
-						$configResize['new_image']			= $config["upload_path"]."/".$copyName;
-						$configResize['master_dim']			= 'auto';
-						// set mem higher if needed
-						$this->_setMemory($currentSizes);
-						// trace_($configResize);
-						$CI->image_lib->initialize($configResize);
-						if (!$CI->image_lib->resize()) {
-							$this->error=$CI->image_lib->display_errors();
-							// trace_($this->error);
-							$goodluck=FALSE;
-						}
-						$CI->image_lib->clear();
-						// trace_('Resized nr_'.$nr.' '.$configResize['new_image']);
-					}
-					$nr++;
-				}
-				// resize original
-				if ($cfg["b_resize_img"]!=FALSE) {
-					// check if resize is necessary
-					if ($currentSizes[0]>$cfg["int_img_width"] or $currentSizes[1]>$cfg["int_img_height"] ) {
-						$configResize['source_image'] 	= $config["upload_path"]."/".$this->file_name;
-						$configResize['maintain_ratio'] = TRUE;
-						$configResize['width'] 					= $cfg["int_img_width"];
-						$configResize['height'] 				= $cfg["int_img_height"];
-						$configResize['new_image']			= "";
-						$configResize['master_dim']			= 'auto';
-						// set mem higher if needed
-						$this->_setMemory($currentSizes);
-						$CI->image_lib->initialize($configResize);
-						if (!$CI->image_lib->resize()) {
-							$this->error=$CI->image_lib->display_errors();
-							// trace_($this->error);
-							$goodluck=FALSE;
-						}
-						$CI->image_lib->clear();
-						// trace_('Resized original');
-					}
-				}
-				// create cached thumb for flexyadmin if cache map exists
-				if (file_exists($CI->config->item('THUMBCACHE')) ) {
-					$thumbSize=$CI->config->item('THUMBSIZE');
-					if ($currentSizes[0]>$thumbSize[0] or $currentSizes[1]>$thumbSize[1]) { 
-						$configResize['source_image'] 	= $config["upload_path"]."/".$this->file_name;
-						$configResize['maintain_ratio'] = TRUE;
-						$configResize['width'] 					= $thumbSize[0];
-						$configResize['height'] 				= $thumbSize[1];
-						$configResize['new_image']			= $CI->config->item('THUMBCACHE').pathencode($config["upload_path"]."/".$this->file_name,FALSE);
-						$configResize['master_dim']			= 'auto';
-						// set mem higher if needed
-						$this->_setMemory($currentSizes);
-						// trace_($configResize);
-						$CI->image_lib->initialize($configResize);
-						if (!$CI->image_lib->resize()) {
-							$this->error=$CI->image_lib->display_errors();
-							// trace_($this->error);
-							$goodluck=FALSE;
-						}
-						$CI->image_lib->clear();
-						// trace_('Resized thumb in cache '.$configResize['new_image']);
-					}
-				}
 			}
-			// trace_($goodluck);
-		}
-		else {
-			die($this->error);
 		}
 		return $goodluck;
 	}
+	
+	function resize_image($image,$path) {
+		$goodluck=TRUE;
+		if (!isset($this->CI)) {
+			$this->CI =& get_instance();
+			$this->CI->load->library('image_lib');
+		}
+		$CI=$this->CI;
+		
+		$uPath=str_replace($CI->config->item('ASSETS'),"",$path);
+		$cfg=$CI->cfg->get('CFG_img_info',$uPath);
+		$currentSizes=getimagesize($path."/".$this->file_name);
+
+		// first resize copies
+		$nr=1;
+		while (isset($cfg["b_create_$nr"])) {
+			if ($cfg["b_create_$nr"]!=FALSE) {
+				// check if resize is not bigger than original (that would be strange)
+				if ($currentSizes[0]<$cfg["int_width_$nr"] and $currentSizes[1]<$cfg["int_height_$nr"] ) {
+					$cfg["int_width_$nr"]=$currentSizes[0];
+					$cfg["int_height_$nr"]=$currentSizes[1];
+				}
+				$pre=$cfg["str_prefix_$nr"];
+				$post=$cfg["str_postfix_$nr"];
+				$ext=get_file_extension($this->file_name);
+				$name=str_replace(".$ext","",$this->file_name);
+				$copyName=$pre.$name.$post.".".$ext;
+				$configResize['source_image'] 	= $path."/".$this->file_name;
+				$configResize['maintain_ratio'] = TRUE;
+				$configResize['width'] 					= $cfg["int_width_$nr"];
+				$configResize['height'] 				= $cfg["int_height_$nr"];
+				$configResize['new_image']			= $path."/".$copyName;
+				$configResize['master_dim']			= 'auto';
+				$this->_setMemory($currentSizes);
+				// trace_($configResize);
+				$CI->image_lib->initialize($configResize);
+				if (!$CI->image_lib->resize()) {
+					$this->error=$CI->image_lib->display_errors();
+					// trace_($this->error);
+					$goodluck=FALSE;
+				}
+				$CI->image_lib->clear();
+				// trace_('Resized nr_'.$nr.' '.$configResize['new_image']);
+			}
+			$nr++;
+		}
+
+		// resize original
+		if ($cfg["b_resize_img"]!=FALSE) {
+			// check if resize is necessary
+			if ($currentSizes[0]>$cfg["int_img_width"] or $currentSizes[1]>$cfg["int_img_height"] ) {
+				$configResize['source_image'] 	= $path."/".$this->file_name;
+				$configResize['maintain_ratio'] = TRUE;
+				$configResize['width'] 					= $cfg["int_img_width"];
+				$configResize['height'] 				= $cfg["int_img_height"];
+				$configResize['new_image']			= "";
+				$configResize['master_dim']			= 'auto';
+				// set mem higher if needed
+				$this->_setMemory($currentSizes);
+				$CI->image_lib->initialize($configResize);
+				if (!$CI->image_lib->resize()) {
+					$this->error=$CI->image_lib->display_errors();
+					// trace_($this->error);
+					$goodluck=FALSE;
+				}
+				$CI->image_lib->clear();
+				// trace_('Resized original');
+			}
+		}
+
+		// create cached thumb for flexyadmin if cache map exists
+		if (file_exists($CI->config->item('THUMBCACHE')) ) {
+			$thumbSize=$CI->config->item('THUMBSIZE');
+			if ($currentSizes[0]>$thumbSize[0] or $currentSizes[1]>$thumbSize[1]) { 
+				$configResize['source_image'] 	= $path."/".$this->file_name;
+				$configResize['maintain_ratio'] = TRUE;
+				$configResize['width'] 					= $thumbSize[0];
+				$configResize['height'] 				= $thumbSize[1];
+				$configResize['new_image']			= $CI->config->item('THUMBCACHE').pathencode($path."/".$this->file_name,FALSE);
+				$configResize['master_dim']			= 'auto';
+				// set mem higher if needed
+				$this->_setMemory($currentSizes);
+				// trace_($configResize);
+				$CI->image_lib->initialize($configResize);
+				if (!$CI->image_lib->resize()) {
+					$this->error=$CI->image_lib->display_errors();
+					// trace_($this->error);
+					$goodluck=FALSE;
+				}
+				$CI->image_lib->clear();
+				// trace_('Resized thumb in cache '.$configResize['new_image']);
+			}
+		}
+		return $goodluck;
+	}
+
 
 }
 ?>
