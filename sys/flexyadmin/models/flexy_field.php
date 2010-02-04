d6df090eebbc2bdcff371b170f02d3fdaa2bdfed
Jan den Besten <info@jandenbesten.net>
2010-02-04 11:23:34 +0000

    - now also media/medias dragndrop can be as wide as the editor
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@626 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/models/flexy_field.php
+++ sys/flexyadmin/models/flexy_field.php
@@ -340,11 +340,13 @@ class Flexy_field extends Model {
 			//$out['max_height'] = '768';
 		}
 		/**
-		 * HTML editor field
+		 * Wide?
 		 */
-		if ($this->type=="htmleditor") {
+		if ($this->type=="htmleditor" or $this->type=="function_dropdown_media" or $this->type=='function_dropdown_medias') {
 			$out['class'] = $this->cfg->get('CFG_editor','str_class');
-		}		/**
+		}
+		
+		/**
 		 * Add validation rules:
 		 * -first the standard validation rules set in flexyadmin_config.php.
 		 * -then add validation rules set in cfg_field_info
