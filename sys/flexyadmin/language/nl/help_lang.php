cae8f6e453ccaa6bfafeef48d0a75c90a89061ef
Jan den Besten <info@jandenbesten.net>
2010-02-04 01:34:08 +0000

    - small bug in str helper: safe_string (removes . now)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@625 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/language/nl/help_lang.php
+++ sys/flexyadmin/language/nl/help_lang.php
@@ -5,7 +5,7 @@ $lang['file']								= "Bestand";
 $lang['help_no_help']				= 'Sorry, er is geen help gevonden. Neem contact op met je webbouwer.';
 
 $lang['grid_new'] 					= '<p>Voeg een nieuw item toe aan %s.</p>';
-$lang['grid_filter'] 				= '<p>Snelzoeken: type hier je zoekterm</p>';
+$lang['grid_filter'] 				= '<p>Snelzoeken - type hier je zoekterm</p>';
 $lang['grid_edit']					= '<p>Pas dit item aan</p>';
 $lang['grid_delete']				= '<p>Verwijder dit item</p>';
 $lang['grid_select']				= '<p>Selecteer dit item</p>';
