2f472ff2fb285aa1e803742dafa8be5ad8c9a426
Jan den Besten <info@jandenbesten.net>
2010-07-02 13:52:10 +0000

    - Cleanup
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@723 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/views/links.php
+++ site/views/links.php
@@ -1,5 +1,3 @@
-<div class="<?=$view?>">
-	<? foreach($links as $link): ?>
-		<p class="link"><a href="<?=$link["url_url"]?>" target="_blank"><?=$link["str_title"]?></a></p>
-	<? endforeach; ?>
-</div>
+<? foreach($links as $link): ?>
+	<p class="link"><a href="<?=$link["url_url"]?>" target="_blank"><?=$link["str_title"]?></a></p>
+<? endforeach; ?>
