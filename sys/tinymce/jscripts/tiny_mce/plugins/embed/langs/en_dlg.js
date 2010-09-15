7f83dcd772a03330bcd7cbc22a7ed7f0a111e2bb
Jan den Besten <info@jandenbesten.net>
2010-09-15 09:52:33 +0000

    remove tinyMCE
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@778 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/tinymce/jscripts/tiny_mce/plugins/embed/langs/en_dlg.js
+++
@@ -1,6 +0,0 @@
-tinyMCE.addI18n('en.embed_dlg',{
-	title : 'Insert Embed Code',
-	tabone : 'Insert Embed Code',
-	embedlist: 'Embed list',
-	embedcode : 'Paste embed code here <br />(youTube, Picasa, Flickr, GoogleMaps, etc):'
-});
