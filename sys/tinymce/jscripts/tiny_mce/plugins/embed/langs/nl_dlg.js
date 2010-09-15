d0d3106b35e5960cc37e6351c6fc0e0cd8e77df9
Jan den Besten <info@jandenbesten.net>
2010-09-15 09:52:33 +0000

    remove tinyMCE
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@778 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/tinymce/jscripts/tiny_mce/plugins/embed/langs/nl_dlg.js
+++
@@ -1,6 +0,0 @@
-tinyMCE.addI18n('nl.embed_dlg',{
-	title : 'Sluit Code In (Embedden)',
-	tabone : 'Sluit Code In (Embedden)',
-	embedlist: 'Embed lijst',
-	embedcode : 'Plak de code hier in: <br />(youTube, Picasa, Flickr, GoogleMaps, etc):'
-});
