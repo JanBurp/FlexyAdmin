f37732ad9f516e20d94624afbbfbf017bd7e6147
Jan den Besten <info@jandenbesten.net>
2011-01-25 11:18:16 +0000

    - flexy forms -  more robust (no errors if tables doesn't exists)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@957 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/controllers/admin/MY_Controller.php
+++ sys/flexyadmin/controllers/admin/MY_Controller.php
@@ -251,53 +251,56 @@ class FrontEndController extends MY_Controller {
 	}
 	
 	function _get_form() {
-		$form=array();
-		$form['form']=$this->db->get_row('tbl_forms');
-		if ($form['form']) {
-			$this->db->where('id_form',$form['form']['id']);
-			$fields=$this->db->get_result('tbl_formfields');
-			if ($fields) {
-				$options=false;
-				$optionsKey='';
-				foreach ($fields as $key => $value) {
-					unset($value['id']);
-					unset($value['id_form']);
-					foreach ($value as $k => $v) {
-						$value[remove_prefix($k)]=$v;
-						unset($value[$k]);
-					}
-					$name=$value['label'].'_'.$key;
-					$value['name']=$name;
-					$value['validation']=add_validation_parameters($value['validation'],$value['validation_parameters']);
-					unset($value['validation_parameters']);
-					if ($this->input->post($name)) $value['value']=$this->input->post($name);
-					$fields[$key]=$value;
-					
-					// End options setting
-					if (is_array($options) and $value['type']!='option') {
-						$fields[$optionsKey]['options']=$options;
-						$options=FALSE;
-						$optionsKey='';
-					}
-					// Start option settings
-					if ($value['type']=='radio' or $value['type']=='select') {
-						$optionsKey=$name;
-						$options=array();
-					}
-					// add option
-					if ($value['type']=='option' and is_array($options)) {
-						$options[safe_string($value['html'],50)]=$value['html'];
-						unset($fields[$key]);
-					}
+		$form=false;
+		if ($this->db->table_exists('tbl_forms') and $this->db->table_exists('tbl_formfields')) {
+			$form=array();
+			$form['form']=$this->db->get_row('tbl_forms');
+			if ($form['form']) {
+				$this->db->where('id_form',$form['form']['id']);
+				$fields=$this->db->get_result('tbl_formfields');
+				if ($fields) {
+					$options=false;
+					$optionsKey='';
+					foreach ($fields as $key => $value) {
+						unset($value['id']);
+						unset($value['id_form']);
+						foreach ($value as $k => $v) {
+							$value[remove_prefix($k)]=$v;
+							unset($value[$k]);
+						}
+						$name=$value['label'].'_'.$key;
+						$value['name']=$name;
+						$value['validation']=add_validation_parameters($value['validation'],$value['validation_parameters']);
+						unset($value['validation_parameters']);
+						if ($this->input->post($name)) $value['value']=$this->input->post($name);
+						$fields[$key]=$value;
 
-					if (isset($fields[$key])) {
-						$fields[$name]=$fields[$key];
-						unset($fields[$key]);
+						// End options setting
+						if (is_array($options) and $value['type']!='option') {
+							$fields[$optionsKey]['options']=$options;
+							$options=FALSE;
+							$optionsKey='';
+						}
+						// Start option settings
+						if ($value['type']=='radio' or $value['type']=='select') {
+							$optionsKey=$name;
+							$options=array();
+						}
+						// add option
+						if ($value['type']=='option' and is_array($options)) {
+							$options[safe_string($value['html'],50)]=$value['html'];
+							unset($fields[$key]);
+						}
+
+						if (isset($fields[$key])) {
+							$fields[$name]=$fields[$key];
+							unset($fields[$key]);
+						}
 					}
 				}
+				$form['fields']=$fields;
+				$form['buttons']=array('submit'=>array("submit"=>"submit","value"=>'submit'));
 			}
-			$form['fields']=$fields;
-			$form['buttons']=array('submit'=>array("submit"=>"submit","value"=>'submit'));
 		}
 		return $form;
 	}
