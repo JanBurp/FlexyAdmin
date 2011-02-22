79edc47230a571808f5a4afd8767ed348fc14aba
Jan den Besten <info@jandenbesten.net>
2011-02-22 10:13:17 +0000

    - sitemap xml declaration bug
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@977 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/plugins/plugin_sitemap_pi.php
+++ sys/flexyadmin/plugins/plugin_sitemap_pi.php
@@ -47,7 +47,7 @@ class plugin_sitemap extends plugin_ {
 		$sitemap['urlset']=$urlset;
 		
 		// create XML and save it
-		$XML=array2XML($sitemap,array('urlset','url'),array('urlset'=>array('xmlns'=>"http://www.sitemaps.org/schemas/sitemap/0.9")));
+		$XML=array2XML($sitemap,array('urlset','url'),array('urlset'=>array('xmlns'=>"http://www.sitemaps.org/schemas/sitemap/0.9", 'xmlns:xsi'=>"http://www.w3.org/2001/XMLSchema-instance", 'xsi:schemaLocation'=>"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" )));
 		$err=write_file('sitemap.xml', $XML);
 		
 		if ($err) {
