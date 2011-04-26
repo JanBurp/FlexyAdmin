77411c90dbd0d8da2d5ea9eab8f24457e15e7c3b
Jan den Besten <info@jandenbesten.net>
2011-04-26 13:59:27 +0000

    - update to tinyMCE 3.9.4
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1044 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ sys/tinymce/jscripts/tiny_mce/plugins/embed/langs/nl_dlg.js
@@ -0,0 +1,6 @@
+tinyMCE.addI18n('nl.embed_dlg',{
+	title : 'Sluit Code In (Embedden)',
+	tabone : 'Sluit Code In (Embedden)',
+	embedlist: 'Embed lijst',
+	embedcode : 'Plak de code hier in: <br />(youTube, Picasa, Flickr, GoogleMaps, etc):'
+});
