775a601a8635ecb568c405feab2913ff620af547
Jan den Besten <info@jandenbesten.net>
2011-07-23 10:22:13 +0000

    - update to tinyMCE 3.4.3.2 (jQuery) - some styling bug of tinyMCE - advanced tabs of tablecontrols can be set in admin.css
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1111 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/tinymce/jscripts/tiny_mce/themes/advanced/js/charmap.js
+++ sys/tinymce/jscripts/tiny_mce/themes/advanced/js/charmap.js
@@ -77,28 +77,28 @@ var charmap = [
 	['&sum;',     '&#8721;', true, 'n-ary sumation'],
 	['&infin;',   '&#8734;', true, 'infinity'],
 	['&radic;',   '&#8730;', true, 'square root'],
-	['&sim;',     '&#8764;', true,'similar to'],
-	['&cong;',    '&#8773;', true,'approximately equal to'],
+	['&sim;',     '&#8764;', false,'similar to'],
+	['&cong;',    '&#8773;', false,'approximately equal to'],
 	['&asymp;',   '&#8776;', true, 'almost equal to'],
 	['&ne;',      '&#8800;', true, 'not equal to'],
 	['&equiv;',   '&#8801;', true, 'identical to'],
-	['&isin;',    '&#8712;', true,'element of'],
-	['&notin;',   '&#8713;', true,'not an element of'],
-	['&ni;',      '&#8715;', true,'contains as member'],
+	['&isin;',    '&#8712;', false,'element of'],
+	['&notin;',   '&#8713;', false,'not an element of'],
+	['&ni;',      '&#8715;', false,'contains as member'],
 	['&prod;',    '&#8719;', true, 'n-ary product'],
-	['&and;',     '&#8743;', true,'logical and'],
-	['&or;',      '&#8744;', true,'logical or'],
+	['&and;',     '&#8743;', false,'logical and'],
+	['&or;',      '&#8744;', false,'logical or'],
 	['&not;',     '&#172;',  true, 'not sign'],
 	['&cap;',     '&#8745;', true, 'intersection'],
-	['&cup;',     '&#8746;', true,'union'],
+	['&cup;',     '&#8746;', false,'union'],
 	['&part;',    '&#8706;', true, 'partial differential'],
-	['&forall;',  '&#8704;', true,'for all'],
-	['&exist;',   '&#8707;', true,'there exists'],
-	['&empty;',   '&#8709;', true,'diameter'],
-	['&nabla;',   '&#8711;', true,'backward difference'],
-	['&lowast;',  '&#8727;', true,'asterisk operator'],
-	['&prop;',    '&#8733;', true,'proportional to'],
-	['&ang;',     '&#8736;', true,'angle'],
+	['&forall;',  '&#8704;', false,'for all'],
+	['&exist;',   '&#8707;', false,'there exists'],
+	['&empty;',   '&#8709;', false,'diameter'],
+	['&nabla;',   '&#8711;', false,'backward difference'],
+	['&lowast;',  '&#8727;', false,'asterisk operator'],
+	['&prop;',    '&#8733;', false,'proportional to'],
+	['&ang;',     '&#8736;', false,'angle'],
 // undefined
 	['&acute;',   '&#180;',  true, 'acute accent'],
 	['&cedil;',   '&#184;',  true, 'cedilla'],
@@ -119,7 +119,6 @@ var charmap = [
 	['&Eacute;',  '&#201;',  true, 'E - acute'],
 	['&Ecirc;',   '&#202;',  true, 'E - circumflex'],
 	['&Euml;',    '&#203;',  true, 'E - diaeresis'],
-	['&#286;',		'&#286;',	 true, 'G - breve'],
 	['&Igrave;',  '&#204;',  true, 'I - grave'],
 	['&Iacute;',  '&#205;',  true, 'I - acute'],
 	['&Icirc;',   '&#206;',  true, 'I - circumflex'],
@@ -134,7 +133,6 @@ var charmap = [
 	['&Oslash;',  '&#216;',  true, 'O - slash'],
 	['&OElig;',   '&#338;',  true, 'ligature OE'],
 	['&Scaron;',  '&#352;',  true, 'S - caron'],
-	['&#350;',		'&#350;',	 true, 'S - cedilla'],
 	['&Ugrave;',  '&#217;',  true, 'U - grave'],
 	['&Uacute;',  '&#218;',  true, 'U - acute'],
 	['&Ucirc;',   '&#219;',  true, 'U - circumflex'],
@@ -154,7 +152,6 @@ var charmap = [
 	['&eacute;',  '&#233;',  true, 'e - acute'],
 	['&ecirc;',   '&#234;',  true, 'e - circumflex'],
 	['&euml;',    '&#235;',  true, 'e - diaeresis'],
-	['&#287;',		'&#287;',	 true, 'g - breve'],
 	['&igrave;',  '&#236;',  true, 'i - grave'],
 	['&iacute;',  '&#237;',  true, 'i - acute'],
 	['&icirc;',   '&#238;',  true, 'i - circumflex'],
@@ -169,7 +166,6 @@ var charmap = [
 	['&oslash;',  '&#248;',  true, 'o slash'],
 	['&oelig;',   '&#339;',  true, 'ligature oe'],
 	['&scaron;',  '&#353;',  true, 's - caron'],
-	['&#351;',		'&#351;',	 true, 's - cedilla'],
 	['&ugrave;',  '&#249;',  true, 'u - grave'],
 	['&uacute;',  '&#250;',  true, 'u - acute'],
 	['&ucirc;',   '&#251;',  true, 'u - circumflex'],
@@ -177,7 +173,7 @@ var charmap = [
 	['&yacute;',  '&#253;',  true, 'y - acute'],
 	['&thorn;',   '&#254;',  true, 'thorn'],
 	['&yuml;',    '&#255;',  true, 'y - diaeresis'],
-  ['&Alpha;',   '&#913;',  true, 'Alpha'],
+	['&Alpha;',   '&#913;',  true, 'Alpha'],
 	['&Beta;',    '&#914;',  true, 'Beta'],
 	['&Gamma;',   '&#915;',  true, 'Gamma'],
 	['&Delta;',   '&#916;',  true, 'Delta'],
@@ -227,43 +223,43 @@ var charmap = [
 	['&psi;',     '&#968;',  true, 'psi'],
 	['&omega;',   '&#969;',  true, 'omega'],
 // symbols
-	['&alefsym;', '&#8501;', true,'alef symbol'],
-	['&piv;',     '&#982;',  true,'pi symbol'],
-	['&real;',    '&#8476;', true,'real part symbol'],
-	['&thetasym;','&#977;',  true,'theta symbol'],
-	['&upsih;',   '&#978;',  true,'upsilon - hook symbol'],
-	['&weierp;',  '&#8472;', true,'Weierstrass p'],
-	['&image;',   '&#8465;', true,'imaginary part'],
+	['&alefsym;', '&#8501;', false,'alef symbol'],
+	['&piv;',     '&#982;',  false,'pi symbol'],
+	['&real;',    '&#8476;', false,'real part symbol'],
+	['&thetasym;','&#977;',  false,'theta symbol'],
+	['&upsih;',   '&#978;',  false,'upsilon - hook symbol'],
+	['&weierp;',  '&#8472;', false,'Weierstrass p'],
+	['&image;',   '&#8465;', false,'imaginary part'],
 // arrows
 	['&larr;',    '&#8592;', true, 'leftwards arrow'],
 	['&uarr;',    '&#8593;', true, 'upwards arrow'],
 	['&rarr;',    '&#8594;', true, 'rightwards arrow'],
 	['&darr;',    '&#8595;', true, 'downwards arrow'],
 	['&harr;',    '&#8596;', true, 'left right arrow'],
-	['&crarr;',   '&#8629;', true,'carriage return'],
-	['&lArr;',    '&#8656;', true,'leftwards double arrow'],
-	['&uArr;',    '&#8657;', true,'upwards double arrow'],
-	['&rArr;',    '&#8658;', true,'rightwards double arrow'],
-	['&dArr;',    '&#8659;', true,'downwards double arrow'],
-	['&hArr;',    '&#8660;', true,'left right double arrow'],
-	['&there4;',  '&#8756;', true,'therefore'],
-	['&sub;',     '&#8834;', true,'subset of'],
-	['&sup;',     '&#8835;', true,'superset of'],
-	['&nsub;',    '&#8836;', true,'not a subset of'],
-	['&sube;',    '&#8838;', true,'subset of or equal to'],
-	['&supe;',    '&#8839;', true,'superset of or equal to'],
-	['&oplus;',   '&#8853;', true,'circled plus'],
-	['&otimes;',  '&#8855;', true,'circled times'],
-	['&perp;',    '&#8869;', true,'perpendicular'],
-	['&sdot;',    '&#8901;', true,'dot operator'],
-	['&lceil;',   '&#8968;', true,'left ceiling'],
-	['&rceil;',   '&#8969;', true,'right ceiling'],
-	['&lfloor;',  '&#8970;', true,'left floor'],
-	['&rfloor;',  '&#8971;', true,'right floor'],
-	['&lang;',    '&#9001;', true,'left-pointing angle bracket'],
-	['&rang;',    '&#9002;', true,'right-pointing angle bracket'],
-	['&loz;',     '&#9674;', true,'lozenge'],
-	['&spades;',  '&#9824;', true,'black spade suit'],
+	['&crarr;',   '&#8629;', false,'carriage return'],
+	['&lArr;',    '&#8656;', false,'leftwards double arrow'],
+	['&uArr;',    '&#8657;', false,'upwards double arrow'],
+	['&rArr;',    '&#8658;', false,'rightwards double arrow'],
+	['&dArr;',    '&#8659;', false,'downwards double arrow'],
+	['&hArr;',    '&#8660;', false,'left right double arrow'],
+	['&there4;',  '&#8756;', false,'therefore'],
+	['&sub;',     '&#8834;', false,'subset of'],
+	['&sup;',     '&#8835;', false,'superset of'],
+	['&nsub;',    '&#8836;', false,'not a subset of'],
+	['&sube;',    '&#8838;', false,'subset of or equal to'],
+	['&supe;',    '&#8839;', false,'superset of or equal to'],
+	['&oplus;',   '&#8853;', false,'circled plus'],
+	['&otimes;',  '&#8855;', false,'circled times'],
+	['&perp;',    '&#8869;', false,'perpendicular'],
+	['&sdot;',    '&#8901;', false,'dot operator'],
+	['&lceil;',   '&#8968;', false,'left ceiling'],
+	['&rceil;',   '&#8969;', false,'right ceiling'],
+	['&lfloor;',  '&#8970;', false,'left floor'],
+	['&rfloor;',  '&#8971;', false,'right floor'],
+	['&lang;',    '&#9001;', false,'left-pointing angle bracket'],
+	['&rang;',    '&#9002;', false,'right-pointing angle bracket'],
+	['&loz;',     '&#9674;', true, 'lozenge'],
+	['&spades;',  '&#9824;', true, 'black spade suit'],
 	['&clubs;',   '&#9827;', true, 'black club suit'],
 	['&hearts;',  '&#9829;', true, 'black heart suit'],
 	['&diams;',   '&#9830;', true, 'black diamond suit'],
@@ -279,19 +275,38 @@ var charmap = [
 
 tinyMCEPopup.onInit.add(function() {
 	tinyMCEPopup.dom.setHTML('charmapView', renderCharMapHTML());
+	addKeyboardNavigation();
 });
+
+function addKeyboardNavigation(){
+	var tableElm, cells, settings;
+
+	cells = tinyMCEPopup.dom.select(".charmaplink", "charmapgroup");
+
+	settings ={
+		root: "charmapgroup",
+		items: cells
+	};
+
+	tinyMCEPopup.editor.windowManager.createInstance('tinymce.ui.KeyboardNavigation', settings, tinyMCEPopup.dom);
+}
 
 function renderCharMapHTML() {
 	var charsPerRow = 20, tdWidth=20, tdHeight=20, i;
-	var html = '<table border="0" cellspacing="1" cellpadding="0" width="' + (tdWidth*charsPerRow) + '"><tr height="' + tdHeight + '">';
+	var html = '<div id="charmapgroup" aria-labelledby="charmap_label" tabindex="0" role="listbox">'+
+	'<table role="presentation" border="0" cellspacing="1" cellpadding="0" width="' + (tdWidth*charsPerRow) + 
+	'"><tr height="' + tdHeight + '">';
 	var cols=-1;
 
 	for (i=0; i<charmap.length; i++) {
+		var previewCharFn;
+
 		if (charmap[i][2]==true) {
 			cols++;
+			previewCharFn = 'previewChar(\'' + charmap[i][1].substring(1,charmap[i][1].length) + '\',\'' + charmap[i][0].substring(1,charmap[i][0].length) + '\',\'' + charmap[i][3] + '\');';
 			html += ''
 				+ '<td class="charmap">'
-				+ '<a onmouseover="previewChar(\'' + charmap[i][1].substring(1,charmap[i][1].length) + '\',\'' + charmap[i][0].substring(1,charmap[i][0].length) + '\',\'' + charmap[i][3] + '\');" onfocus="previewChar(\'' + charmap[i][1].substring(1,charmap[i][1].length) + '\',\'' + charmap[i][0].substring(1,charmap[i][0].length) + '\',\'' + charmap[i][3] + '\');" href="javascript:void(0)" onclick="insertChar(\'' + charmap[i][1].substring(2,charmap[i][1].length-1) + '\');" onclick="return false;" onmousedown="return false;" title="' + charmap[i][3] + '">'
+				+ '<a class="charmaplink" role="button" onmouseover="'+previewCharFn+'" onfocus="'+previewCharFn+'" href="javascript:void(0)" onclick="insertChar(\'' + charmap[i][1].substring(2,charmap[i][1].length-1) + '\');" onclick="return false;" onmousedown="return false;" title="' + charmap[i][3] + '">'
 				+ charmap[i][1]
 				+ '</a></td>';
 			if ((cols+1) % charsPerRow == 0)
@@ -305,7 +320,8 @@ function renderCharMapHTML() {
 			html += '<td width="' + tdWidth + '" height="' + tdHeight + '" class="charmap">&nbsp;</td>';
 	}
 
-	html += '</tr></table>';
+	html += '</tr></table></div>';
+	html = html.replace(/<tr height="20"><\/tr>/g, '');
 
 	return html;
 }
--- sys/tinymce/jscripts/tiny_mce/themes/advanced/langs/en.js
+++ sys/tinymce/jscripts/tiny_mce/themes/advanced/langs/en.js
@@ -45,7 +45,6 @@ custom1_desc:"Your custom description here",
 forecolor_desc:"Select text color",
 backcolor_desc:"Select background color",
 charmap_desc:"Insert custom character",
-charmap_delta_height:60,
 visualaid_desc:"Toggle guidelines/invisible elements",
 anchor_desc:"Insert/edit anchor",
 cut_desc:"Cut",
@@ -59,5 +58,11 @@ clipboard_msg:"Copy/Cut/Paste is not available in Mozilla and Firefox.\r\nDo you
 path:"Path",
 newdocument:"Are you sure you want clear all contents?",
 toolbar_focus:"Jump to tool buttons - Alt+Q, Jump to editor - Alt-Z, Jump to element path - Alt-X",
-more_colors:"More colors"
+more_colors:"More colors",
+
+// Accessibility Strings
+shortcuts_desc:'Accessibility Help',
+help_shortcut:'. Press ALT F10 for toolbar. Press ALT 0 for help.',
+rich_text_area:"Rich Text Area",
+toolbar:"Toolbar"
 });
