12ca1b4553b462119cf4fbab49b64b7c0ad3836a
Jan den Besten <info@jandenbesten.net>
2011-09-19 17:48:54 +0000

    - add search module with views & config - add jquery.clearInput.js
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1268 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ site/views/search_results.php
@@ -0,0 +1,12 @@
+<div id="results">
+<h2>zoekresultaat van: '<?=$search?>'</h2>
+<? if ($items): ?>
+	<ul>
+	<? foreach($items as $item): ?>
+		<li class="result"><a href="<?=$item["uri"]?>"><?=$item["str_title"]?></a><br/><i><?=$item['txt_text']?></i></li>
+	<? endforeach; ?>
+	</ul>
+<? else: ?>
+<p>Geen resultaten gevonden voor '<?=$search?>'</p>
+<? endif; ?>
+</div>
