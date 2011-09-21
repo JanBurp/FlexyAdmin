d876f2c1c8b2383f75f7c8bb3cbe40d559a6389d
Jan den Besten <info@jandenbesten.net>
2011-09-21 10:23:01 +0000

    - plugin: add_relation_table checks now if argumenst are present
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1276 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/plugins/plugin_add_relation_table.php
+++ sys/flexyadmin/plugins/plugin_add_relation_table.php
@@ -15,13 +15,16 @@ class Plugin_add_relation_table extends Plugin_ {
 				if (isset($args[0])) $table1=$args[0];
 				if (isset($args[1])) $table2=$args[1];
 				if (isset($table1) and isset($table2)) $goodArgs=true;
-				$relTable='rel_'.remove_prefix($table1).'__'.remove_prefix($table2);
-				$this->dbforge->add_field('id');
-				$fields=array(	'id_'.remove_prefix($table1)	=>	array('type'=>'INT','unsigned'=>TRUE),
-												'id_'.remove_prefix($table2)	=>	array('type'=>'INT','unsigned'=>TRUE));
-				$this->dbforge->add_field($fields);
-				$this->dbforge->create_table($relTable,TRUE);
-				$this->_add_content(h("Created '$relTable' from '$table1' and '$table2'.",2));
+				if ($goodArgs) {
+					$relTable='rel_'.remove_prefix($table1).'__'.remove_prefix($table2);
+					if ($table1==$table2) $table2=$table2.'_';
+					$this->dbforge->add_field('id');
+					$fields=array(	'id_'.remove_prefix($table1)	=>	array('type'=>'INT','unsigned'=>TRUE),
+													'id_'.remove_prefix($table2)	=>	array('type'=>'INT','unsigned'=>TRUE));
+					$this->dbforge->add_field($fields);
+					$this->dbforge->create_table($relTable,TRUE);
+					$this->_add_content(h("Created '$relTable' from '$table1' and '$table2'.",2));
+				}
 			}
 			if (!$goodArgs) {
 				$this->_add_content('<p>Add relation table, for which table(s)?</br></br>Give: /tbl_xxx/tbl_xxx</p>');
