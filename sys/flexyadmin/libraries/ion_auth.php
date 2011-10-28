ed25bb5fa50d95916c30e7481799e023fb36d193
Jan den Besten <info@jandenbesten.net>
2011-10-28 10:53:04 +0000

    - bug in ion_auto_model (FlexyAdmin db names
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1312 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/libraries/ion_auth.php
+++ sys/flexyadmin/libraries/ion_auth.php
@@ -234,14 +234,14 @@ class Ion_auth
 	{
 		$identity = $this->ci->config->item('identity', 'ion_auth');
 		$profile  = $this->ci->ion_auth_model->profile($code, true); //pass the code to profile
-
+		
 		if (!is_object($profile))
 		{
 			$this->set_error('password_change_unsuccessful');
 			return FALSE;
 		}
 
-		$new_password = $this->ci->ion_auth_model->forgotten_password_complete($code, $profile->salt);
+		$new_password = $this->ci->ion_auth_model->forgotten_password_complete($code, $profile->str_salt);
 
 		if ($new_password)
 		{
@@ -257,7 +257,7 @@ class Ion_auth
 			$this->ci->email->initialize($config);
 			$this->ci->email->set_newline("\r\n");
 			$this->ci->email->from($this->ci->config->item('admin_email', 'ion_auth'), $this->ci->config->item('site_title', 'ion_auth'));
-			$this->ci->email->to($profile->email);
+			$this->ci->email->to($profile->email_email);
 			$this->ci->email->subject($this->ci->config->item('site_title', 'ion_auth') . ' - New Password');
 			$this->ci->email->message($message);
 
