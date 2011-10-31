a870c177395eeb6472a92061b6ab0ccb6f3daaa7
Jan den Besten <info@jandenbesten.net>
2011-10-31 14:01:29 +0000

    - Loading config files: now load them all and override site configs - Plugins now can load config - Plugin Automenu had now a config
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1315 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ sys/flexyadmin/config/plugin_automenu.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Plugin automenu config
+|--------------------------------------------------------------------------
+|
+*/
+
+$config['module_field']='str_module';
+
+
+?>
--- sys/flexyadmin/core/MY_Config.php
+++ sys/flexyadmin/core/MY_Config.php
@@ -27,6 +27,89 @@ Class MY_Config extends CI_Config {
 	}
 
 
+
+
+	/**
+	 * Load Config File in all paths and override them
+	 */
+	function load($file = '', $use_sections = FALSE, $fail_gracefully = FALSE)	{
+		
+		$file = ($file == '') ? 'config' : str_replace('.php', '', $file);
+		$loaded = FALSE;
+
+		// First select paths's
+		$check_locations = defined('ENVIRONMENT')	? array(ENVIRONMENT.'/'.$file, $file)	: array($file);
+		$locations=array();
+		foreach ($this->_config_paths as $path) {
+			if (defined('ENVIRONMENT')) $locations[].=$path.'config/'.ENVIRONMENT;
+			$locations[]=$path.'config';
+		}
+
+		// Load from all locations, and load and override config
+		foreach ($locations as $location) {
+
+			$file_path = $location.'/'.$file.'.php';
+
+			// echo "$file_path<br/>";
+
+			// Allready loaded?
+			if ( ! in_array($file_path, $this->is_loaded, TRUE))	{
+
+				// Exists?
+				if (file_exists($file_path)) {
+
+					// echo "FOUND: $file_path<br/>";
+
+					// Load
+					include($file_path);
+
+					// Add to config
+					if ( isset($config) AND is_array($config))	{
+
+						if ($use_sections === TRUE)	{
+							if (isset($this->config[$file])) {
+								$this->config[$file] = array_merge($this->config[$file], $config);
+							}
+							else {
+								$this->config[$file] = $config;
+							}
+						}
+						else {
+							$this->config = array_merge($this->config, $config);
+						}
+
+					}
+					else {
+						// show_error('Your '.$file_path.' file does not appear to contain a valid configuration array.');
+					}
+
+					$this->is_loaded[] = $file_path;
+					unset($config);
+					$loaded=TRUE;
+
+					// echo "LOADED: $file_path<br/>";
+
+					log_message('debug', 'Config file loaded: '.$file_path);
+				}
+
+			}
+		}
+
+		if ($loaded === FALSE) {
+			// File not found
+			if ($fail_gracefully === TRUE) {
+				return FALSE;
+			}
+			// show_error('The configuration file '.$file_path.' does not exist.');
+		}
+
+		return $loaded;
+	}
+
+
+
+
+
 // --------------------------------------------------------------------
 
 }
