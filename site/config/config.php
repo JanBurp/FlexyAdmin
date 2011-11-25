d6fae407f149653061a2afbf184684c138c45298
Jan den Besten <info@jandenbesten.net>
2011-11-25 13:30:42 +0000

    - added autoload_modules_if to controller
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1349 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/config/config.php
+++ site/config/config.php
@@ -73,10 +73,13 @@ $config['auto_pagination']	= FALSE;
 | Autoload Modules
 |--------------------------------------------------------------------------
 |
-| You can autoload modules from the controller. They are also called.
-| Usefull if you need to do something on every page and don't won't to change the controlle.
+| You can autoload and call modules from the controller.
+| Usefull if you need to do something on every page and don't won't to change the controller.
+| You can also autoload a module with a simple test on a field in $item
 */
-// $config['autoload_modules']=array('submenu');
+// $config['autoload_modules'] = array('submenu','comments');
+// $config['autoload_modules_if'] = array( 'comments'=>array('b_allow_comments'=>true) );
+
 
 /*
 |--------------------------------------------------------------------------
