f11e891ec7916eb82851efcb3fa31d36b3f07695
Jan den Besten <info@jandenbesten.net>
2011-11-30 11:28:02 +0000

    - adding auth library
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1365 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/views/account/en/forgot_password.tpl.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-	<h1>Reset Password for <?=$user?></h1>
-	<p>Please click this link to <?php echo anchor('auth/reset_password?code='. $forgotten_password_code, 'Reset Your Password');?>.</p>
-</body>
-</html>
