7035b7a93cebad361897902e7b08447d1f7167bb
Jan den Besten <info@jandenbesten.net>
2011-11-30 11:28:02 +0000

    - adding auth library
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1365 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/views/account/nl/activate.tpl.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-	<h1>Activeer account voor <?php echo $identity;?></h1>
-	<p>Klik op deze link <?php echo anchor( site_url().'nl/activate?id='.$id.'&activation='.$activation,  'Activate Your Account');?>.</p>
-</body>
-</html>
--- site/views/account/nl/forgot_password.tpl.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-	<h1>Wachtwoord is gereset voor <?=$user?></h1>
-	<p>Klik hier om <?php echo anchor('nl/reset_password?code='. $forgotten_password_code, 'wachtwoord te resetten');?>.</p>
-</body>
-</html>
