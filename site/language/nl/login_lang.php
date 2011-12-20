ea70472aed0b2f2bce504068ee3a115352a2c5e4
Jan den Besten <info@jandenbesten.net>
2011-12-20 11:07:44 +0000

    - changed login module: now an option is added that admin needs to activate - more views for login module - plugin_login_activate: admin can deny/accept users with this
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1397 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/language/nl/login_lang.php
+++ site/language/nl/login_lang.php
@@ -4,12 +4,13 @@
 $lang['username']								= 'Naam';
 $lang['username_used']					= 'Deze naam wordt al gebruikt.';
 $lang['password']								= 'Wachtwoord';
-$lang['password2']							= 'Wachtwoord, nogmaals ter controle.';
+$lang['password2']							= 'Wachtwoord 2x';
 $lang['password_mismatch']			= 'Het wachtwoord is anders dan het controle-wachtwoord. Vul 2 keer hetzelfde in.';
 $lang['email']									= 'Email adres';
 $lang['email_used']							= 'Dit email adres is al in gebruik.';
 $lang['register_submit']				= 'Aanmelden!';
 $lang['register_completed']			= 'Je krijgt nu een email waarmee je je account kunt activeren.';
+$lang['register_wait']					= 'Je aanvraag wordt beoordeeld. Je krijgt daar binnenkort per email uitsluitsel over.';
 $lang['register_succes']				= 'Je aanmelding is gelukt. Je kunt nu <a href="%s">inloggen</a>.';
 $lang['register_fail']					= 'Je aanmelding is mislukt.';
 $lang['register_mail_subject']	= 'Aanmelding.';
@@ -18,7 +19,7 @@ $lang['register_mail_subject']	= 'Aanmelding.';
 // LOGIN
 $lang['forgot_password']				= 'Wachtwoord vergeten.';
 $lang['register']								= 'Maak account aan.';
-$lang['remember']								= 'Onthou mij.';
+$lang['remember']								= 'Laat me ingelogd';
 $lang['login_submit']						= 'Inloggen';
 $lang['login_error']						= 'Verkeerde gebruikersnaam/wachtwoord combinatie.';
 $lang['login_already']					= 'Je bent (al) ingelogd, %s.';
@@ -35,5 +36,17 @@ $lang['forgot_password_mail_subject']	= 'Vergeten wachtwoord reset.';
 $lang['reset_password_succes'] 				= 'Het resseten van je wachtwoord is gelukt.<br/>Per mail heb je een nieuw wachtwoord ontvangen.<br/><br/><a href="%s">Login</a>';
 $lang['reset_password_error'] 				= 'Het resseten van je wachtwoord is mislukt of is al eerder gebeurd.';
 $lang['reset_password_mail_subject']	= 'Nieuw wachtwoord.';
+
+// ADMIN ACTIVATION
+$lang['activate_users']								= 'Activeer gebruikers';
+$lang['show_inactive_users']					= 'Inactieve gebruikers';
+$lang['no_inactive_users']						= 'Er zijn geen in-actieve gebruikers op dit moment.';
+$lang['deny']													= 'Weiger deze gebruiker';
+$lang['accept']												= 'Accepteer deze gebruiker';
+$lang['user_removed']									= 'Gebruiker %s verwijderd en een afwijzings email gestuurd.';
+$lang['user_accepted']								= 'Gebruiker %s is geactiveerd en heeft een accepterings email gekregen.';
+$lang['mail_denied_subject']					= 'Account aanvraag afgewezen.';
+$lang['mail_accepted_subject']				= 'Account aanvraag geaccepteerd.';
+
 
 ?>
