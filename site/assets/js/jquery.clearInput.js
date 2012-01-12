3f63b542615986efdfc967c6172ff3dd79500043
Jan den Besten <info@jandenbesten.net>
2012-01-12 13:27:19 +0000

    - changed jquery.clearInput.js
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1422 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/assets/js/jquery.clearInput.js
+++ site/assets/js/jquery.clearInput.js
@@ -6,13 +6,17 @@
 
 (function($) {
 	$.fn.clearInput = function(opts) {
-		return this.each(function() {
+    // remove buttons
+    var obj=$(this).not('.button');
+    // traverse
+		return obj.each(function() {
       // init
       $(this).addClass('clearInput');
       if ($(this).attr('value')==$(this).attr('empty_value') || $(this).attr('value')=='') {
 				$(this).val($(this).attr('empty_value'));
         $(this).addClass('empty_value');
       }
+      // focus events
 			$(this).focus(function(){
 				$(this).val('');
 			}).focusout(function(){
@@ -24,7 +28,7 @@
           $(this).removeClass('empty_value');
         }
 			});
-      // Make sure all values are cleaned if they are at start value after submit
+      // Make sure all values are cleaned after submit
       $(this).parents('form').submit(function(){
         $(this).find('.clearInput').each(function(){
           if ($(this).attr('value')==$(this).attr('empty_value')) $(this).val('');
