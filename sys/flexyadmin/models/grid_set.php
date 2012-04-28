33fe3d0af7661d58e6f9b65c83e26e22f068502c
Jan den Besten <info@jandenbesten.net>
2012-04-28 09:06:36 +0000

    - New Model grid_set that handles the order,search and paging parameters of a grid or filemananager
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1499 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ sys/flexyadmin/models/grid_set.php
@@ -0,0 +1,41 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Handles the state a grid is in: order,search and page
+
+
+class Grid_set extends CI_Model {
+
+  var $api='API_view_grid';
+
+  public function set_api($api='API_view_grid') {
+    $this->api=$api;
+  }
+
+	public function save($set=array()) {
+		$default=array('table'=>'','offset'=>'','order'=>'','search'=>'');
+		$set=array_merge($default,$set);
+		$this->session->set_userdata('grid_set',$set);
+	}
+	
+	public function open() {
+		$set=$this->session->userdata('grid_set');
+		return $set;
+	}
+	
+	public function open_uri($table='') {
+		$set=$this->open();
+		if (empty($table)) $table=$set['table'];
+		$uri=api_uri($this->api,$table);
+		unset($set['table']);
+		foreach ($set as $key => $value) {
+			if (!empty($value)) $uri.="/$key/$value";
+		}
+		return $uri;
+	}
+	
+	public function reset() {
+		$this->session->unset_userdata('grid_set');
+	}
+
+
+}
