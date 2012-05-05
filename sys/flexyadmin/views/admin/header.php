9cb64eff57722176a8610650635bda3fecf35ebe
Jan den Besten <info@jandenbesten.net>
2012-05-05 13:36:39 +0000

    - update tinyMCE 3.5
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1508 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/views/admin/header.php
+++ sys/flexyadmin/views/admin/header.php
@@ -51,7 +51,7 @@ $isForm=has_string('form',$show_type);
 	</script>
 
 	<!-- jQuery -->
-	<script language="javascript" type="text/javascript" src="sys/jquery/jquery-1.7.1.min.js"></script>
+	<script language="javascript" type="text/javascript" src="sys/jquery/jquery-1.7.2.min.js"></script>
 	<!-- jQuery UI -->
 	<link rel="stylesheet" type="text/css" href="sys/jquery/ui/custom-theme/jquery-ui-1.8.7.custom<?=$css?>" />
 	<script language="javascript" type="text/javascript" src="sys/jquery/ui/jquery-ui-1.8.18.custom.min.js"></script>
@@ -90,7 +90,8 @@ $isForm=has_string('form',$show_type);
 						language : "<?=$language?>",
 						docs_language : "<?=$language?>",
 						theme : "advanced",
-						skin : "flexyadmin",
+            skin : "flexyadmin",
+            // skin : "default",
 						<?
 						switch ($editor_class) {
 							case 'wide':
