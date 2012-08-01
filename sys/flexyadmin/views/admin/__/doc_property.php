9746109396523a6c61459cf796ac2d53813f8cef
Jan den Besten <info@jandenbesten.net>
2012-08-01 08:17:16 +0000

    - documentation search finetuning - fold in/out of methods, fuctions and properties
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1654 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/views/admin/__/doc_property.php
+++ sys/flexyadmin/views/admin/__/doc_property.php
@@ -1,9 +1,11 @@
 <div class="<?=(isset($inherited))?'inherited':''?>">
-  <h3 class="doc_property"><? if (!empty($type[0])): ?>(<?=$type[0]?>)<? endif ?> <?=$name?></h3>
-  <? if (isset($inherited)): ?>
-  <p class="doc_info doc_inherited">inherited from: <a href="<?=$inherited?>.html"><?=$inherited?></a></p>
-  <? endif ?>
-  <p class="doc_description"><?=$shortdescription?></p>
-  <p class="doc_description"><?=$description?></p>
-  <br />
+  <h2 class="doc_property"><? if (!empty($type[0])): ?>(<?=$type[0]?>)<? endif ?> <?=$name?></h2>
+  <div>
+    <? if (isset($inherited)): ?>
+    <p class="doc_info doc_inherited">inherited from: <a href="<?=$inherited?>.html"><?=$inherited?></a></p>
+    <? endif ?>
+    <? if (!empty($shortdescription)): ?><p class="doc_description"><?=$shortdescription?></p><? endif ?>
+    <? if (!empty($description)): ?><p class="doc_description"><?=$description?></p><? endif ?>
+    <br />
+  </div>
 </div>
