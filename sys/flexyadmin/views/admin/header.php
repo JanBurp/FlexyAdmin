f976a17f8cf77fe1a1545b1d49b1b93145d5da34
Jan den Besten <info@jandenbesten.net>
2012-08-02 14:00:37 +0000

    - finetuning autodocumentation - updated jQuery UI 1.8.22
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1659 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/views/admin/header.php
+++ sys/flexyadmin/views/admin/header.php
@@ -54,7 +54,7 @@ $isForm=has_string('form',$show_type);
 	<script language="javascript" type="text/javascript" src="sys/jquery/jquery-1.7.2.min.js"></script>
 	<!-- jQuery UI -->
 	<link rel="stylesheet" type="text/css" href="sys/jquery/ui/custom-theme/jquery-ui-1.8.7.custom<?=$css?>" />
-	<script language="javascript" type="text/javascript" src="sys/jquery/ui/jquery-ui-1.8.20.custom.min.js"></script>
+	<script language="javascript" type="text/javascript" src="sys/jquery/ui/jquery-ui-1.8.22.custom.min.js"></script>
 	<!-- jQuery plugins-->
 	<link rel="stylesheet" type="text/css" href="sys/jquery/plugins/fullsize/fullsize.css" />
 	<script language="javascript" type="text/javascript" src="sys/jquery/plugins/fullsize/jquery.fullsize<?=$js?>"></script>
