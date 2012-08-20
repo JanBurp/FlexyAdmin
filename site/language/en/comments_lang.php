9c394e9d3bb0566ae3195558662357a7bd82bfda
Jan den Besten <info@jandenbesten.net>
2012-08-20 13:05:50 +0000

    - changed module comments: * cleanup unneaded methods * changed language files by remover prefix 'comments_'
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1709 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/language/en/comments_lang.php
+++ site/language/en/comments_lang.php
@@ -3,11 +3,12 @@
 $lang['comments_title']='Comments';
 $lang['comments_submit']='Place comments';
 $lang['comments_spam']='<p class="error">This comment is recognised as spam.</p>';
+
 // for each field you can give an alternative label text:
-$lang['comments_str_name']='Naam';
-$lang['comments_email_email']='Email';
-$lang['comments_str_title']='Titel';
-$lang['comments_txt_text']='Comment';
+$lang['str_name']='Naam';
+$lang['email_email']='Email';
+$lang['str_title']='Titel';
+$lang['txt_text']='Comment';
 
 $lang['comments_mail_to_owner_subject']='New comment on site: %s.';
 $lang['comments_mail_to_owner_body']='New comment on page %s.';
