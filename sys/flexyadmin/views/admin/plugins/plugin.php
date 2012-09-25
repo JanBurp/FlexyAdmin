21ff0f0a175fe992d5a335d855555d5315943142
Jan den Besten <info@jandenbesten.net>
2012-09-25 08:59:28 +0000

    - bug in plugin_uri (not viewing content) - updated standard plugin view to handle messages and content
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1775 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/views/admin/plugins/plugin.php
+++ sys/flexyadmin/views/admin/plugins/plugin.php
@@ -1,2 +1,9 @@
-<h1><?=$name?></h1>
-<?=$content?>
+<h1><?=$title?></h1>
+
+<? if (isset($messages)): ?>
+  <? foreach ($messages as $message): ?>
+  <p><?=$message?></p>
+  <? endforeach ?>
+<? endif ?>
+
+<? if (isset($content)): ?><?=$content?><? endif ?>
