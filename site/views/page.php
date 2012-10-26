31c02c9d86a4854649ea88fcfe7ac25d59f9fe7a
Jan den Besten <info@jandenbesten.net>
2012-10-26 14:48:34 +0000

    - page view aangepast zodat de titel van een afbeelding wordt getoond - update doc
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1821 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/views/page.php
+++ site/views/page.php
@@ -2,8 +2,11 @@
   <h1><?=ascii_to_entities($str_title);?></h1>
   <div class="text"><?=$txt_text?></div>
 
-  <? if (!empty($media_foto)) : ?>
-  <div class="photo"><img src="<?=SITEPATH?>/assets/pictures/<?=$media_foto?>" alt="<?=$str_title?>" /></div>
+  <? if (!empty($media_foto)) : $title=get_img_title('pictures/'.$media_foto); ?>
+  <div class="photo">
+    <img src="<?=SITEPATH?>/assets/pictures/<?=$media_foto?>" alt="<?=$title?>" />
+    <p class="img_title"><?=$title?></p>
+  </div>
   <? endif; ?>
 <? endif ?>
 
