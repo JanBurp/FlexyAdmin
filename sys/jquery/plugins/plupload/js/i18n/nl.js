f5459b5d29b540046a6430d31f84d687cbb75bad
Jan den Besten <info@jandenbesten.net>
2012-10-28 16:53:17 +0000

    - multiple file-upload
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1826 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ sys/jquery/plugins/plupload/js/i18n/nl.js
@@ -0,0 +1,21 @@
+// Dutch
+plupload.addI18n({
+   'Select files' : 'Selecteer bestand(en):',
+   'Add files to the upload queue and click the start button.' : 'Voeg bestanden toe aan de wachtrij en druk op \'Start\'.',
+   'Filename' : 'Bestandsnaam',
+   'Status' : 'Status',
+   'Size' : 'Grootte',
+   'Add Files' : 'Voeg bestanden toe',
+   'Stop Current Upload' : 'Stop',
+   'Start Uploading Queue' : 'Start',
+   'Uploaded %d/%d Files': '%d/%d bestanden ge-upload',
+   'N/A' : 'Niet beschikbaar',
+   'Drag files here.' : 'Sleep bestanden hierheen.',
+   'File extension error.': 'Ongeldig bestandstype.',
+   'File size error.': 'Bestandsgrootte Error.',
+   'Init error.': 'Initialisatie error.',
+   'HTTP Error.': 'HTTP Error.',
+   'Security error.': 'Beveiliging error.',
+   'Generic error.': 'Onbekende error.',
+   'IO error.': 'IO error.'
+});
