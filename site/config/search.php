82d7b8eec9ab60ea0dc91dea9bce0204aaf763a2
Jan den Besten <info@jandenbesten.net>
2012-11-13 09:50:35 +0000

    - search module: volgorde kan ingesteld worden in config
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1842 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/config/search.php
+++ site/config/search.php
@@ -9,7 +9,7 @@
 | Stel hier de tabel en de velden van die tabel in waarin wordt gezocht
 |
 */
-$config['table']=get_menu_table();
+$config['table']='tbl_items';
 $config['title_field']='str_title'; // Titelveld: wordt in gezocht en gebruikt als titel voor het resultaat
 $config['text_field']='txt_text';   // Tekstveld: wordt in gezocht
 $config['extra_fields']=array();    // Geef hier extra velden waarin gezocht moet worden
@@ -34,13 +34,14 @@ $config['result_page_where']=array('str_module','search');
 |
 | Hier bepaal je hoe het zoekresultaat eruit komt te zien
 */
-$config['order_as_tree']=TRUE;				    // Gesorteerd als een menu, kan alleen als gezocht wordt in een menu tabel
+$config['order_as_tree']=FALSE;				    // Gesorteerd als een menu, kan alleen als gezocht wordt in een menu tabel
+$config['order_by']=FALSE;                // Eventuele volgorde aangeven hier (zoals db->order_by(..))
 $config['show_full_title']=FALSE;			    // Laat titel helemaal zien
 
 $config['group_result_by_uris']=array();  // Je kunt het resultaat groeperen in groepen met dezelfde uri-parts
 
 $config['result_max_length']=0;           // Lengte van tekst onder de titel van de zoekresultaten
-$config['result_max_type']='CHARS';       // Lengte wordt geteld als: CHARS | WORDS | LINES
+$config['result_max_type']='LINES';       // Lengte wordt geteld als: CHARS | WORDS | LINES
 $config['result_max_ellipses']='...';     // Als de getoonde tekst langer is: stel hier het eind van de tekst in.
 
 
