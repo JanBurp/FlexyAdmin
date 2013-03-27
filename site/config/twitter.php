dd1ec35337113a1208306cecd99a5f8419caa0ac
Jan den Besten <info@jandenbesten.net>
2013-03-27 11:04:00 +0000

    - new Twitter module - new jquery.styleTwitter plugin
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1967 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/config/twitter.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-
-$config['user']='jan_db';
-
-/* End of file config.php */
-/* Location: ./system/application/config/config.php */
