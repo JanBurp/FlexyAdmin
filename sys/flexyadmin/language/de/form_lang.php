2c407704b07a106e1d0ac540f4a0c45ac33b4d16
Jan den Besten <info@jandenbesten.net>
2013-04-10 07:51:08 +0000

    - adding CI translations in german (de), the FA specific files are still in English - better doc for multilanguage
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@1995 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ sys/flexyadmin/language/de/form_lang.php
@@ -0,0 +1,19 @@
+<?
+
+$lang['form_cancel'] 			= "Cancel";
+$lang['form_reset'] 			= "Reset";
+$lang['form_submit'] 			= "Submit";
+$lang['form_new_item'] 		= "new item";
+
+$lang['form_new'] 				= "New";
+$lang['form_edit'] 				= "Edit";
+$lang['form_insert'] 			= "Insert";
+$lang['form_delete'] 			= "Delete";
+
+$lang['form_item'] 				= "Item";
+
+$lang['form_dropdown_sort_on_name'] 				= "Sorted on name";
+$lang['form_dropdown_sort_on_last_upload'] 	= "Last %s uploads";
+
+
+?>
