439d4368bc4ec29c8e1da221153ee7e2c9dae892
Jan den Besten <info@jandenbesten.net>
2013-05-22 08:44:47 +0000

    - add remove_sizes option to Content - cleanup Content - set standard config for Content
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2048 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/config/config.php
+++ site/config/config.php
@@ -65,10 +65,9 @@ $config['language']	= "nl";
 | Parse content
 |--------------------------------------------------------------------------
 |
-| Set how the content must be parsed, possible settings: 'safe_emails', 'auto_target_links', 'add_classes', 'add_popups', 'replace_language_links', 'replace_soft_hyphens'.
-| Defaults are all FALSE except for 'safe_emails' and 'add_classes'.
+| Set how the content must be parsed, possible settings: 'safe_emails', 'auto_target_links', 'add_classes', 'remove_sizes', 'replace_language_links', 'replace_soft_hyphens'.
 */
-$config['parse_content']  = array( 'safe_emails'=>TRUE, 'auto_target_links'=> TRUE );
+$config['parse_content']  = array( 'safe_emails'=>FALSE, 'auto_target_links'=> TRUE, 'add_classes'=>FALSE, 'remove_sizes'=>TRUE );
 
 
 /*
