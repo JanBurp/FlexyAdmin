973edba669340fbe0fd12276c334cd5673525f66
Jan den Besten <info@jandenbesten.net>
2013-08-09 18:52:07 +0000

    - CHANGED: nicer plugin_newsletter, with styling options
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2154 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/config/plugin_newsletter.php
+++ site/config/plugin_newsletter.php
@@ -32,7 +32,7 @@ $config['trigger'] = array(
 |
 */
 
-$config['intro_length']          = 300;
+$config['intro_length']          = 0;
 $config['allowed_tags']          = '<br/><strong><italic><em><b><a><p><h1><h2><h3><h4>';
 
 $config['send_to_address_table'] = 'tbl_newsletter_addresses';
@@ -42,5 +42,15 @@ $config['send_to_name_field']    = 'str_name';
 $config['content_table']         = ''; // If empty then the standard menu table will be choosen
 
 $config['send_one_by_one']       = TRUE; // SET this to TRUE if Your provider can't send bulk mail with bcc
+
+
+$config['styles'] = array(
+  'td'  => 'font-size:13px;line-height:20px;letter-spacing:.07em;font-family: Arial,Helvetica,sans-serif;',
+  'p'   => 'font-size:13px;line-height:20px;letter-spacing:.07em;font-family: Arial,Helvetica,sans-serif;',
+  'a'   => 'color:#09C;font-size:13px;line-height:20px;letter-spacing:.07em;font-family: Arial,Helvetica,sans-serif;text-decoration:none;',
+  'h1'  => 'color:#09C;font-size:16px;line-height:20px;letter-spacing:.07em;font-family: Arial,Helvetica,sans-serif;',
+  'h2'  => 'color:#09C;font-size:13px;line-height:20px;letter-spacing:.07em;font-family: Arial,Helvetica,sans-serif;',
+  'hr'  => 'color:#09C;border-color:#09C;'
+);
 
 ?>
