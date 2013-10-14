3701ba3a616173759442caa7da44a5ca5a25c359
Jan den Besten <info@jandenbesten.net>
2013-10-14 12:22:02 +0000

    - NEW: plugin_strange_chars
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2208 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ site/config/plugin_strange_chars.php
@@ -0,0 +1,41 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Plugin methods:
+| Set the names of the methods you use when FlexyAdmin want to call you
+| If empty or commented, FlexyAdmin doesn't call them.
+|--------------------------------------------------------------------------
+|
+*/
+
+$config['admin_api_method']        = '_admin_api';
+
+
+/*
+|--------------------------------------------------------------------------
+| Plugins specific config settings
+|--------------------------------------------------------------------------
+|
+*/
+
+$config['fields'] = array('*.str_title','*.txt_text');
+
+$config['replace'] = array(
+  'Ã¯'  => '&iuml;',
+  'Ã¯'  => '&iuml;',
+  'Ã©'  => '&eacute;',
+  'Ã«'  => '&euml;',
+  'â‚¬' => '&euro;',
+  
+
+  'â€˜' => "'",
+
+  '�'  => '',
+  'Â'  => ''
+  
+)
+
+
+
+?>
