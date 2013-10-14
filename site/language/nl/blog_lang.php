602fcc961d6f6fa86c00bd4dc8a31a4447615405
Jan den Besten <info@jandenbesten.net>
2013-10-14 13:42:29 +0000

    - NEW: blog module rewritten and with new configurarions (pagination, read_more etc.)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2211 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

---
+++ site/language/nl/blog_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['read_more']='lees verder';
+
+
+?>
--- site/views/blog.php
+++ site/views/blog.php
@@ -6,10 +6,18 @@
 		<h2><?=$item['str_title']?></h2>
 		<p><?=$item['niceDate']?></p>
 		<p><?=$item['txt_text']?></p>
+    <?php if (isset($item['read_more_url'])): ?>
+      <a href="<?=$item['read_more_url']?>"><?=$read_more?></a>
+    <?php endif ?>
 		<? if (isset($item['comments'])): ?>
 			<?=$item['comments']?>
 		<? endif ?>
 	</div>
 	<? endforeach; ?>
+  
+  <?php if (!empty($pagination)): ?>
+	<hr/>
+  <div class="pagination"><?=$pagination?></div> 
+  <?php endif ?>
 
 </div>
