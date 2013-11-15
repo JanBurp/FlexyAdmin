438a2ee2415077d151896e6557adbfa54869bcb1
Jan den Besten <info@jandenbesten.net>
2013-11-15 19:25:23 +0000

    - removed plugin views
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2270 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- site/views/forum/new_thread.php
+++
@@ -1,4 +0,0 @@
-<div class="forum_form new_thread">
-  <h2><?=lang('form_new_thread');?></h2>
-  <?=$form?>
-</div>
--- site/views/forum/top.php
+++
@@ -1,6 +0,0 @@
-<div class="forum_top">
-  <? if (isset($config['user_name'])): ?>
-  <div class="forum_welcome"><p><?=langp('info_welcome',$config['user_name'])?> <?=langp('info_last_login',$config['user_lastlogin'])?></p></div>
-  <? endif ?>
-  <?=$config['menu']?>
-</div>
