c1184819c73e199127901d8c7317d11c620aed6c
Jan den Besten <info@jandenbesten.net>
2014-01-09 15:07:32 +0000

    - BUG: when using 'url_suffix' some admin links (pagination) gave problems
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2324 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/core/MY_Config.php
+++ sys/flexyadmin/core/MY_Config.php
@@ -19,6 +19,35 @@ Class MY_Config extends CI_Config {
 		array_push($this->_config_paths,SITEPATH);
 	}
 
+	/**
+	 * Site URL
+	 * Returns base_url . index_page [. uri_string]
+	 *
+	 * @access	public
+	 * @param	string	the URI string
+	 * @return	string
+	 */
+	function site_url($uri = '')
+	{
+		if ($uri == '')
+		{
+			return $this->slash_item('base_url').$this->item('index_page');
+		}
+
+		if ($this->item('enable_query_strings') == FALSE)
+		{
+			$suffix = ($this->item('url_suffix') == FALSE) ? '' : $this->item('url_suffix');
+      if (has_string('/admin',$uri)) $suffix=''; // JDB Change to original
+			return $this->slash_item('base_url').$this->slash_item('index_page').$this->_uri_string($uri).$suffix;
+		}
+		else
+		{
+			return $this->slash_item('base_url').$this->item('index_page').'?'.$this->_uri_string($uri);
+		}
+	}
+
+
+
 /**
 	* Zet een config item, eventueel met een sectie
 	*
