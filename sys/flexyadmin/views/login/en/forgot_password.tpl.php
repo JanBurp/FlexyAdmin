59674f9bd00cc13a4793dab0f0fd036f93751159
Jan den Besten <info@jandenbesten.net>
2014-01-30 10:44:43 +0000

    - move login views
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2346 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/views/login/en/forgot_password.tpl.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-	<h1>Reset Password for '<?=$user?>'</h1>
-	<p>Please click <a href="<?=site_url($forgotten_password_uri.'?code='.$forgotten_password_code?>)">this link</a> to reset your password.</p>
-</body>
-</html>
--- sys/flexyadmin/views/login/nl/deny.tpl.php
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-	<h1>Afgewezen account voor <?=$identity;?></h1>
-	<p>Uw aanvraag voor een account is afgewezen.</p>
-</body>
-</html>
