a5196dd2e4c3fefd83000e246870fc94c346896f
Jan den Besten <info@jandenbesten.net>
2014-05-22 13:36:07 +0000

    - NEW: email address for bug reports can be set in index.php: // define("ERROR_EMAIL","error@flexyadmin.com");
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2426 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951

--- sys/flexyadmin/errors/error_php.php
+++ sys/flexyadmin/errors/error_php.php
@@ -1,34 +1,29 @@
 <div style="border:1px solid #696;margin:4px;padding:10px;color:#000;">
-<?php if (IS_LOCALHOST): ?>
+<?php if (false): ?>
 	<h4 style="font-weight:bold;font-size: 14px;color: #696;">A PHP Error was encountered</h4>
-	<pre>
-	Severity:	<?php echo $severity; ?>
-	Message:	<?php echo $message; ?>
-	Filename:	<?php echo $filepath; ?>
-	Line:		<?php echo $line; ?>
-	</pre>
-
+	Severity:	<?php echo $severity; ?><br>
+	Message:	<?php echo $message; ?><br>
+	Filename:	<?php echo $filepath; ?><br>
+	Line:		<?php echo $line; ?><br>
 	<?php if ($severity!="Warning") { backtrace_(3); } ?>
 <?php else : ?>
+  <?php
+	  $to="error@flexyadmin.com";
+    if (defined("ERROR_EMAIL")) $to=ERROR_EMAIL;
+  ?>
   <h4 style="font-weight:bold;font-size: 14px;color: #696;">Sorry, an error (<?php echo $severity?>) has been encoutered</h4>
-  <?php if ($severity!="Warning") : ?>
-		<p>A mail is sent to flexyadmin.com with all necessary error information.</p>
-    <?php
-		$to="error@flexyadmin.com";
-		$subject="FlexyAdmin ERROR: ".$_SERVER['HTTP_HOST'];
-		$body="FLEXYADMIN ERROR FROM: '".$_SERVER['HTTP_HOST']."' \n\n";
-		$body.="Severity:\t".$severity."\n";
-		$body.="Message:\t".$message."\n";
-		$body.="File:\t\t".$filepath."\n";
-		$body.="Line:\t\t".$line."\n";
-		$body.="\n\nBACKTRACE:\n".print_r(backtrace_(3,false),true);
-		$body.="\n\nSERVER:\n".print_r($_SERVER,true);
-		mail($to,$subject,$body);
-    ?>
-  <?php else : ?>
-	  <p>Contact your webmaster.</p>
-  <?php endif ?>
-
+  <p>A mail is sent to "<?=$to?>" with all necessary error information.</p>
+  <?php
+    $subject="FlexyAdmin ERROR: ".$_SERVER['HTTP_HOST'];
+    $body="FLEXYADMIN ERROR FROM: '".$_SERVER['HTTP_HOST']."' \n\n";
+    $body.="Severity:\t".$severity."\n";
+    $body.="Message:\t".$message."\n";
+    $body.="File:\t\t".$filepath."\n";
+    $body.="Line:\t\t".$line."\n";
+    $body.="\n\nBACKTRACE:\n".print_r(backtrace_(3,10,false),true);
+    $body.="\n\nSERVER:\n".print_r($_SERVER,true);
+    mail($to,$subject,$body);
+  ?>
 <?php endif ?>
 </div>
 
