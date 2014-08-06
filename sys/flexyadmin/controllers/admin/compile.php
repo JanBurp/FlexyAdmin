2a6fa1baecb42cbbcc529690a4882df7db280985
Jan den Besten <info@jandenbesten.net>
2014-08-06 04:18:13 +0000

    - NEW: Added rem.min.js for using rem units in css and this will make them work on unsupported browsers (IE8) - CHANGED: compile has now an config file
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2494 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 06d97b16647efa47303d5410fd63c4f7813034aa
    Former-commit-id: 0c9ce7ec553f9ed171680cb9377e1c6bdc52a261

--- sys/flexyadmin/controllers/admin/compile.php
+++ sys/flexyadmin/controllers/admin/compile.php
@@ -3,66 +3,56 @@
 require_once(APPPATH.'libraries/less/Less.php');
 
 /**
- * LESS compiler and minimizer for frontend styles, use: admin/compile
+ * LESS compiler and minimizer for frontend styles, use: admin/compile and set config in config/compile.php
  *
  * @package FlexyAdmin
  * @author Jan den Besten
  */
 
 class Compile extends AdminController {
-  
-  var $css_path = 'site/assets/css';
-  var $normalize_css = 'normalize.css';
-  var $minimized_css = 'styles.min.css';
-  var $exclude_css_files = array('admin.css','ie6.css','ie7.css','ie8.css','ie9.css','ie10.css','normalize.css','text.css','layout.css');
 
 	public function __construct() {
 		parent::__construct();
+    $this->load->config('compile',true);
+    $this->settings=$this->config->item('compile');
 	}
   
   public function index() {
     $options=array('compress' => true);
-		$this->_set_content(h('Compile LESS files to CSS files'));
+		$this->_set_content(h('Compile LESS files & Minify CSS files'));
 
-    // Start with minimizing normalize.css
-    $minimized="/* Minified styles. Created by FlexyAdmin (www.flexyadmin.com) ".date("Y-m-d")." */\n";
-    $minimized.=$this->minimize_css( read_file($this->css_path.'/'.$this->normalize_css) );
-    // Compile Less files and add them
-    $less_files=read_map($this->css_path,'less',FALSE,FALSE);
-    // always text.less first!
-    if (isset($less_files['text.less'])) {
-      $text=$less_files['text.less'];
-      unset($less_files['text.less']);
-      array_unshift($less_files,$text);
-    }
-    foreach ($less_files as $less_file) {
-      $less=$less_file['path'];
-      $css=str_replace('.less','.css',$less);
+    // 1 - Compile Less files
+    $this->_add_content(h('Compiling LESS files',2));
+    $less_files=$this->settings['less_files'];
+    foreach ($less_files as $less => $css) {
       try{
         $parser = new Less_Parser();
         $parser->parseFile($less, site_url());
         $output = $parser->getCss();
         write_file($css,$output);
-        // minimized
-        $parser = new Less_Parser($options);
-        $parser->parseFile($less, site_url());
-        $minimized.=$parser->getCss();
         // message
-    		$this->_add_content(p().$less.' => '.$css._p());
+    		$this->_add_content($less.' => '.$css.br());
       }catch(Exception $e){
     		$this->_add_content(p('error').$less.' => Fatal error: ' . $e->getMessage()._p());
       }
     }
     
-    // Add and minimize other .css files
-    $css_files=read_map($this->css_path,'css',FALSE,FALSE);
-    $css_files=array_unset_keys($css_files,$this->exclude_css_files);
-    unset($css_files[$this->minimized_css]);
+    // 2 - Combine files
+    $this->_add_content(h('Combining CSS files',2));
+    $combine=$this->settings['banner'];
+    $css_files=$this->settings['css_files'];
     foreach ($css_files as $css_file) {
-      $minimized.=$this->minimize_css( read_file($css_file['path']) );
+      $combine.=$this->minimize_css( read_file($css_file) );
     }
-		$this->_add_content(p().$this->css_path.'/'.$this->minimized_css.' CREATED'._p());
-    write_file($this->css_path.'/'.$this->minimized_css,$minimized);
+		$this->_add_content(implode('<br>',$css_files));
+    
+    // 3 - Minify
+    $minified=$this->minimize_css($combine);
+    
+    // 4 - Save
+    write_file($this->settings['dest_file'], $minified);
+    
+		$this->_add_content(h('CREATED',2).$this->settings['dest_file']);
     
 		$this->_show_all();
   }
