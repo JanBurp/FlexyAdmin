c6f5baa823232ec374ea57f2cd3eb1cec251b5a1
Jan den Besten <info@jandenbesten.net>
2014-08-06 09:26:51 +0000

    - BUG: missing some keys in en forms language file
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2502 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 7d504e992c58af277eaeefd36eecb41fa59b937f
    Former-commit-id: 8539b99f31af93429c676591754b96da841faf9a

--- site/language/en/forms_lang.php
+++ site/language/en/forms_lang.php
@@ -4,12 +4,16 @@
 $lang['error_not_exists']     = "WARNING: `forms.%s` isn't set.";
 $lang['error_no_fields']      = "WARNING: `forms.%s` has no fields.";
 $lang['error_spam']           = "You'r submit has been marked as spam.";
+$lang['submit']               = 'Submit';
+
 
 // formaction_mail
 $lang['subject']              = "Email from '%str_name%' via %URL%";
 
 // forms.contact
 $lang['contact_title']        = 'Contact';
+$lang['field__str_name']      = 'Name';
+$lang['field__email_email']   = 'Email';
 $lang['field__txt_text']      = 'Question';
 $lang['contact_send_text']    = "Thanks for you're mail.";
 
