96259b518e375cb9fcbfbdd97577194dbf3e9e0f
Jan den Besten <info@jandenbesten.net>
2014-08-06 04:44:27 +0000

    - BUSY: Moving Angular test to sys/_test
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2497 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 09ae4b587a30bf66d61a499fd2c54ba99b09bdfe
    Former-commit-id: af76b008b99b211ee7a8de5bb9fe9983a43707ce

--- sys/flexyadmin_js/controllers/menu.js
+++
@@ -1,9 +0,0 @@
-flexyAdmin.controller('menuController', function($scope) {
-
-  $scope.menu = [
-    { href: "admin/test#/form/tbl_site/first", title: 'Site' },
-    { href: "admin/test#/grid/tbl_menu", title: 'Pages' },
-    { href: "admin/test#/grid/tbl_links", title: 'Links' }
-  ];
-
-});
