b87a5e3d9182e2806740b6b3f11453148b8709e2
Jan den Besten <info@jandenbesten.net>
2014-09-28 07:09:40 +0000

    - NEW: grid ready for server-side - NEW: bug grid controller was called 2 times
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2595 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 7c48a940c6ff66848ced61c3ce6e82f4b13c0802
    Former-commit-id: 990a45d7caccd390d00fbde9db891efee6d2a796

--- sys/__test/controllers/grid.js
+++ sys/__test/controllers/grid.js
@@ -1,4 +1,4 @@
-flexyAdmin.controller('GridController', ['$scope','$routeParams','$http', '$log', function($scope,$routeParams,$http,$log) {
+flexyAdmin.controller('GridController', ['$scope','$routeParams','$http', function($scope,$routeParams,$http) {
 
   // URI PARAMS
   $scope.table = $routeParams.table;
@@ -16,30 +16,29 @@ flexyAdmin.controller('GridController', ['$scope','$routeParams','$http', '$log'
     'displayedPages': 7,
     'totalItems'    : 0,
     'totalPages'    : 0
-  }
-
-  // AJAX REQUEST
-  $http.post('__api/get_table',{'table':$scope.table}).success(function(result){
-    $scope.grid=result.data;
-    $scope.afterLoading();
-  }).error(function(data){
-    $log.log('AJAX error -> Grid');
-  });
-
-  // ACTIONS AFTER LOADING
-  $scope.afterLoading = function() {
-    // Copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
-    $scope.displayedItems = [].concat($scope.grid.items);
-    // Fieldtypes
-    angular.forEach($scope.grid.field_info, function(value, key) {
-      $scope.grid.field_info[key].type = value.field.prefix();
+  };
+  
+  // LOAD FROM SERVER
+  $scope.callServer = function(tableState) {
+    // TODO server side: https://lorenzofox3.github.io/smart-table-website/#section-pipe: set st-pipe="callServer" in grid.html
+    $http.post('__api/get_table',{'table':$scope.table}).success(function(result){
+      $scope.grid=result.data;
+      // Copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
+      $scope.grid.displayedItems = [].concat($scope.grid.items);
+      // Fieldtypes
+      angular.forEach($scope.grid.field_info, function(value, key) {
+        $scope.grid.field_info[key].type = value.field.prefix();
+      });
+      // Calc stats/pagination
+      $scope.pagination.totalItems = $scope.grid.items.length;
+      $scope.pagination.totalPages = Math.ceil($scope.pagination.totalItems / $scope.pagination.itemsPerPage) ;
+    }).error(function(data){
+      $log.log('AJAX error -> Grid');
     });
-    
-    
-    // Calc stats/pagination
-    $scope.pagination.totalItems = $scope.grid.items.length;
-    $scope.pagination.totalPages = Math.ceil($scope.pagination.totalItems / $scope.pagination.itemsPerPage) ;
-  }
+  };
+  
+  $scope.callServer(); // TODO comment this when serverside
+
 
   // Order an array to its default
   $scope.keys = function(obj){
