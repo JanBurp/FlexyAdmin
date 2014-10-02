3386c01df23f3108c18984741754a2a29daae3ec
Jan den Besten <info@jandenbesten.net>
2014-10-02 08:55:22 +0000

    - NEW: img_lib config settings for frontend and new setting sharpen
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2605 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 8c94ab99bdf6b80650b650935cfe3fd00a6d0dbc
    Former-commit-id: 50b8632620cc37bf7c0313f2fc21e2f6cc5c6067

--- site/config/image_lib.php
+++ site/config/image_lib.php
@@ -1,5 +1,5 @@
 <?php
 
 // $config['image_library']    = 'imagemagick';                    // Can be:  imagemagick, netpbm, gd, gd2
-// $config['sharpen']          = '1.5';                            // Add -sharpen xx to resize options (imagemagick)
+// $config['sharpen']          = '1.5';                            // FALSE or a string with a sharpen value which will be used with imagemagick
 // $config['library_path']      = '/Applications/MAMP/Library/bin'; // local setting with MAMP
