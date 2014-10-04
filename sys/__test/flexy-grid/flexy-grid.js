547afb27e07e497e6ff634739f6785f6adbd4d24
Jan den Besten <info@jandenbesten.net>
2014-10-04 10:59:45 +0000

    - NEW: flexy-grid tree styling - NEW: flexy-grid basic tree drag n drop ordering
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2617 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: e9ed69697324d9604031ccc2be9f44902585d3d5
    Former-commit-id: f9e2f211adc8acfb68e57081ca7117f585f6eb63

--- sys/__test/flexy-grid/flexy-grid.js
+++ sys/__test/flexy-grid/flexy-grid.js
@@ -3,31 +3,6 @@ flexyAdmin.controller('GridController', ['$scope','$routeParams','$http', functi
   // URI PARAMS
   $scope.table = $routeParams.table;
   
-  $scope.sortableOptions = {
-    dragStart : function(obj) {
-      // Preserve width of the elements, first get width of headers
-      var element=obj.source.itemScope.element;
-      angular.element(element).addClass('bg-primary');
-      var cells = element.find('td');
-      var header=angular.element(document.querySelector('.flexy-grid thead'));
-      var thCells = header.find('th');
-      var widths = [];
-      angular.forEach(thCells, function(cell, key) {
-        widths.push(cell.offsetWidth+"px"); // 2px less = border
-      });
-      // set cell widths
-      var i=0;
-      angular.forEach(cells, function(cell, key) {
-        angular.element(cell).css({'width':widths[i]});
-        i++;
-      });
-    },
-    // accept: function (sourceItemHandleScope, destSortableScope) {return true},
-    // itemMoved: function (event) {},
-    // orderChanged: function(event) {},
-    containment: '.flexy-grid tbody'
-  };
-  
   // INIT DATA
   $scope.grid = {
     'table_info'      : {},
@@ -42,32 +17,134 @@ flexyAdmin.controller('GridController', ['$scope','$routeParams','$http', functi
     'totalItems'    : 0,
     'totalPages'    : 0
   };
-  //
-  $scope.cellWidths = [];
   
-  // LOAD FROM SERVER
+
+  /**
+   * SORTABLE
+   */
+  $scope.dragged_nodes = [];
+  $scope.sortableOptions = {
+    containment: '.flexy-grid tbody',
+    
+    dragStart : function(obj) {
+      console.log('dragStart');
+      
+      var row=obj.source.itemScope.element;
+      var table=angular.element(document.querySelector('.flexy-grid.'+$scope.table+' table'));
+
+      // Tree? 'Drag' whole branch
+      if ($scope.grid.table_info.tree && row.hasClass('flexy-tree-branch')) {
+        // find all nodes
+        var index=obj.source.index;
+        var level=$scope.grid.items[index]._info['level'];
+        $scope.dragged_nodes=[];
+        var node_level=0;
+        var next=index;
+        do {
+          next++;
+          node_level=$scope.grid.items[next]._info['level'];
+          if (node_level>level) $scope.dragged_nodes.push($scope.grid.items[next].id);
+        } while (node_level>level);
+        // 'hide' the nodes
+        angular.forEach($scope.dragged_nodes,function(node,key){
+          var el=angular.element(document.querySelector('.flexy-grid.'+$scope.table+' table tbody tr[id="'+node+'"]'));
+          el.addClass('hidden');
+        })
+      }
+
+      // Preserve width of the elements, first get width of headers
+      var header=table.find('thead');
+      var thCells = header.find('th');
+      var widths = [];
+      angular.forEach(thCells, function(cell, key) { widths.push(cell.offsetWidth+"px"); });
+      
+      // Preserve cell widths: sets the widths
+      var cells = row.find('td');
+      var i=0;
+      angular.forEach(cells, function(cell, key) { angular.element(cell).css({'width':widths[i++]}); });
+
+      // nice color of dragged row
+      angular.element(row).addClass('bg-primary');
+    },
+    
+    dragEnd: function (obj) {
+      console.log('dragEnd');
+      var row=obj.source.itemScope.element;
+      // Tree? show hidden nodes again
+      if ($scope.grid.table_info.tree && row.hasClass('flexy-tree-branch')) {
+        angular.forEach($scope.dragged_nodes,function(node,key){
+          var hidden=angular.element(document.querySelector('.flexy-grid.'+$scope.table+' table tbody tr[id="'+node+'"]'));
+          hidden.removeClass('hidden');
+        });
+        $scope.dragged_nodes=[];
+      }
+    },
+    
+    orderChanged : function(obj) {
+      console.log('orderChanged',obj);
+      var row=obj.source.itemScope.element;
+      // Tree? move dragged nodes after new index
+      if ($scope.grid.table_info.tree && row.hasClass('flexy-tree-branch')) {
+        var old_index=obj.source.index;
+        var new_index=obj.dest.index;
+        var number_of_nodes = $scope.dragged_nodes.length;
+        var old_nodes_index = old_index + 1;
+        var new_nodes_indes = new_index + 1;
+        // doit
+        console.log('MOVING',old_index,new_index,number_of_nodes);
+        var moving_node={};
+        for (var i = 0; i < number_of_nodes; i++) {
+          // copy node
+          moving_node = obj.dest.sortableScope.modelValue[old_nodes_index+i];
+          // remove node from list
+          obj.dest.sortableScope.removeItem(old_nodes_index+i);
+          // add node after new index
+          obj.source.itemScope.sortableScope.insertItem(new_index+1+i, moving_node);
+        }
+        // update grid.displayedItems
+        $scope.grid.displayedItems = [].concat($scope.grid.items);
+      }
+    },
+    
+    // accept: function (sourceItemHandleScope, destSortableScope) {return true},
+    // orderChanged: function(event) {},
+  };
+  
+  
+  /**
+   * LOAD FROM SERVER
+   */
   $scope.callServer = function(tableState) {
+    
     // TODO server side: https://lorenzofox3.github.io/smart-table-website/#section-pipe: set st-pipe="callServer" in grid.html
     $http.post('__api/get_table',{'table':$scope.table}).success(function(result){
+      
       $scope.grid=result.data;
-      // Copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
+
+      // Copy the references, needed for smart-table to wacht for changes in the data
       $scope.grid.displayedItems = [].concat($scope.grid.items);
+      
       // Fieldtypes
       angular.forEach($scope.grid.field_info, function(value, key) {
         $scope.grid.field_info[key].type = value.field.prefix();
       });
+      
       // Calc stats/pagination
       $scope.pagination.totalItems = $scope.grid.items.length;
       $scope.pagination.totalPages = Math.ceil($scope.pagination.totalItems / $scope.pagination.itemsPerPage) ;
+      
     }).error(function(data){
       $log.log('AJAX error -> Grid');
     });
   };
   
-  $scope.callServer(); // TODO comment this when serverside
+  $scope.callServer(); // TODO comment this when serverside pagination/order etc.
 
 
-  // Order an array to its default
+
+  /**
+   * MAKE SURE ORDER OF ROWS IS ORIGINAL (keys)
+   */
   $scope.keys = function(obj){
     if (!obj) return [];
     var keys=Object.keys(obj);
--- sys/flexyadmin/controllers/__api/get_table.php
+++ sys/flexyadmin/controllers/__api/get_table.php
@@ -7,6 +7,9 @@ class get_table extends ApiController {
     'limit'   => 0,
     'offset'  => 0
   );
+  
+  private $fields;
+  private $hidden_fields;
 
 
 	public function __construct($field='') {
@@ -15,63 +18,29 @@ class get_table extends ApiController {
 	}
   
 
+  /**
+   * Gets the data and information and returns it
+   *
+   * @return void
+   * @author Jan den Besten
+   */
   public function index() {
     // DEFAULTS
     $items=FALSE;
     $field_info=FALSE;
     $table_info=FALSE;
-    
+
     if ($this->args['table']) {
-      
       // FIELD INFO
-      $fields=$this->db->list_fields($this->args['table']);
-      $field_info=array();
-      $unset_fields=array();
-      foreach ($fields as $field) {
-        $prefix=get_prefix($field);
-        $full_name=$this->args['table'].'.'.$field;
-        $info=$this->cfg->get('cfg_field_info',$full_name);
-        if (!el('b_show_in_grid',$info,true)) {
-          $unset_fields[]=$field;
-        }
-        else {
-          if ($info) $info=array_unset_keys($info,array('id','field_field'));
-          $field_info[$field]=array(
-            'table'     => $this->args['table'],
-            'field'     => $field,
-            'ui_name'   => $this->ui->get($field),
-            'info'      => $info,
-            'editable'  => !in_array($field,$this->config->item('NON_EDITABLE_FIELDS')),
-            'incomplete'=> in_array($prefix,$this->config->item('INCOMPLETE_DATA_TYPES'))
-          );
-        }
-      }
-      
+      $this->fields = $this->db->list_fields($this->args['table']);
+      $field_info=$this->_get_field_info();
       // TABLE INFO
-      $table_info=$this->cfg->get('cfg_table_info',$this->args['table']);
-      $table_info['ui_name']  = $this->ui->get($this->args['table']);
-      $table_info['sortable'] = !in_array('order',$fields);
-      $table_info['tree']     = in_array('self_parent',$fields);
-
+      $table_info=$this->_get_table_info();
       // GET DATA
-      $this->db->unselect($unset_fields);
-      $this->db->max_text_len(100);
-      if ($table_info['tree']) $this->db->order_as_tree();
-      $items = $this->crud->get($this->args);
-
+      $items=$this->_get_data($table_info);
       // PROCESS DATA
       if ($items) {
-        // STRIP TAGS
-        $txtKeys=array_combine($fields,$fields);
-        $txtKeys=filter_by_key($txtKeys,'txt');
-        if ($txtKeys) {
-          foreach ($items as $id => $row) {
-            foreach ($txtKeys as $key) {
-              $items[$id][$key]=strip_tags($row[$key]);
-            }
-          }
-        }
-        else $txtKeys=array();
+        $items = $this->_process_data($items,$table_info,$field_info);
       }
     }
     
@@ -81,9 +50,140 @@ class get_table extends ApiController {
       'field_info'  =>$field_info,
       'items'       =>$items
     );
-    
     return $this->_result(array('data'=>$data));
   }
+  
+  
+  
+  /**
+   * Gets information about all fields
+   *
+   * @return array
+   * @author Jan den Besten
+   */
+  private function _get_field_info() {
+    $this->hidden_fields=array();
+    $field_info=array();
+    foreach ($this->fields as $field) {
+      $prefix=get_prefix($field);
+      $full_name=$this->args['table'].'.'.$field;
+      $info=$this->cfg->get('cfg_field_info',$full_name);
+      if (!el('b_show_in_grid',$info,true)) {
+        $this->hidden_fields[]=$field;
+      }
+      else {
+        if ($info) $info=array_unset_keys($info,array('id','field_field'));
+        $field_info[$field]=array(
+          'table'     => $this->args['table'],
+          'field'     => $field,
+          'ui_name'   => $this->ui->get($field),
+          'info'      => $info,
+          'editable'  => !in_array($field,$this->config->item('NON_EDITABLE_FIELDS')),
+          'incomplete'=> in_array($prefix,$this->config->item('INCOMPLETE_DATA_TYPES'))
+        );
+      }
+    }
+    return $field_info;
+  }
+  
+  /**
+   * Gets information about the table
+   *
+   * @return array
+   * @author Jan den Besten
+   */
+  private function _get_table_info() {
+    $table_info=$this->cfg->get('cfg_table_info',$this->args['table']);
+    $table_info['ui_name']  = $this->ui->get($this->args['table']);
+    $table_info['sortable'] = !in_array('order',$this->fields);
+    $table_info['tree']     = in_array('self_parent',$this->fields);
+    return $table_info;
+  }
+  
+  
+  /**
+   * Gets the data from the table
+   *
+   * @return array
+   * @author Jan den Besten
+   */
+  private function _get_data($table_info) {
+    $this->db->unselect($this->hidden_fields);
+    $this->db->max_text_len(100);
+    if ($table_info['tree']) $this->db->order_as_tree();
+    $items = $this->crud->get($this->args);
+    return $items;
+  }
+  
+  
+  /**
+   * Loop through all rows and process them
+   *
+   * @param string $items 
+   * @param string $table_info 
+   * @param string $field_info 
+   * @return void
+   * @author Jan den Besten
+   */
+  private function _process_data($items,$table_info,$field_info) {
+
+    // init STRIP TAGS in txt fields
+    $txtKeys=array_combine($this->fields,$this->fields);
+    $txtKeys=filter_by_key($txtKeys,'txt');
+
+    // INIT TREE
+    $parents=array();
+
+    // LOOP ROWS
+    foreach ($items as $id => $row) {
+      
+      // STRIP TAGS in txt fields
+      foreach ($txtKeys as $key) {
+        $items[$id][$key]=strip_tags($row[$key]);
+      }
+      
+      // TREE, BRANCHES & NODES
+      if ($table_info['tree']) {
+        $parent_id = $row['self_parent'];
+      
+        // toplevel: no branch, no node
+        if ($parent_id == 0) {
+          $level=0;
+          $is_branch=false;
+          $is_node=false;
+        }
+        // find out what level
+        else {
+          $is_node=true;
+          // are we on a known level?
+          if (isset($parents[$parent_id])) {
+            $level=$parents[$parent_id];
+          }
+          else {
+            // no: remember new level
+            $level++;
+            $parents[$parent_id]=$level;
+          }
+        }
+        // add this info to this item
+        $items[$id]['_info'] = array(
+          'is_branch' => false, // this will be set later...
+          'is_node'   => $is_node,
+          'level'     => $level
+        );
+      }
+    }
+    
+    // LOOP AGAIN TO ADD BRANCH INFO
+    if ($table_info['tree'] and !empty($parents)) {
+      foreach ($parents as $id => $level) {
+        $items[$id]['_info']['is_branch']=true;
+      }
+    }
+    
+    return $items;
+  }
+  
 
 }
 
