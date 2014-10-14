c9b54347877af8d078cf56bee06edc5eb37b513a
Jan den Besten <info@jandenbesten.net>
2014-10-14 19:21:42 +0000

    - NEW: unit-testing main-app & flexy-login-controller @busy
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2646 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 244b75cf4f18e168f6dff9acbc9c0a296392de8b
    Former-commit-id: 9c31f1953c2f0f29ed277842fb94cfe9ecc95606
    Former-commit-id: 8bf67e52c2548c632b87a5833c6320191b9bc176

---
+++ sys/__test/flexy-admin-app.js
@@ -0,0 +1,119 @@
+'use strict';
+
+var flexyAdmin = angular.module( 'flexyAdmin', [
+  // Angular JS
+  'ngRoute',
+  
+  // Angular Modules
+  'http-auth-interceptor',
+  'angular-toArrayFilter',
+  'angular-loading-bar',
+  'ui.bootstrap',
+  'ui.bootstrap.showErrors',
+  'smart-table',
+  'ui.sortable',
+  
+  // flexyAdmin Modules
+  'flexyMenu',
+  'flexyBlocks',
+  ]
+);
+
+
+/**
+ * SOME SPARE GLOBALS as a constant
+ */
+
+flexyAdmin.constant('flexyAdminGlobals',{
+  base_url      : 'admin/__test',  
+  api_base_url  : '__api/',
+  sys_folder    : 'sys/__test/',
+  log_prefix    : 'FA ',
+});
+
+
+
+/**
+ * ROUTING
+ */
+
+flexyAdmin.config( function($routeProvider){
+  $routeProvider
+    .when('/home',{
+      controller  : '',
+      templateUrl : 'flexy-ui/flexy-home.html',
+    })
+    .when('/logout',{
+      controller  : 'flexyLogoutController',
+      template    : '',
+    })
+    .when('/grid/:table',{
+      controller  : '',
+      templateUrl : 'flexy-grid/flexy-grid.html'
+    })
+    .when('/form/:table/:id',{
+      controller  : 'FormController',
+      templateUrl : 'flexy-form/flexy-form.html'
+    })
+    .otherwise({ redirectTo: '/home' });
+});
+
+
+
+
+
+/**
+ * Taken from demo of: https://github.com/witoldsz/angular-http-auth
+ * 
+ * Hide/shows login-form if needed
+ * Show app if angular is ready
+ */
+flexyAdmin.directive('flexyAuthenticate', ['flexyAuthService',function(flexyAuthService) {
+
+  var login = angular.element(document.querySelector('#login'));
+  var container  = angular.element(document.querySelector('#container'));
+
+  function hide_all() {
+    login.addClass('hidden');
+    container.addClass('hidden');
+  }
+  function show_login() {
+    login.removeClass('hidden');
+    container.addClass('hidden');
+  }
+  function hide_login() {
+    login.addClass('hidden');
+    container.removeClass('hidden');
+  }
+  
+  return {
+    restrict: 'C',
+    link: function(scope, elem, attrs) {
+      hide_all();
+      flexyAuthService.check().then(
+        function(success) {
+          if (flexyAuthService.loggedIn()) {
+            hide_login();
+          }
+          else {
+            show_login();
+          }
+        },
+        function(error) {
+          show_login();
+        }
+      );
+      
+      // EVENT RESPONSE
+      scope.$on('event:auth-loginRequired', function() {
+        show_login();
+      });
+      scope.$on('event:auth-loginConfirmed', function() {
+        hide_login();
+      });
+
+      // READY LOADING ANGULAR
+      elem.removeClass('hidden');
+    }
+  }
+}]);
