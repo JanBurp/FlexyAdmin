a4f12252b64779637f4d8574b1fe2273795993c7
Jan den Besten <info@jandenbesten.net>
2014-11-06 12:46:17 +0000

    - USERGUIDE: Added the old userguide again (fits better in my workflow)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2681 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 170f3a043f8e7834155d0c08d245c57216b1c608
    Former-commit-id: d494daf10ddcc6eb7bac3fa8051d58c1dc927195
    Former-commit-id: 688c3966a78e1df4b3f88a163a08b154e91fae7f

--- sys/flexyadmin/views/admin/__/doc_tipue_json.php
+++
@@ -1,5 +0,0 @@
-var tipuedrop = { "pages":[
-<?php foreach ($data as $s): ?>
-  { "title": "<?=$s['title']?>", "thumb": "", "text": "<?=$s['text']?>", "tags": "<?=$s['tags']?>", "loc": (root+"<?=$s['loc']?>") },
-<?php endforeach ?>
-]};
