e59769a6831559e520c79b4c696268e9e590d263
Jan den Besten <info@jandenbesten.net>
2014-11-23 00:22:12 +0000

    - CHANGED: rewrite of form.php, and keep old form old_form.php
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2741 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 2ada67358e84535a44f204895949c9877d1bbb63
    Former-commit-id: 7ab6a54a53776b25930978f62efa188ae0415e5a
    Former-commit-id: 92b339b7fa1e8282a100ad080ee2cd562d349299

---
+++ sys/flexyadmin/views/admin/form/form.php
@@ -0,0 +1,9 @@
+<form id="<?=$id?>" form_id="<?=$form_id?>" class="<?=$class?>" action="<?=$action?>" method="<?=$method?>" role="form" accept-charset="utf-8" enctype="multipart/form-data">
+  <input name="__form_id" id="__form_id" value="<?=$form_id?>" class="hidden" type="hidden">
+  <?php foreach ($fieldsets as $fieldset): ?>
+  <fieldset class="<?=$fieldset['class']?>" id="<?=$fieldset['id']?>">
+    <?php if (!empty($fieldset['title'])): ?><legend><?=$fieldset['title']?></legend><?php endif ?>
+    <?=$fieldset['fields']?>
+  </fieldset>
+  <?php endforeach ?>
+</div>
