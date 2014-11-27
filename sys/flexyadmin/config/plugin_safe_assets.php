34389bae26060225a72b899cfbc5016329b0c874
Jan den Besten <info@jandenbesten.net>
2014-11-27 16:20:57 +0000

    - CHANGED: better gulpfile.js
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2758 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: c8a321ff620141026141d1816dc081ad7b737f72
    Former-commit-id: 97a27c89a9ba5a039730b9371c9021b28d13e8a6
    Former-commit-id: d24ba8d7d7757e4d720e512ff6bb55efdae9e7d6

--- sys/flexyadmin/config/plugin_safe_assets.php
+++ sys/flexyadmin/config/plugin_safe_assets.php
@@ -51,7 +51,7 @@ $config['file_types'] = array(
 	SITEPATH.'assets/lists'				=> 'js',
 	SITEPATH.'assets/css'					=> 'css|htc|php|eot|svg|ttf|woff|otf|less|map',
 	SITEPATH.'assets/img'					=> 'jpg|jpeg|gif|png|tiff|cur|ico|swf|flv', 
-	SITEPATH.'assets/js'					=> 'js|css|html|swf|jpg|jpeg|gif|png|tiff|cur',
+	SITEPATH.'assets/js'					=> 'js|css|html|swf|jpg|jpeg|gif|png|tiff|cur|map',
   SITEPATH.'assets/fonts'       => 'eot|svg|ttf|woff'
 );
 
