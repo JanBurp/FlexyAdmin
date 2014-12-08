a1bd3c6177fd2ffa1212308aa99de6e990876387
Jan den Besten <info@jandenbesten.net>
2014-12-08 16:01:31 +0000

    - CHANGED: form betere bootstrap ondersteuning
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2772 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 34025558e29203d007f3e620ed5692e614ba89c4
    Former-commit-id: e051b8572585db77dfcf17f56bf760391e712aee
    Former-commit-id: 6f36271448fc823d37f381956478cac372a54a10

--- sys/flexyadmin/views/admin/form/field.php
+++ sys/flexyadmin/views/admin/form/field.php
@@ -1,6 +1,6 @@
 <div class="<?=$field['container_class']?>">
-  <label for="<?=$field['name']?>" class="<?=$field['label_class']?> <?=($field['horizontal_bootstrap'])?'col-sm-2':'';?>"><?=$field['label']?></label>
   <?php if (isset($field['validation_error'])): ?><?=$field['validation_error']?><?php endif ?>
+  <label for="<?=$field['name']?>" class="<?=$field['label_class']?> <?=($field['horizontal_bootstrap'])?'col-sm-2':'';?>"><?=$field['label']?></label>
   <?php if ($field['horizontal_bootstrap']): ?><div class="col-sm-10"><?php endif ?>
   <?=$field['control']?>
   <?php if ($field['horizontal_bootstrap']): ?></div><?php endif ?>
