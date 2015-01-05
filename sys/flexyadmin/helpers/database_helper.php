8e068d8d68895a3f2c1dead7be47bcce7e44908b
Jan den Besten <info@jandenbesten.net>
2015-01-05 11:48:09 +0000

    - CHANGED: mogelijk om een site te maken zonder menu tabel
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2815 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 4c8d52346bc8d2a64be8d7f777fbb8b94d2a7918
    Former-commit-id: 0345f6b997f6eaf8e583852816d6a62182458c28
    Former-commit-id: 7163cc3816d308154c729bd0531bf6e55cbef11e

--- sys/flexyadmin/helpers/database_helper.php
+++ sys/flexyadmin/helpers/database_helper.php
@@ -208,31 +208,34 @@ function get_fields_from_input($wildfields,$tables='') {
  * @author Jan den Besten
  */
 function find_module_uri($module,$full_uri=true,$table='') {
-  $CI=&get_instance();
-	$CI->db->select('id,uri');
-  if ($table) $CI->db->where('str_table',$table);
-	if ($full_uri) {
-		$CI->db->select('order,self_parent');
-		$CI->db->uri_as_full_uri();
-	}
-	if (get_prefix($CI->config->item('module_field'))=='id') {
-		// Modules from foreign table
-		$foreign_key=$CI->config->item('module_field');
-		$foreign_field='str_'.get_suffix($CI->config->item('module_field'));
-		$foreign_table=foreign_table_from_key($foreign_key);
-		$CI->db->add_foreigns();
-		$like_field=$foreign_table.'__'.$foreign_field;
-	}
-	else {
-		// Modules direct from field
-		$like_field=$CI->config->item('module_field');
-	}
-	$CI->db->like($CI->config->item('module_field'),$module);
-  $CI->db->order_by('id');
-	$items=$CI->db->get_result(get_menu_table());
-  reset($items);
-  $item=current($items);
-	return $item['uri'];
+  if (get_menu_table()) {
+    $CI=&get_instance();
+  	$CI->db->select('id,uri');
+    if ($table) $CI->db->where('str_table',$table);
+  	if ($full_uri) {
+  		$CI->db->select('order,self_parent');
+  		$CI->db->uri_as_full_uri();
+  	}
+  	if (get_prefix($CI->config->item('module_field'))=='id') {
+  		// Modules from foreign table
+  		$foreign_key=$CI->config->item('module_field');
+  		$foreign_field='str_'.get_suffix($CI->config->item('module_field'));
+  		$foreign_table=foreign_table_from_key($foreign_key);
+  		$CI->db->add_foreigns();
+  		$like_field=$foreign_table.'__'.$foreign_field;
+  	}
+  	else {
+  		// Modules direct from field
+  		$like_field=$CI->config->item('module_field');
+  	}
+  	$CI->db->like($CI->config->item('module_field'),$module);
+    $CI->db->order_by('id');
+  	$items=$CI->db->get_result(get_menu_table());
+    reset($items);
+    $item=current($items);
+  	return $item['uri'];
+  }
+  return false;
 }
 
 
