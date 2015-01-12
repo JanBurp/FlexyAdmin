a205669b85d632cfc681fa6cefd18a09c9f892da
Jan den Besten <info@jandenbesten.net>
2015-01-12 13:23:22 +0000

    - CHANGED: form groups has more classes
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2821 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 838caeb72d59b99029c7a48cd80d589602bc46bc
    Former-commit-id: 884cf4e67dd590651b5b55bd2cd2756ad97c9389
    Former-commit-id: 27a24ada79e6c87be6cef99c640ef4e311efcfbf

--- sys/flexyadmin/views/admin/form/field.php
+++ sys/flexyadmin/views/admin/form/field.php
@@ -1,4 +1,4 @@
-<div class="<?=$field['container_class']?>">
+<div class="<?=$field['container_class']?> field_<?=$field['name']?> fieldtype_<?=get_prefix( $field['name'],'_')?>">
   <?php if (isset($field['validation_error'])): ?><?=$field['validation_error']?><?php endif ?>
   <label for="<?=$field['name']?>" class="<?=$field['label_class']?> <?=($field['horizontal_bootstrap'])?'col-sm-2':'';?>"><?=$field['label']?></label>
   <?php if ($field['horizontal_bootstrap']): ?><div class="col-sm-10"><?php endif ?>
