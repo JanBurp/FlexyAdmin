4ee5c8e58b1ddf504deca70f75a88a147035dc73
Jan den Besten <info@jandenbesten.net>
2015-01-20 05:52:32 +0000

    - CHANGED: build.cfg default watch=FALSE (was 'dev') - CHANGED: formfields has now status classes (and style) - BUG: formfield requered class was not set
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2835 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 6bcd5f3b70d47821346504d1e7080c9833f7e1ff
    Former-commit-id: bfff963a035e62f1257f679053e2e1efa0621a79
    Former-commit-id: d2ab882b56dbb396644aac69ba05c534f39ac794

--- sys/flexyadmin/views/admin/form/field.php
+++ sys/flexyadmin/views/admin/form/field.php
@@ -1,10 +1,10 @@
-<div class="<?=$field['container_class']?> field_<?=$field['name']?> fieldtype_<?=get_prefix( $field['name'],'_')?>">
+<div class="<?=$field['container_class']?> field_<?=$field['name']?> fieldtype_<?=get_prefix( $field['name'],'_')?> <?=$field['status']?>">
   <?php if (isset($field['validation_error'])): ?><?=$field['validation_error']?><?php endif ?>
   <label for="<?=$field['name']?>" class="<?=$field['label_class']?> <?=($field['horizontal_bootstrap'])?'col-sm-2':'';?>"><?=$field['label']?></label>
   <?php if ($field['horizontal_bootstrap']): ?><div class="col-sm-10"><?php endif ?>
   <?=$field['control']?>
   <?php if ($field['horizontal_bootstrap']): ?></div><?php endif ?>
-  <?php if (isset($field['html'])): ?>
+  <?php if (isset($field['html']) and !empty($field['html'])): ?>
   <div class="<?=$styles['field_html']?>"><?=$field['html']?></div>
   <?php endif ?>
 </div>
