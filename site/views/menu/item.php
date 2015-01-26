7fea36f4905165d9519777424026ab843b450f93
Jan den Besten <info@jandenbesten.net>
2015-01-26 11:31:09 +0000

    - CHANGED: menu has now a _pos counter (from back) - BUG: debug_helper trace_() did not show integers
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2849 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: d5d28d3c1b6f3c78d6f50b0c6baceadc6e5d664d
    Former-commit-id: fba5405feb75c9861d22047accfcbde4d35883ec
    Former-commit-id: 02fff5859c3650267210a17b2e866f6b96524cd3

--- site/views/menu/item.php
+++ site/views/menu/item.php
@@ -1,8 +1,8 @@
 <li id="menu_lev<?=$lev?>_pos<?=$pos?>" class="lev<?=$lev?> pos<?=$pos?> _pos<?=$_pos?> <?=$order?> <?=$sub?> <?=$class_uri?> <?=$class?> <?=$current?>" <?=$attr?>>
   <?php if (!$submenu): ?>
-  <a href="<?=site_url($uri)?>" class="lev<?=$lev?> pos<?=$pos?> <?=$order?> <?=$class_uri?> <?=$current?> <?=(!$clickable?'disabled':'')?>"><?=$title?></a>
+  <a href="<?=site_url($uri)?>" class="lev<?=$lev?> pos<?=$pos?> _pos<?=$_pos?><?=$order?> <?=$class_uri?> <?=$current?> <?=(!$clickable?'disabled':'')?>"><?=$title?></a>
   <?php else: ?>
-  <a href="<?=site_url($uri)?>" class="dropdown-toggle <?=($framework=='bootstrap'?'disabled':'')?> lev<?=$lev?> pos<?=$pos?> <?=$order?> <?=$class_uri?> <?=$current?>"><?=$title?></a>
+  <a href="<?=site_url($uri)?>" class="dropdown-toggle <?=($framework=='bootstrap'?'disabled':'')?> lev<?=$lev?> pos<?=$pos?> _pos<?=$_pos?> <?=$order?> <?=$class_uri?> <?=$current?>"><?=$title?></a>
   <?php if ($framework=='bootstrap'): ?><a class="dropdown-toggle dropdown-caret" data-toggle="dropdown" aria-expanded="false"><b class="caret"></b></a><?php endif ?>
   <?=$submenu?>
   <?php endif ?>
