c1954fdc5015553ce0c719bf51b0a004c0862ef0
Jan den Besten <info@jandenbesten.net>
2015-02-22 19:16:56 +0000

    - API: cleanup
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2927 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 8097788154fd26e88fbfb57cb4b0eba9c83fb6d9
    Former-commit-id: df02ca87d31b0a0ecc3e911bf1d02d8994572fb4
    Former-commit-id: 4b922ed86b5fd7e4b4b73fb10928181dac4616b7

---
+++ sys/__test/flexy-services/flexy-menu-service.js
@@ -0,0 +1,29 @@
+/**
+ * FlexyAdmin (c) Jan den Besten
+ * www.flexyadmin.com
+ * 
+ * @author: Jan den Besten
+ * @copyright: Jan den Besten
+ * @license: n/a
+ * 
+ * $Author$
+ * $Date$
+ * $Revision$
+ * $HeadURL$ 
+ */
+
+
+/**
+ * flexy-menu-service
+ */
+
+flexyMenu.factory( 'flexy-menu-service', ['flexy-api-service',function(api) {
+  'use strict';
+  
+  
+  
+
+  return 
+  
+  };
+}]);
