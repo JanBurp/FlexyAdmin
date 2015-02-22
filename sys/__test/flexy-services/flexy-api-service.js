5919d87f41fede84da92c19f6c6ba677be0052ce
Jan den Besten <info@jandenbesten.net>
2015-02-22 16:59:59 +0000

    - API: result.args has a key type which holds GET or POST
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@2924 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 7052e9f98335c9d04bfa2ec43f6182a07c19f92f
    Former-commit-id: e65485735a4d5cf0eccca043cc44d940e35d0878
    Former-commit-id: 5ec47460e2a8f1055e4c458c94e2938ff6e2bff4

---
+++ sys/__test/flexy-services/flexy-api-service.js
@@ -0,0 +1,27 @@
+/**
+ * FlexyAdmin (c) Jan den Besten
+ * www.flexyadmin.com
+ * 
+ * @author: Jan den Besten
+ * @copyright: Jan den Besten
+ * @license: n/a
+ * 
+ * $Author$
+ * $Date$
+ * $Revision$
+ * $HeadURL$ 
+ */
+
+
+/**
+ * flexy-api-service
+ */
+
+flexyMenu.factory( 'flexy-api-service', [flexyAdminGlobals,'$http',function(flexyAdminGlobals,$http) {
+  'use strict';
+  
+
+  return 
+  
+  };
+}]);
