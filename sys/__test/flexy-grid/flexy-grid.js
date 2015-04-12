b0dc2a782ae2c92b4c9a17a56bef5b3a9e70324c
Jan den Besten <info@jandenbesten.net>
2015-04-12 07:07:25 +0000

    - NEW: flexy-grid sortable better code & tested better
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3041 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 62723c3797cfafc62a9b14481f21af20c45e844c
    Former-commit-id: ff1b4c2335e9821f353f65b6512e01f472d86234
    Former-commit-id: 46fa41ce7e1f116c902bab645f6e4c2abd250ac1

--- sys/__test/flexy-grid/flexy-grid.js
+++ sys/__test/flexy-grid/flexy-grid.js
@@ -107,119 +107,144 @@ flexyAdmin.controller('GridController', ['flexySettingsService','flexyGridServic
 
 
   /**
-   * ngSortable OPTIONS & CALLBACKS
+   * NG.SORTABLE OPTIONS
    */
-  var dragged_children = [];
-  var table = angular.element(document.querySelector('.flexy-grid.'+$scope.table+' table'));
-  
   $scope.sortableOptions = {
-    containment: '.flexy-grid tbody',
-    // containerPositioning: 'relative',
-
-    /**
-     * START DRAGGING
-     */
-    dragStart : function(obj) {
-      dragged_children=[];
-      var row=obj.source.itemScope.row._info;
-      // Tree? Find children and hide them
-      if ($scope.type.is_tree && row.has_children) {
-        var index=obj.source.index;
-        var node_level=0;
-        do {
-          index++;
-          if (angular.isDefined($scope.gridItems[index])) {
-            node_level=$scope.gridItems[index]._info.level;
-            if (node_level>row.level) dragged_children.push($scope.gridItems[index].id);
-          }
-        } while (node_level>row.level && angular.isDefined($scope.gridItems[index]));
-        // Hide the children
-        angular.forEach(dragged_children,function(node,key){
-          angular.element(document.querySelector('tbody tr[id="flexy-grid-row_'+node+'"]',table)).hide();
-        });
-      }
-      
-      // Preserve width of the elements
-      var widths = [];
-      angular.forEach( table.find('th') , function(cell, key) {
-        widths.push(cell.offsetWidth+"px");
-      });
-      angular.forEach( obj.source.itemScope.element.find('td'), function(cell, key) {
-        angular.element(cell).css({'width':widths.shift()});
-      });
-      // Preserve position & size of dragging row
-      var panel = angular.element(document.querySelector('.panel.flexy-grid.'+$scope.table));
-      var pos   = panel.offset();
-      var width = panel.width()+1;
-      angular.element(document.querySelector('.as-sortable-drag',panel)).css({'margin-top':(-pos.top),'margin-left':(-pos.left+13),'max-width':width});
-    },
-
-    /**
-     * ORDER HAS CHANGED, if has children make sure they are on right place and determine new level & parent
-     */
-    orderChanged : function(obj) {
-      if ($scope.type.is_tree) {
-        var new_index=obj.dest.index;
-        var number_of_children = dragged_children.length;
-        var new_level=0;
-        var new_parent_id=0;
-        
-        // level = 0 except when the next item has a higher level: use that level and parent
-        var next_index = new_index+1;
-        if (next_index < obj.dest.sortableScope.modelValue.length) {
-          var next_item = obj.dest.sortableScope.modelValue[next_index];
-          new_level     = next_item._info.level;
-          new_parent_id = next_item._info.self_parent;
-        }
-        // set level, parent and is_child
-        obj.dest.sortableScope.modelValue[new_index]._info.level = new_level;
-        obj.dest.sortableScope.modelValue[new_index].self_parent = new_parent_id;
-        // is_child when level > 0
-        obj.dest.sortableScope.modelValue[new_index]._info.is_child = false;
-        if (new_level>0) obj.dest.sortableScope.modelValue[new_index]._info.is_child = true;
-        
-        // MOVE dragged NODES after new index
-        if (number_of_children>0) {
-          var level_diff = obj.dest.sortableScope.modelValue[new_index]._info.level - new_level; // old level - new level
-          var old_index = obj.source.index;
-          var up = (new_index<old_index);
-          if (up) {
-            old_index++;
-            new_index++;
-          }
-          // collect dragged nodes
-          for (var i = 0; i < number_of_children; i++) {
-            // adjust level & copy the node from dest
-            obj.dest.sortableScope.modelValue[old_index]._info.level -= level_diff;
-            dragged_children.push(obj.dest.sortableScope.modelValue[old_index]);
-            // remove node from dest
-            obj.dest.sortableScope.removeItem(old_index);
-          }
-          // insert new items in dest after new index
-          dragged_children.reverse();
-          if (!up) new_index = new_index-number_of_children + 1;
-          for (i = 0; i < number_of_children; i++) {
-            obj.dest.sortableScope.insertItem(new_index, dragged_children[i]);
-          }
-        }
-        
-        // Update Grid
-        $scope.displayedItems = [].concat($scope.gridItems);
-      }
-
-    },
-
-    /**
-     * DRAG END -  if tree show hidden children again
-     */
-    dragEnd: function (obj) {
-      angular.forEach(dragged_children,function(node,key){
-        angular.element(document.querySelector('tbody tr[id="flexy-grid-row_'+node+'"]',table)).show();
-      });
-    },
-
+    containment:    '.flexy-grid tbody',
+    dragStart :     function(obj) { sortable.dragStart(obj); },
+    orderChanged :  function(obj) { sortable.orderChanged(obj); },
+    dragEnd:        function(obj) { sortable.dragEnd(obj); },
   };
 
+  var sortable = {
+    dragged_children : [],
+    table            : angular.element(document.querySelector('.flexy-grid.'+$scope.table+' table'))
+  };
+
+  /**
+   * START DRAGGING: hide and remember children
+   */
+  sortable.dragStart = function(obj) {
+    // Reset dragged children
+    sortable.dragged_children=[];
+    // Is table a tree and row has children?
+    if ($scope.type.is_tree && obj.source.itemScope.row._info.has_children) {
+      // Find the children
+      sortable.dragged_children = sortable.find_children(obj);
+      // Hide them
+      sortable.hide_rows( sortable.dragged_children );
+    }
+    // Preserve width of cells in dragged row
+    var widths = [];
+    angular.forEach( sortable.table.find('th') ,              function(cell, key) {  widths.push(cell.offsetWidth+"px");                    });
+    angular.forEach( obj.source.itemScope.element.find('td'), function(cell, key) {  angular.element(cell).css({'width':widths.shift()});   });
+    // Preserve position & size of dragged row
+    var panel = angular.element(document.querySelector('.panel.flexy-grid.'+$scope.table));
+    var pos   = panel.offset();
+    var width = panel.width()+1;
+    angular.element(document.querySelector('.as-sortable-drag',panel)).css({'margin-top':(-pos.top),'margin-left':(-pos.left+13),'max-width':width});
+  };
+
+  /**
+   * ORDER HAS CHANGED, if has children make sure they are on right place and determine new level & parent
+   */
+  sortable.orderChanged = function(obj) {
+    if ($scope.type.is_tree) {
+      var number_of_children = sortable.dragged_children.length;
+      var new_index          = obj.dest.index;
+      
+      // set (new) level, parent and is_child
+      var next_index    = new_index+1;
+      var nr_of_items   = obj.dest.sortableScope.modelValue.length;
+      var old_level     = obj.dest.sortableScope.modelValue[new_index]._info.level;
+      var new_level     = 0; // level = 0 except when the next item has a higher level: use that level
+      var new_parent_id = 0; // parent = 0 except when the next item has a higher level: use that parent
+      if (next_index < nr_of_items) {
+        new_level     = obj.dest.sortableScope.modelValue[next_index]._info.level;
+        new_parent_id = obj.dest.sortableScope.modelValue[next_index]._info.self_parent;
+      }
+      // set level, parent and is_child
+      obj.dest.sortableScope.modelValue[new_index].self_parent    = new_parent_id;
+      obj.dest.sortableScope.modelValue[new_index]._info.level    = new_level;
+      obj.dest.sortableScope.modelValue[new_index]._info.is_child = (new_level>0);  // is_child when level > 0
+      
+      // MOVE dragged NODES after new index
+      if (number_of_children>0) {
+        var level_diff = old_level - new_level;
+        var old_index = obj.source.index;
+        var up = (new_index<old_index);
+        if (up) {
+          old_index++;
+          new_index++;
+        }
+        // collect dragged nodes
+        for (var i = 0; i < number_of_children; i++) {
+          // adjust level & copy the node from dest
+          obj.dest.sortableScope.modelValue[old_index]._info.level -= level_diff;
+          sortable.dragged_children.push(obj.dest.sortableScope.modelValue[old_index]);
+          // remove node from dest
+          obj.dest.sortableScope.removeItem(old_index);
+        }
+        // insert new items in dest after new index
+        sortable.dragged_children.reverse();
+        if (!up) new_index = new_index-number_of_children + 1;
+        for (i = 0; i < number_of_children; i++) {
+          obj.dest.sortableScope.insertItem(new_index, sortable.dragged_children[i]);
+        }
+      }
+      
+      // Update Grid
+      $scope.displayedItems = [].concat($scope.gridItems);
+    }
+  };
+  
+  /**
+   * DRAG END -  Show hidden children again
+   */
+  sortable.dragEnd = function(obj) {
+    sortable.show_rows( sortable.dragged_children );
+  };
+
+
+
+
+  /**
+   * Find children of row object
+   */
+  sortable.find_children = function(obj) {
+    var children = [];
+    var row=obj.source.itemScope.row._info;
+    var index=obj.source.index;
+    var node_level=0;
+    do {
+      index++;
+      if (angular.isDefined($scope.gridItems[index])) {
+        node_level=$scope.gridItems[index]._info.level;
+        if (node_level>row.level) children.push($scope.gridItems[index].id);
+      }
+    } while (node_level>row.level && angular.isDefined($scope.gridItems[index]));
+    return children;
+  };
+  
+  /**
+   * Hide rows
+   */
+  sortable.hide_rows = function(rows) {
+    angular.forEach(rows,function(node,key){
+      angular.element(document.querySelector('tbody tr[id="flexy-grid-row_'+node+'"]', sortable.table )).hide();
+    });
+  };
+
+  /**
+   * Show rows
+   */
+  sortable.show_rows = function(rows) {
+    angular.forEach(rows,function(node,key){
+      angular.element(document.querySelector('tbody tr[id="flexy-grid-row_'+node+'"]',sortable.table)).show();
+    });
+  };
+  
+  
 
     
 }]);
