68cccc7e49579268481f281ddf5b544e09644ab1
Jan den Besten <info@jandenbesten.net>
2015-05-03 16:14:27 +0000

    - TESTS after CI 3.0 update
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3077 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 4a396cfee9d734ef44d71b66c856d315e956d775
    Former-commit-id: 091615d404d980daf60f50269c534834060aa4c4
    Former-commit-id: f8779565bbceb01e1fc4e70f86dab31ee8a21c04

--- sys/__test/tests/e2e_test-sortable.js
+++ sys/__test/tests/e2e_test-sortable.js
@@ -5,9 +5,28 @@ describe('E2E test for sortable', function(){
   var rows  = element.all(by.css('tbody tr',table));
 
 
+   beforeEach(function(){
+     browser.driver.manage().window().maximize();
+     // Login
+     browser.get('#home');
+     var loginButtonExists = by.id('login-button');
+     browser.driver.wait(function() {
+       return browser.driver.isElementPresent(loginButtonExists);
+     }, 5000);
+     browser.driver.findElement(by.id('login-username')).sendKeys( 'admin' );
+     browser.driver.findElement(by.id('login-password')).sendKeys( 'admin' );
+     element(by.id('login-button')).click();
+     expect( element(by.css('#container')).isDisplayed() ).toBe(true);
+   });
+   
+   afterEach(function(){
+     // Logout
+     browser.get('#logout');
+   });
+
   
   it('Show Menu', function() {
-    browser.driver.manage().window().maximize();
+
     browser.get('#/grid/tbl_menu');
     expect( element(by.css('#content')).isDisplayed() ).toBe(true);
     expect( element(by.css('#content .flexy-grid')).isDisplayed() ).toBe(true);
@@ -16,16 +35,16 @@ describe('E2E test for sortable', function(){
     expect( rows.count() ).toEqual(5);
     
     // Verplaats eerste item naar eind
-    var first = element.all(by.css('tbody tr:first',table));
-    
-    var yourOffset = {x:5,y:5};
-    browser.actions()
-        .mouseMove(first,yourOffset)
+    // var first = element.all(by.css('tbody tr:first',table));
+    //
+    // var yourOffset = {x:5,y:5};
+    // browser.actions()
+        // .mouseMove(first,yourOffset)
         // .mouseDown()
         // .mouseMove(first,{x:0,y:0}) // Initial move to trigger drag start
         // .mouseMove(first,{x:0,y:50}) // [] optional
         // .mouseUp()
-        .perform();
+        // .perform();
     
     // var pos = table.getPosition(first);
     
