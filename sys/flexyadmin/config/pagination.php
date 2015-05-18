88c9aa76c8c3177ab0a040f80788e64203580afd
Jan den Besten <info@jandenbesten.net>
2015-05-18 13:44:13 +0000

    - CHANGED: MY_Pagination gebruikt nu config bestand site/config/pagination.php Daarin de templates (default maar ook bootstrap)
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3144 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: dfc21aedde7130992acbf72ba0f3bad63c836bcf
    Former-commit-id: 87b8db18f3a3cf11f5654781f3fcb239c5b4e983
    Former-commit-id: 29fd2db84076e3cd00fe864566ebc207e93ba36e

---
+++ sys/flexyadmin/config/pagination.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Default FlexyAdmin
+$config['total_tag_open']  = '<span class="pagination_total">';
+$config['total_tag_close'] = '</span>';
+$config['first_link']      = '&lt;&lt;';
+$config['last_link']       = '&gt;&gt;';
+$config['full_tag_open']   = '<ul>';
+$config['full_tag_close']  = '</ul>';
+$config['num_tag_open']    = '<li class="pager"><span class="btn btn-default">';
+$config['num_tag_close']   = '</span></li>';
+$config['cur_tag_open']    = '<li class="active"><span class="btn btn-primary">';
+$config['cur_tag_close']   = '</span></li>';
+$config['first_tag_open']  = '<li class="pager pagination_first"><span class="btn btn-default">';
+$config['first_tag_close'] = '</span></li>';
+$config['last_tag_open']   = '<li class="pager pagination_last"><span class="btn btn-default">';
+$config['last_tag_close']  = '</span></li>';
+$config['prev_tag_open']   = '<li class="pager pagination_prev"><span class="btn btn-default">';
+$config['prev_tag_close']  = '</span></li>';
+$config['next_tag_open']   = '<li class="pager pagination_next"><span class="btn btn-default">';
+$config['next_tag_close']  = '</span></li>';
+
+// Bootstrap
+// $config['total_tag_open']  = '<ul class="pagination"><li class="pagination-total"><a>';
+// $config['total_tag_close'] = '</a></li></ul>';
+// $config['first_link']      = '<span class="glyphicon glyphicon-fast-backward"></span>';
+// $config['last_link']       = '<span class="glyphicon glyphicon-fast-forward"></span>';
+// $config['prev_link']       = '<span class="glyphicon glyphicon-backward"></span>';
+// $config['next_link']       = '<span class="glyphicon glyphicon-forward"></span>';
+// $config['full_tag_open']   = '<ul class="pagination">';
+// $config['full_tag_close']  = '</ul>';
+// $config['num_tag_open']    = '<li>';
+// $config['num_tag_close']   = '</li>';
+// $config['cur_tag_open']    = '<li class="active"><a>';
+// $config['cur_tag_close']   = '</a></li>';
+// $config['first_tag_open']  = '<li class="pagination-first">';
+// $config['first_tag_close'] = '</li>';
+// $config['last_tag_open']   = '<li class="pagination-last">';
+// $config['last_tag_close']  = '</li>';
+// $config['prev_tag_open']   = '<li class="pagination-prev">';
+// $config['prev_tag_close']  = '</li>';
+// $config['next_tag_open']   = '<li class="pagination-next">';
+// $config['next_tag_close']  = '</li>';
+
+
+if (file_exists(SITEPATH."config/pagination.php")) require_once(SITEPATH."config/pagination.php");
+
