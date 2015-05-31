cac9c5c02b2b44b00eaaadd166b8f4553956f760
Jan den Besten <info@jandenbesten.net>
2015-05-31 22:05:26 +0000

    - TESTS: Make PHPUNIT work for FlexyAdmin
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3164 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 200f956e72dfc20ad18affa3b4b91512a157e4a6
    Former-commit-id: 44d76fb8d938ae029b5095653c585e56b4c636b0
    Former-commit-id: 6083f615df3b1300e51b5a1a1d79f6ac4fb74a7f

---
+++ sys/flexyadmin/tests/_old/flexyadmin/ZZ_CodeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class ZZ_CodeTest extends CIUnit_Framework_TestCase {
+
+  /**
+   * Test of er nog debughelper commando's zijn
+   *
+   * @return void
+   * @author Jan den Besten
+   */
+  public function test_debug_code() {
+    $files=read_map('sys/flexyadmin','php',true,false,false,false);
+    unset($files['sys/flexyadmin/helpers/debug_helper.php']);
+    foreach ($files as $file) {
+      $lines=file($file['path']);
+      foreach ($lines as $key => $line) {
+        $found=preg_match("/^\s*\s*(trace_|trace_if|strace_|backtrace_|xdebug_break)\(/u", $line);
+        $this->assertLessThan(1,$found, 'Debug helper found in `<i><b>'.$file['path'].'</i></b>` at line '.($key+1).':<br><code>'.$line.'</code>');
+      }
+    }
+  }
+
+
+}
+
+
+?>
