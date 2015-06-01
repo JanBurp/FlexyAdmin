81c944901a9026dc214e5fe784f40fc491f5b4ef
Jan den Besten <info@jandenbesten.net>
2015-06-01 04:10:40 +0000

    - TESTS: Add flexyadmin api testing
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3177 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: dc31d27c889e72bc25eec948cc2a7f7bd5fb833d
    Former-commit-id: fe5088f9dd98e61e4576a9ac97fe051604b74d48
    Former-commit-id: a14c1314475a98ce94c4d31419b5955d87c36441

--- sys/flexyadmin/tests/helpers/ArrayHelperTest.php
+++ sys/flexyadmin/tests/helpers/ArrayHelperTest.php
@@ -45,27 +45,28 @@ class ArrayHelperTest extends CITestCase {
   }
   
   
-  public function testRandomElement() {
-    $quotes = array(
-        "I find that the harder I work, the more luck I seem to have. - Thomas Jefferson",
-        "Don't stay in bed, unless you can make money in bed. - George Burns",
-        "We didn't lose the game; we just ran out of time. - Vince Lombardi",
-        "If everything seems under control, you're not going fast enough. - Mario Andretti",
-        "Reality is merely an illusion, albeit a very persistent one. - Albert Einstein",
-        "Chance favors the prepared mind - Louis Pasteur",
-        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-        "At vero eos et accusam et justo duo dolores et ea rebum.",
-        "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-        "At vero eos et accusam et justo duo dolores et ea rebum.",
-        "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-        "At vero eos et accusam et justo duo dolores et ea rebum.",
-        "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-    );
-    
-    $this->assertNotEquals(random_element($quotes), random_element($quotes));
-  }
+  // public function testRandomElement() {
+  //   $quotes = array(
+  //       "I find that the harder I work, the more luck I seem to have. - Thomas Jefferson",
+  //       "Don't stay in bed, unless you can make money in bed. - George Burns",
+  //       "We didn't lose the game; we just ran out of time. - Vince Lombardi",
+  //       "If everything seems under control, you're not going fast enough. - Mario Andretti",
+  //       "Reality is merely an illusion, albeit a very persistent one. - Albert Einstein",
+  //       "Chance favors the prepared mind - Louis Pasteur",
+  //       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  //       "At vero eos et accusam et justo duo dolores et ea rebum.",
+  //       "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+  //       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  //       "At vero eos et accusam et justo duo dolores et ea rebum.",
+  //       "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+  //       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  //       "At vero eos et accusam et justo duo dolores et ea rebum.",
+  //       "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+  //   );
+  //
+  //   $this->assertNotEquals(random_element($quotes), random_element($quotes));
+  // }
+
 }
 
 ?>
