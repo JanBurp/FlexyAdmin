12359ac1083404abe91aa8d8da7ba0c6773ead7c
Jan den Besten <info@jandenbesten.net>
2015-06-01 04:48:43 +0000

    - TESTS: Splitting tests in fast and slow and db suites
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3181 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 54d22b8e3f0e6d991f54f6bf0c37718735371046
    Former-commit-id: 5304c46f2e7d561accfb72593cb702774629adb8
    Former-commit-id: 898ccf964c90b8582742e42a55f7bf80465f8510

---
+++ sys/flexyadmin/tests/fast/helpers/ArrayHelperTest.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once('sys/flexyadmin/tests/CITestCase.php');
+
+class ArrayHelperTest extends CITestCase {
+
+  protected function setUp() {
+    $this->CI->load->helper('array');
+  }
+
+  public function testElement() { 
+    $array = array(
+        'color' => 'red',
+        'shape' => 'round',
+        'size' => ''
+    ); 
+    $expected = "red";
+     
+    $this->assertArrayHasKey('color', $array); 
+    $this->assertEquals($expected, element('color', $array)); 
+    $this->assertEquals('',element('size', $array)); 
+    $this->assertNull(element('age', $array, NULL));
+  }
+
+  public function testElements() {
+    $array = array(
+        'color' => 'red',
+        'shape' => 'round',
+        'radius' => '10',
+        'diameter' => '20'
+    );
+    
+    $expected = array(
+        'color' => 'red',
+        'shape' => 'round',
+        'height' => FALSE
+    );
+    
+    $this->assertEquals($expected, 
+        elements(array(
+            'color',
+            'shape',
+            'height'
+        ), $array));
+  }
+  
+  
+  // public function testRandomElement() {
+  //   $quotes = array(
+  //       "I find that the harder I work, the more luck I seem to have. - Thomas Jefferson",
+  //       "Don't stay in bed, unless you can make money in bed. - George Burns",
+  //       "We didn't lose the game; we just ran out of time. - Vince Lombardi",
+  //       "If everything seems under control, you're not going fast enough. - Mario Andretti",
+  //       "Reality is merely an illusion, albeit a very persistent one. - Albert Einstein",
+  //       "Chance favors the prepared mind - Louis Pasteur",
+  //       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  //       "At vero eos et accusam et justo duo dolores et ea rebum.",
+  //       "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+  //       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  //       "At vero eos et accusam et justo duo dolores et ea rebum.",
+  //       "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+  //       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+  //       "At vero eos et accusam et justo duo dolores et ea rebum.",
+  //       "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+  //   );
+  //
+  //   $this->assertNotEquals(random_element($quotes), random_element($quotes));
+  // }
+
+}
+
+?>
---
+++ sys/flexyadmin/tests/fast/helpers/EmailHelperTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('sys/flexyadmin/tests/CITestCase.php');
+
+class EmailHelperTest extends CITestCase {    
+
+  public function setUp() {
+    $this->CI->load->helper('email');
+  }
+
+  public function testEmailValidation() {
+    $this->assertTrue(valid_email('test@test.com'));
+    $this->assertFalse(valid_email('test#testcom'));
+  }
+
+}
+
+?>
