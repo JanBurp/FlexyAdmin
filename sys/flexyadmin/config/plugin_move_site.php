6aadb4016c342bfb59354b088012e149219387e5
Jan den Besten <info@jandenbesten.net>
2015-06-02 12:57:32 +0000

    - ADD: New plugin_move_site // you can move an old site to a fresh install
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3200 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: fe87e85739462c9122be3c1a7bf20472c65d7213
    Former-commit-id: feae8dfe723981fedd31acb73ed9b4696cb4b7b6
    Former-commit-id: c83fcb284f8e478d0f3f1beb1bcdc7ad0c13ec0f

---
+++ sys/flexyadmin/config/plugin_move_site.php
@@ -0,0 +1,43 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Plugin methods:
+| Set the names of the methods you use when FlexyAdmin want to call you
+| If empty or commented, FlexyAdmin doesn't call them.
+|--------------------------------------------------------------------------
+|
+*/
+
+$config['admin_api_method'] = '_admin_api';
+
+
+// paths of Old & New site
+$config['old'] = str_replace(array('sys/flexyadmin/config','/www.'),array('','/_www.'),__DIR__);
+$config['new'] = str_replace('sys/flexyadmin/config','',__DIR__);
+
+// Paths to clean before move new content in
+$config['empty'] = array(
+  'site/assets/_thumbcache',
+  'site/assets/pictures',
+  'site/assets/downloads',
+  'site/assets/img',
+);
+
+// Paths & files  to move
+$config['move'] = array(
+  'site/assets/',
+  'ontwerp/',
+);
+
+// Paths & files to merge (keep newest)
+$config['merge'] = array(
+  'site/views/',
+  'site/libraries/',
+  'site/models/',
+  'site/helpers/',
+  'site/config/',
+);
+
+
+?>
