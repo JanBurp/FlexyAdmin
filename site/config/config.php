e73bb9b6c25bef1317acfafdbf994db84feb5ddf
Jan den Besten <info@jandenbesten.net>
2015-06-17 14:51:42 +0000

    - CHANGED:  Content - remove_sizes: Je kunt nu ook alleen de 'width' of 'height' attributen verwijderen door 'width' of 'height' (of 'style').
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3224 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: bbc457af9ccb278ed54903ad88b99def0d561229
    Former-commit-id: e12195db3b273fb407e9383dfc631b2a8b953f28
    Former-commit-id: b566851d925686528b8af8e8e41bb4b8d6ce4c06

--- site/config/config.php
+++ site/config/config.php
@@ -77,7 +77,7 @@ $config['use_minimized'] = TRUE;
  * - auto_target_links      - [TRUE] alle link-tags naar externe adressen krijgen de attributen `target="_blank"` en `rel="external"` mee.
  * - site_links             - [FALSE] alle link-tags naar interne adressen worden aangepast met site_url(), zodat eventueel index.php ervoor wordt gezet.
  * - add_classes            - [FALSE] alle div, p, en img tags krijgen extra classes: een nr en 'odd' of 'even'
- * - remove_sizes           - [FALSE] width en height attributen van img tags worden verwijderd (zodat met css styling kan worden ingegrepen)
+ * - remove_sizes           - [FALSE] Als TRUE dan worden width en height attributen van img tags verwijderd (zodat met css styling kan worden ingegrepen). Je kunt ook alleen de 'width' of 'height' attributen verwijderen door 'width' of 'height' (of 'style').
  * - replace_language_links - [FALSE] Links die beginnen met een taal, bijvoorbeeld _nl/contact_ worden vervangen worden door links met de juiste taal bv: _en/contact_
  * - replace_soft_hyphens   - [FALSE] Soft Hyphens karakters (standaard [-]) worden vervangen door de HTML entity: &#173;
  */
