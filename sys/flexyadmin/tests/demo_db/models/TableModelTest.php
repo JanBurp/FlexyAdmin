589b51ed85b49323abf9ea794446707cc8e9d8cd
Jan den Besten <info@jandenbesten.net>
2015-09-11 22:35:35 +0000

    - CHANGED: Table_Model can be used on its own by setting the table name (and reste/autoset all stuff). Like Crud….
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3351 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 0d3547b357b7b18555a41cbabd06573258fccd5e
    Former-commit-id: 70f27a5f7ea7b7e89a2d71e6cd3d13e4ec9f64d3
    Former-commit-id: 9e9e991e4a631b6e7e93525caa38341ab88a8d0e [formerly 0182ae6db3f0a7b68bc38614e993f0c1879a6b27]
    Former-commit-id: 65060bbda23d09a97e5b8c0214d80092482eef65

--- sys/flexyadmin/tests/demo_db/models/TableModelTest.php
+++ sys/flexyadmin/tests/demo_db/models/TableModelTest.php
@@ -15,6 +15,23 @@ class TableModelTest extends CITestCase {
   
   
   public function testQueryBuilder() {
+    // Standard model:
+    
+    // tbl_menu
+    $this->CI->table_model->table('tbl_menu');
+    $this->assertEquals( array('id','order','self_parent','uri','str_title','txt_text','str_module','stx_description','str_keywords'), $this->CI->table_model->list_fields() );
+    $query = $this->CI->table_model->get();
+    $this->assertEquals( 5, $query->num_rows() );
+    $this->assertEquals( 9, $query->num_fields() );
+    // tbl_links
+    $this->CI->table_model->table('tbl_links');
+    $this->assertEquals( array('id','str_title','url_url'), $this->CI->table_model->list_fields() );
+    $query = $this->CI->table_model->get();
+    $this->assertEquals( 3, $query->num_rows() );
+    $this->assertEquals( 3, $query->num_fields() );
+    
+    // tbl_menu Model:
+    $this->assertEquals( array('id','order','self_parent','uri','str_title','txt_text','str_module','stx_description','str_keywords'), $this->CI->tbl_menu->list_fields() );
     $query = $this->CI->tbl_menu->get(2);
     $this->assertEquals( 2, $query->num_rows() );
     $this->assertEquals( 9, $query->num_fields() );
