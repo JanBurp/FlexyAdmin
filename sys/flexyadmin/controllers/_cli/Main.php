d161c1af496eb01811cceee5eb0db131e6e0b205
Jan den Besten <info@jandenbesten.net>
2015-09-11 05:54:33 +0000

    - ADDED: CLI controller werkt nu
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3344 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 6b488abb621570f705a845d8682a59c0853b544d
    Former-commit-id: 932ac2653fe36e686d45e2e78cc86f2e6007d3a9
    Former-commit-id: f0c93f3ba1d689a17356df56da9d89e6e6ae2dc1 [formerly d9d5d2cfcf62a592a9c9d76d781a7c10261c957c]
    Former-commit-id: 70c0aa937fba4ed9c86bd35f3546f4e4f2daffe4

---
+++ sys/flexyadmin/controllers/_cli/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * CLI Main
+ *
+ * @package default
+ * @author Jan den Besten
+ */
+class Main extends CI_Controller {
+
+  public function index()  {
+    echo "FlexyAdmin CLI present!".PHP_EOL;
+  }
+}
+
+
+
+
+?>
