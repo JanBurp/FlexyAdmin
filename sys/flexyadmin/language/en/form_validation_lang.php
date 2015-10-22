907bc6c4bcecc5f5c1b31e607b6db30dd74c9f10
Jan den Besten <info@jandenbesten.net>
2015-10-22 14:47:25 +0000

    - CHANGED: id_user veld is altijd verplicht, en begint met een leeg veld
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3436 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 0d1819cf132cec74f51a9d25702de6c085a4c388
    Former-commit-id: b5f73f71e62f5962f57abf100d20e0ac51c97b69
    Former-commit-id: 7c8f1bf23d9a3c19f8c7f47cac344c3eb5136c94 [formerly 079c7a2de61b861b5d215fa146d4965c371aa0f4]
    Former-commit-id: 9d499ede2c76e61092501dcfaf0d0c05a372ce8e

--- sys/flexyadmin/language/en/form_validation_lang.php
+++ sys/flexyadmin/language/en/form_validation_lang.php
@@ -37,6 +37,7 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$lang['validation_warning'] = 'CAUTION: Submitting did not succeed: one or more fields are not filled properly.';
 $lang['form_validation_required']		= 'The {field} field is required.';
 $lang['form_validation_isset']			= 'The {field} field must have a value.';
 $lang['form_validation_valid_email']		= 'The {field} field must contain a valid email address.';
