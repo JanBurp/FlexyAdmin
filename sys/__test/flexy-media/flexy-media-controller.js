c4d6d82d4fced872c9ef0b9e1f48667a29ca181f
Jan den Besten <info@jandenbesten.net>
2015-11-05 00:02:38 +0000

    - NEW: Busy with Media…
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3456 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 8019b9bf212acd33ee9040d2f2e58da6bd611701
    Former-commit-id: 7d6f7049d37ba3ed8bb95fd39b78730d55d7e7c6
    Former-commit-id: 57fadb33cd8c4f7f2b1d8699281198a5c5c5b6b2 [formerly ae67ddd84741cbefd231c3a42c6c3d88c3646723]
    Former-commit-id: 7f641ae6916ccc697ada61c738b57e810df2f99f

---
+++ sys/__test/flexy-media/flexy-media-controller.js
@@ -0,0 +1,24 @@
+/**
+ * FlexyAdmin (c) Jan den Besten
+ * www.flexyadmin.com
+ * 
+ * @author: Jan den Besten
+ * @copyright: Jan den Besten
+ * @license: n/a
+ * 
+ * $Author$
+ * $Date$
+ * $Revision$
+ */
+
+/*jshint -W069 */
+
+flexyAdmin.controller('MediaController', ['$scope','$routeParams', function($scope,$routeParams) {
+  'use strict';
+  
+  /**
+   * The path
+   */
+  $scope.path = $routeParams.path;
+  
+}]);
