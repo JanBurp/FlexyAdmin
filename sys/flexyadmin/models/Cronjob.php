3b5cc0441ae4cf9f6a5859e8ec4d78928acc2dfe
Jan den Besten <info@jandenbesten.net>
2015-11-10 13:38:25 +0000

    - BUG: Cronjob verbetert
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3469 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: fd8c69d4c088340a5ad12bbc5c275fb6d7f766a1
    Former-commit-id: 58fa9ebde760960e8cf8efc352bacfec555c8827
    Former-commit-id: f3a825adae3dd9bc7fe71bd63c8f39cc2be58471 [formerly defea7d5eeafd919a97255580680f1d7c258275b]
    Former-commit-id: e74dd5c05fc60f7e87f97f557059f02e4227cae5

--- sys/flexyadmin/models/Cronjob.php
+++ sys/flexyadmin/models/Cronjob.php
@@ -17,141 +17,135 @@ class Cronjob extends CI_Model {
 
 	public function go()	{
     if ($this->db->table_exists('log_cronjobs')) {
+      
       $this->jobs=$this->config->item('cronjobs');
+      
+      // Eerst alle informatie verzamelen
       foreach ($this->jobs as $key=>$job) {
-        // trace_($job);
-        $this->jobs[$key] = $this->needs_run($job);
-        if ($this->jobs[$key]['needs_run']) {
-          $this->jobs[$key]=$this->run($this->jobs[$key]);
+        $last = $this->db->get_field_where('log_cronjobs','tme_last_run','str_job',$job['name']);
+        if ($last===false)
+          $job['last'] = 0;
+        else
+          $job['last'] = mysql_to_unix($last);
+        $job['next'] = $this->_calc_next( $job['every'], $job['last'] );
+        // Moet de job gebeuren?
+        $job['run']  = ( time()>=$job['next'] );
+        $this->jobs[$key] = $job;
+      }
+      
+      
+      // Run de jobs die moeten runnen
+      foreach ($this->jobs as $key => $job) {
+        if ($this->jobs[$key]['run']) {
+          // Run
+          $name = $job['name'];
+          $this->load->library($name);
+          $job['result'] = $this->$name->index($job);
+          // $job['result'] = true;
+
+          // log in db
+          $job['at']=time();
+          if ( $job['result'] ) {
+            $this->db->set( array( 'str_job'=>$job['name'], 'tme_last_run'=>unix_to_mysql($job['at'])) );
+            if (empty($job['last']))
+              $this->db->insert('log_cronjobs');
+            else
+              $this->db->where('str_job',$job['name'])->update('log_cronjobs');
+          }
+          // log
+          if (is_string($job['result']))
+            log_message('error', 'FlexyAdmin CRONJOB '.$job['name'].' ERROR: '.$job['result']);
+          else
+            log_message('info', 'FlexyAdmin CRONJOB '.$job['name']);
         }
         else {
-          log_message('info', 'FlexyAdmin CRONJOB '.$job['name'].' needs no run');
+          log_message('info', 'FlexyAdmin CRONJOB '.$job['name'].' dit not run');
         }
+        
+        $this->jobs[$key] = $job;
       }
+      
+      
+      // echo results
+      foreach ($this->jobs as $job) {
+        if (isset($job['at']))
+          trace_( [ 'job  '=>$job['name'], 'every' => $job['every'], 'nu   '=>unix_to_normal(time()), 'last '=>unix_to_normal($job['last']), 'next '=>unix_to_normal($job['next']), 'run  '=>$job['run'], 'at    '=>unix_to_normal($job['at']), 'result'=>$job['result'] ]);
+        else
+          trace_( [ 'job  '=>$job['name'], 'every' => $job['every'], 'nu   '=>unix_to_normal(time()), 'last '=>unix_to_normal($job['last']), 'next '=>unix_to_normal($job['next']), 'run  '=>$job['run'] ]);
+      }
+      
+
     }
 	}
   
   
-  private function get_last_run($job) {
-    $job['last']=$this->db->get_field_where('log_cronjobs','tme_last_run','str_job',$job['name']);
-    return $job;
-  }
-  
-  public function needs_run($job,$now=false) {
-    if (!$now) $now=time();
-    if (!isset($job['last'])) $job=$this->get_last_run($job);
-    $last=mysql_to_unix($job['last']);        // unix stamp
-    $next=$this->_calc_next($job['every'],$last,$now);
-    $job['needs_run'] = ($last < $next) && ($now >= $next);
-    // trace_(['time'=>unix_to_mysql($now),'last'=>unix_to_mysql($last),'next'=>unix_to_mysql($next),'run'=>$job['needs_run'] ]);
-    return $job;
-  }
-  
-  private function run($job) {
-    $name=$job['name'];
-    // load
-    $this->load->library($name);
-    // run
-    $job['result']=$this->$name->index($job);
-    $job['at']=unix_to_mysql(time());
-    // trace_($job);
-    // store in db
-    if ($job['result']===TRUE) {
-      $this->db->set(array('str_job'=>$job['name'],'tme_last_run'=>$job['at']));
-      if (empty($job['last']))
-        $this->db->insert('log_cronjobs');
-      else
-        $this->db->where('str_job',$job['name'])->update('log_cronjobs');
-    }
-    // log
-    if (is_string($job['result']))
-      log_message('error', 'FlexyAdmin CRONJOB '.$job['name'].' ERROR: '.$job['result']);
-    else
-      log_message('info', 'FlexyAdmin CRONJOB '.$job['name']);
-    return $job;
-  }
-  
-  
   /**
-   * Berekent het tijdstip waarop het volgende moment valt 
+   * Berekent het tijdstip waarop het volgende moment valt, met dit als input
    * 
-   * - 5                  // every 5 minutes
-   * - 'day 10:15'        // every day at 10:15
-   * - 'week 5 12:00      // every week at 12:00 on friday (day5)
-   * - 'month 1 18:00     // every first day of the month at 18:00
+   * - 5                  // iedere 5 minuten (marge van 59 seconden)
+   * - 'day 10:15'        // iedere dag om 10:15
+   * - 'week 0 12:00      // iedere week om 12:00 op vrijdag (dag 0)
+   * - 'month 1 18:00     // iedere eerste dag van de maand om 18:00
    *
-   * @param string $every
+   * @param string $every (zie voorbeelden hierboven)
    * @param int $last unixstamp of last run
-   * @param int $now  unixstamp of next run
    * @return $next
    * @author Jan den Besten
    */
-  public function _calc_next($every,$last,$now=false) {
-    if (!$now) $now=time();
+  public function _calc_next( $every, $last ) {
     $next=false;
+    
+    // Per minuut, laatste is afronden op laatste minuut
     if (is_integer($every) or (is_numeric($every))) {
-      $next = $last + (int) $every*60;
+      $start = floor($last/60) * 60;
+      $next  = (int) $start + (int) $every * TIME_MINUTE;
+      // trace_(['every'=>$every,'last'=>unix_to_normal($last),'start'=>unix_to_normal($start),'next'=>unix_to_normal($next)]);
     }
+    
+    // Andere keuze, splits $every in soort, dagen, uren en minuten
     else {
-      $every=explode(' ',$every);
-      if ($every[0]=='day')
-        $time=explode(':',$every[1]);
-      else
-        $time=explode(':',$every[2]);
-      $hour=(int) $time[0];
-      $min=(int) $time[1];
-      
-      $day_of_week=0;
-      $day_of_month=0;
-      $day_of_year=0;
-      
-      switch ($every[0]) {
+      $every = explode(' ',$every);
+      $type  = $every[0];
+      $day   = 0;
+      $time  = explode(':',$every[1]);
+      if ( $type!=='day' ) {
+        $day  = (int)$every[1];
+        $time = explode(':',$every[2]);
+      }
+      $hour = (int) $time[0];
+      $min  = (int) $time[1];
+
+
+      switch ($type) {
         
+        // per dag: startmoment voor next is laatste run dag om 00:00, next is dan de uren en minuten erbij op deze dag
         case 'day':
-          if ($now) {
-            $start=mktime( 0,0,0, date('n',$now), date('j',$now), date('Y',$now) );
-          } else {
-            $start=mktime( 0,0,0 );
-          }
+          $start = mktime( 0,0,0, date('n',$last), date('j',$last), date('Y',$last) );
+          $next  = (int) $start + $hour*TIME_HOUR + $min*TIME_MINUTE;
+          // Next kan nooit kleiner zijn dan laatste keer, tel er dan een dag bij op
+          if ($next<$last) $next+=TIME_DAY;
           break;
-          
+        
+        // per week: - start is zondag van laatste runweek om 0:00, - next is start + de dag vd week en het tijdstip
         case 'week':
-          $a_data=getdate($now);
-          $wday=$a_data['wday']; // 0 = sunday
-          if ($now) {
-            $start=mktime( 0,0,0, date('n',$now), date('j',$now)-$wday, date('Y',$now) );
-          } else {
-            $start=mktime( 0,0,0, date('n'), date('j')-$wday );
-          }
-          $day_of_week=(int) $every[1];
-          $hour++; // Hack, blijkt dat altijd een uur te vroeg, waarom, geen idee
+          $day_of_week = date('w',$last); // 0..7
+          $start = mktime( 0,0,0, date('n',$last), date('j',$last), date('Y',$last) ) - ($day_of_week * TIME_DAY);
+          $next  = (int) $start + ($day * TIME_DAY) + $hour*TIME_HOUR + $min*TIME_MINUTE;
+          // Next kan nooit kleiner zijn dan laatste keer, tel er dan een week bij op
+          if ($next<$last) $next+=TIME_WEEK;
           break;
 
+        // per maand: startmoment is 1e dag van de volgende maand om 0:00, next wordt dan start + de dag en tijd van de maand
         case 'month':
-          $a_data=getdate($now);
-          $mday=$a_data['mday']-1; // 1 = 1e dag
-          if ($now) {
-            $start=mktime( 0,0,0, date('n',$now), date('j',$now)-$mday, date('Y',$now) );
-          } else {
-            $start=mktime( 0,0,0, date('n'), date('j')-$mday );
-          }
-          $day_of_month=(int) $every[1] -1;
+          $days_in_month = date('t',$last); // 28..31
+          $day_of_month  = date('j',$last); // 1..31
+          $start = mktime( 0,0,0, date('n',$last), date('j',$last), date('Y',$last) ) + (-$day_of_month+$days_in_month) * TIME_DAY;
+          $next  = (int) $start + $day * TIME_DAY + $hour*TIME_HOUR + $min*TIME_MINUTE;
+          // Next kan nooit kleiner zijn dan laatste keer, tel er dan een maand bij op
+          if ($next<$last) $next+=($days_in_month * TIME_DAY);
           break;
       }
       
-      $next = $start + $day_of_year*86400 + $day_of_month*86400 + $day_of_week*86400 + $hour*3600 + $min*60;
-      
-      // trace_([
-      //   '$every'=>$every,
-      //   '$start'=>unix_to_mysql($start),
-      //   '$next'=>unix_to_mysql($next),
-      //   '$day_of_year'=>$day_of_year,
-      //   '$day_of_month'=>$day_of_month,
-      //   '$day_of_week'=>$day_of_week,
-      //   '$hour'=>$hour,
-      //   '$min'=>$min,
-      // ]);
-
     }
     return $next;
   }
