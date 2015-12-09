283d016d50231e613cb0bcc17d3c7540bfff8c88
Jan den Besten <info@jandenbesten.net>
2015-12-09 09:54:41 +0000

    - BUG: Better RSS feed?
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3493 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 76bf6f7cad7acdcf2f0d1bb9ab2543316307ccc0
    Former-commit-id: 08ad2365f71ff47f5cfca74722703cd731ef3ae5
    Former-commit-id: 1b5d59f81b04bb1931d3d9aa28b46b478806bac0 [formerly b708d365063ac60760898eced9111c683236df4c]
    Former-commit-id: e7b0a5dc8d0bbd17ffbcbad0cc90ba9420acc8e6

--- sys/flexyadmin/views/rss/feed.php
+++ sys/flexyadmin/views/rss/feed.php
@@ -1,7 +1,6 @@
 <?php
 echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 ?>
-
 <feed xmlns="http://www.w3.org/2005/Atom">
   <id><?=$feed_url;?></id>
   <title><![CDATA[<?=$feed_name;?>]]></title>
@@ -12,7 +11,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 <?php foreach($posts as $entry): ?>
   <entry>
     <id><?=site_url($entry['url']);?></id>
-    <title><?=xml_convert($entry['title']);?></title>
+    <title><![CDATA[<?=xml_convert($entry['title']);?>]]</title>
     <updated><?=$entry['date'];?></updated>
     <link rel="alternate" href="<?=site_url($entry['url']);?>" />
     <summary><![CDATA[<?=$entry['body']; ?>]]></summary>
