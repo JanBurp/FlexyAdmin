8bb677fd86652a9f68329e0d751ae20429dea5ad
Jan den Besten <info@jandenbesten.net>
2016-01-25 15:29:37 +0000

    - NEW: Busy with schemaform…
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3530 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: e195898c5330ad07d7e003e747f2a3a6f6b642aa
    Former-commit-id: 36f47d041f025a0afd0ba3e96107b984f9df5775
    Former-commit-id: 44cb9c18a75a64e5a9ede0fb9560e7aac49474dc [formerly 85915f783555c302c797524e49fcb8517f2cb30d]
    Former-commit-id: 1c9e35f45fafc49f391ccf9dca77c98488e85e33

--- sys/flexyadmin/config/schemaform.php
+++ sys/flexyadmin/config/schemaform.php
@@ -7,288 +7,359 @@
  * @author			Jan den Besten
  **/
 
-
-
-/**
- * Default field
- */
 $config['FIELDS_default'] = array(
-  'type'       => 'string',
-  'form-type'  => 'input',
-  'format'     => 'string',
-  'readonly'   => false,
-  'validation' => '',
-  'default'    => ''
-);
-
-/**
- * Fields by prefix
- */
-$config['FIELDS_prefix'] = array (
-	
-  'id'				=> array (
-    'type'        => 'number',
-    'form-type'   => 'select',
-    'validation'  => ''
-  ),
-  
-  'self' => array(
-    'type'       => 'number',
-    'form-type'  => 'select',
-    'default'    => 0,
+    'schemaType' => 'string',
+    'formType'   => 'text',
+    'grid'       => '%s',
+    'form'       => '',
     'validation' => '',
-  ),
-  
-  'b'             => array (
-    'type'       => 'boolean',
-    'form-type'  => 'checkbox',
-    'validation' => ''
-  ),
-  'is'          => array (
-    'type'      => 'boolean',
-    'form-type'  => 'checkbox',
-    'validation' => ''
-  ),
-  
-  'stx'        => array (
-    'form-type' => 'textarea',
-  ),
-  'txt'        => array (
-    'form-type' => 'wysiwyg',
-    'validation'=> ''
-  ),
-  
-  'media'      => array (
-    'type'      => 'string',
-    'form-type' => 'select',
-    'validation'=> ''
-  ),
-  'medias'    => array (
-    'type'      => 'string',
-    'form-type' => 'select',
-    'validation'=> ''
-  ),
-
-
-  
-  // 'rel'        => array (
-  //     'type'  => 'number',
-  //     'form-type'       => 'select',
-  //     'validation' => ''
-  //   ),
-  // 'field'      => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'select',
-  //     'validation' => '',
-  //   ),
-  // 'fields'    => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'select',
-  //     'validation' => '',
-  //   ),
-  // 'list'      => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'select',
-  //     'validation' => ''
-  //   ),
-  // 'str'        => array (
-  //     'type'        => 'string',
-  //     'form-type'   => 'text',
-  //   ),
-  // 'md'        => array (
-  //     'type'      => 'string',
-  //     'form-type' => 'textarea',
-  //   ),
-  // 'pwd'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'password',
-  //     'validation' => '|valid_password'
-  //   ),
-  // 'gpw'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'password',
-  //     'validation' => '|valid_password'
-  //   ),
-  // 'url'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '|prep_url'
-  //   ),
-  // 'email'      => array (
-  //     'type'        => 'string',
-  //     'form-type'   => 'email',
-  //     'validation'  => '|valid_email'
-  //   ),
-  // 'file'      => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'mp3'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'mov'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'img'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'dat'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'date'      => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'tme'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'datetime'  => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'dtm'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'time'      => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  // 'int'        => array (
-  //     'type'  => 'number',
-  //     'form-type'       => 'text',
-  //     'validation' => '|number'
-  //   ),
-  // 'dec'        => array (
-  //     'type'  => 'number',
-  //     'form-type'       => 'text',
-  //     'validation' => '|numeric'
-  //   ),
-  // 'ip'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '|valid_ip'
-  //   ),
-  // 'rgb'        => array (
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '|valid_rgb'
-  //   ),
 );
                                   
                                   
-/**
- * Special fields
- */
+
 $config['FIELDS_special'] = array(
-	'id' => array(
-    'type'       => 'number',
-    'form-type'  => 'hidden',
-    'readonly'   => true,
-    'validation' => '|number|required',
-    'default'    => -1,
+
+	'id'				=> array(
+    'schemaType'  => 'integer',
+    'formType'    => 'hidden',
+    'grid'        => 'function_primary_key',
+    'form'        => 'function_primary_key',
+    'validation'	=> 'trim|integer|required',
   ),
-  'order' => array(
-    'type'      => 'number',
-    'form-type' => 'hidden',
-    'readonly'  => true,
-    'default'   => 0,
-    'validation'=> '',
+	'id_group'	=> array(
+    'schemaType'  => 'integer',
+    'formType'    => 'select',
+    'grid'        => 'function_foreign_key',
+    'form'        => 'function_id_group',
+    'validation'	=> 'integer|required',
   ),
-  
-  // 'id_group'  => array(
-  //     'type'       => 'number',
-  //     'form-type'  => 'select',
-  //     'validation' => 'number|required',
-  //   ),
-  // 'user'      => array(
-  //     'type'        => 'number',
-  //     'form-type'   => 'text',
-  //     'validation'  => '|number',
-  //   ),
-  // 'user_changed' => array(
-  //     'type'      => 'number',
-  //     'form-type' => 'hidden',
-  //     'readonly'  => true,
-  //   ),
-  //
-  // 'uri'        => array(
-  //     'type'      => 'string',
-  //     'form-type' => 'hidden',
-  //     'readonly'  => true,
-  //     'validation'=> '',
-  //   ),
-  // 'api'        => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '',
-  //   ),
-  // 'plugin'    => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '',
-  //   ),
-  //   'actions'   => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '',
-  //   ),
-  // 'abstract'  => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '',
-  //   ),
-  // 'table'      => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '',
-  //   ),
-  // 'rights'      => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '',
-  //   ),
-  // 'path'      => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '',
-  //   ),
-  // 'file'      => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => '',
-  //   ),
-  // 'str_fieldset' => array(
-  //     'type'  => 'string',
-  //     'form-type'       => 'text',
-  //     'validation' => ''
-  //   ),
-  //
-  //   'last_login'    => array(
-  //     'type'      => 'string',
-  //     'form-type' => 'text',
-  //   ),
-  //   'tme_last_changed' => array(
-  //     'type'      => 'string',
-  //     'form-type' => 'hidden',
-  //     'readonly'  => true,
-  //   ),
+	'user'			=> array(
+    'schemaType'  => 'integer',
+    'formType'    => 'text',
+    'grid'        => 'function_user',
+    'form'        => 'function_user',
+    'validation'	=> 'trim|integer',
+  ),
+	'user_changed' => array(
+    'schemaType'  => 'integer',
+    'formType'    => 'text',
+    'grid'        => 'function_user'
+  ),
+                      
+	'uri'				=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '/%s',
+    'form'        => 'hidden',
+    'validation'	=> 'trim',
+  ),
+	'api'				=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_api',
+    'validation'	=> 'trim',
+  ),
+	'plugin'		=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_plugin',
+    'validation'	=> 'trim',
+  ),
+  'actions'   => array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => 'function_actions',
+    'form'        => '',
+    'validation'	=> '',
+                      ),
+	'order'			=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => 'function_order',
+    'form'        => 'hidden',
+    'validation'	=> 'trim',
+  ),
+	'abstract'	=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => '',
+    'validation'	=> '',
+  ),
+	'table'			=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_tables',
+    'validation'	=> 'trim',
+  ),
+	'rights'			=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_rights',
+    'validation'	=> 'trim',
+  ),
+	'path'			=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_path',
+    'validation'	=> 'trim',
+  ),
+	'file'			=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_allfiles',
+    'validation'	=> 'trim',
+  ),
+	'str_fieldset' => array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_fieldsets',
+    'validation'	=> 'trim'
+  ),
+                      
+  'last_login'    => array(
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => 'function_date("Y-m-d H:i:s",%s)',
+  ),
 										
 );
 
+
+$config['FIELDS_mysql'] = array(
+
+);
+
+
+$config['FIELDS_prefix'] = array (
+	'id'				=> array (
+    'schemaType'  => 'integer',
+    'formType'    => 'select',
+    'grid'        => 'function_foreign_key',
+    'form'        => 'dropdown',
+    'validation'	=> 'trim|integer'
+  ),
+	'self'			=> array(
+    'schemaType'  => 'integer',
+    'formType'    => 'select',
+    'grid'        => 'function_self',
+    'form'        => 'function_self',
+    'validation'	=> 'trim|integer',
+  ),
+	'rel'				=> array (
+    'schemaType'  => 'integer',
+    'formType'    => 'select',
+    'grid'        => 'function_join',
+    'form'        => 'function_join',
+    'validation'	=> ''
+  ),
+	'field'			=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'select',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_field',
+    'validation'	=> 'trim',
+  ),
+	'fields'		=> array(
+    'schemaType'  => 'string',
+    'formType'    => 'select',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_fields',
+    'validation'	=> 'trim',
+  ),											
+	'media'			=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'select',
+    'grid'        => 'function_dropdown_media',
+    'form'        => 'function_dropdown_media',
+    'validation'	=> 'trim'
+  ),
+	'medias'		=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'select',
+    'grid'        => 'function_dropdown_medias',
+    'form'        => 'function_dropdown_media',
+    'validation'	=> 'trim'
+  ),
+	'list'			=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'select',
+    'grid'        => '%s',
+    'form'        => 'function_dropdown_list',
+    'validation'	=> 'trim'
+  ),
+	'str'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => ''
+  ),
+	'stx'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'textarea',
+    'grid'        => 'function_text',
+    'form'        => 'textarea'
+  ),
+	'md'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'textarea',
+    'grid'        => 'function_text',
+    'form'        => 'textarea'
+  ),
+	'txt'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'html',
+    'grid'        => 'function_text',
+    'form'        => 'htmleditor',
+    'validation'	=> ''
+  ),
+	'pwd'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'password',
+    'grid'        => '***',
+    'form'        => 'password',
+    'validation'	=> 'trim|valid_password'
+  ),
+	'gpw'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'password',
+    'grid'        => '***',
+    'form'        => 'password',
+    'validation'	=> 'trim|valid_password'
+  ),
+	'url'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '<a target="_blank" href="%s">%s</a>',
+    'form'        => '',
+    'validation'	=> 'trim|prep_url'
+  ),
+	'email'			=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '<a href="mailto:%s">%s</a>',
+    'form'        => '',
+    'validation'	=> 'trim|valid_email'
+  ),
+	'file'			=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'upload',
+    'validation'	=> 'trim'
+  ),
+	'mp3'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'upload',
+    'validation'	=> 'trim'
+  ),
+	'mov'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'upload',
+    'validation'	=> 'trim'
+  ),
+	'img'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '<img src="#IMG_MAP#/%s" alt="%s" /><p class="img_text">%s</p>',
+    'form'        => 'upload',
+    'validation'	=> 'trim'
+  ),
+	'dat'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'date',
+    'validation'	=> ''
+  ),
+	'date'			=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'date',
+    'validation'	=> ''
+  ),
+
+	'tme'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'datetime',
+    'validation'	=> ''
+  ),
+	'datetime'	=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'datetime',
+    'validation'	=> ''
+  ),
+	'dtm'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'datetime',
+    'validation'	=> ''
+  ),
+	'time'			=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => 'time',
+    'validation'	=> ''
+  ),
+	'int'				=> array (
+    'schemaType'  => 'integer',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => '',
+    'validation'	=> 'trim|integer'
+  ),
+	'dec'				=> array (
+    'schemaType'  => 'number',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => '',
+    'validation'	=> 'trim|numeric'
+  ),
+	'ip'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '%s',
+    'form'        => '',
+    'validation'	=> 'trim|valid_ip'
+  ),
+	'rgb'				=> array (
+    'schemaType'  => 'string',
+    'formType'    => 'text',
+    'grid'        => '<div class="rgb" style="background-color:%s;" title="%s"><span class="hide">%s</span></div>',
+    'form'        => '',
+    'validation'	=> 'trim|valid_rgb'
+  ),
+	'b'					=> array (
+    'schemaType'  => 'boolean',
+    'formType'    => 'checkbox',
+    'grid'        => 'function_boolean',
+    'form'        => 'checkbox',
+    'validation'	=> ''
+  ),
+	'is'					=> array (
+    'schemaType'  => 'boolean',
+    'formType'    => 'checkbox',
+    'grid'        => 'function_boolean',
+    'form'        => 'checkbox',
+    'validation'	=> ''
+	),
+);
 
