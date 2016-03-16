b61df43be63918831aa1be2436c84f971837ba4b
Jan den Besten <info@jandenbesten.net>
2016-03-16 10:44:30 +0000

    - Plugin_move_site now also imports/merge the old database with a fresh new
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3581 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 58ad8cc51aa7cfbb7fa7a3c08a46e1b9ed4110f4
    Former-commit-id: 8ce7e08b2e2f53b282993b8f2d0f3f23320527f1
    Former-commit-id: f71a3625ab8a66fba9187f582e2702d686d8951d [formerly 9eb0c6be18e85d145029dfb8938d650b705fe8b8]
    Former-commit-id: 120fa8ad6b190d330fd7dcc53b0ecb4ddd230297

--- sys/flexyadmin/config/plugins/plugin_move_site.php
+++ sys/flexyadmin/config/plugins/plugin_move_site.php
@@ -12,12 +12,46 @@
 $config['admin_api_method'] = '_admin_api';
 
 
-// paths of Old & New site
+// paths of Old & New site (auto)
 $config['old'] = str_replace(array('sys/flexyadmin/config/plugins','/www.'),array('','/_www.'),__DIR__);
 $config['new'] = str_replace('sys/flexyadmin/config/plugins','',__DIR__);
 
-// $config['old'] = str_replace(array('sys/flexyadmin/config','/www.'),array('','/_www.'),__DIR__);
-// $config['new'] = str_replace('sys/flexyadmin/config','',__DIR__);
+// Paths set by hand
+$config['old'] = '';
+$config['new'] = '';
+
+// Old database
+$config['db'] = 'old_db'; // set this group in config/database_local.php
+
+$config['truncate_demo_tables'] = array(
+  'log_activity',
+  'log_stats',
+  'res_media_files',
+  'res_menu_result',
+  'tbl_links',
+  'tbl_menu',
+);
+
+$config['merge_tables'] = array(
+  'cfg_admin_menu',
+  'cfg_auto_menu',
+  'cfg_field_info',
+  'cfg_img_info',
+  'cfg_media_info',
+  'cfg_table_info',
+  'cfg_ui',
+  'cfg_user_groups',
+  'cfg_users',
+);
+
+$config['merge_and_complete_tables'] = array(
+  'tbl_menu',
+  'tbl_links',
+  'tbl_site',
+  'res_menu_result',
+);
+
+
 
 // Paths to clean before move new content in
 $config['empty'] = array(
