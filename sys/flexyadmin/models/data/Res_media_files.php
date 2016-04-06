5752c3eaa6a4d0bea6b0bcddb7d814de6b047eca
Jan den Besten <info@jandenbesten.net>
2016-04-06 11:07:14 +0000

    - DATAMODEL: Wrapper
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3597 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 70161d9942d89a91c5a0d5310fa6715b3a6d7283
    Former-commit-id: ef9c5bff21426431a4eb2c197efab5f28fa0b769
    Former-commit-id: 65f6f3b68226cfdf0ff8a797077ffed8f897c25a [formerly 77bab6d901b008fc99bbb5ce4a48f2e78c981c82]
    Former-commit-id: 25e3660ef6fb9a6cb5b88b02bc95d189c3ab47ab

--- sys/flexyadmin/models/data/Res_media_files.php
+++ sys/flexyadmin/models/data/Res_media_files.php
@@ -9,11 +9,15 @@
  * @copyright: (c) Jan den Besten
  */
 
-Class Res_media_files extends Data_Model {
+Class Res_media_files extends Data_Model_Core {
 
   public function __construct() {
     parent::__construct();
 		$this->load->model("file_manager");
+    
+    // Add current filemanager_view setting:
+    $user_id = $this->data_model_core->get_user_id();
+    $this->settings['grid_set']['grid_view'] = $this->db->get_field_where( 'cfg_users','str_filemanager_view','id',$user_id);
   }
   
   
