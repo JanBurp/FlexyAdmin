715f71e62962bb68cc81d7b9bf707f190b034f6e
Jan den Besten <info@jandenbesten.net>
2016-05-25 11:12:48 +0000

    - AUTH: kleine aanpassingen
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3669 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 2636574d1833e8c2013d80b9d8d320df6d04170d
    Former-commit-id: 7518ccdf8a71fe4438fd588a47031c476e993698
    Former-commit-id: 93f1e3741334777f4ea08ebd11272fdc4564cce3 [formerly 81c34666962fbe9ead24ba103f658aa9c2501e4b]
    Former-commit-id: c91765fb2d24ff0349fc26ed585fbc43d9866f89

--- sys/flexyadmin/libraries/Flexy_auth.php
+++ sys/flexyadmin/libraries/Flexy_auth.php
@@ -660,7 +660,7 @@ class Flexy_auth extends Ion_auth {
       'all_users' => FALSE,
       'backup'    => FALSE,
       'tools'     => FALSE,
-      'items'     => array_combine($items,array_fill(0,count($items), RIGHTS_NO )),
+      'items'     => array_combine($items,array_fill(0,count($items), FALSE )),
 		);
     
     foreach ($groups as $group) {
@@ -703,6 +703,7 @@ class Flexy_auth extends Ion_auth {
   }
   
   private function _add_item_right( $right, $new_right ) {
+    if ( $right===FALSE ) $right = RIGHTS_NO;
     if ( $new_right['b_delete'] and ($right + RIGHTS_DELETE) <= RIGHTS_ALL ) $right+=RIGHTS_DELETE;
     if ( $new_right['b_add']    and ($right + RIGHTS_ADD)    <= RIGHTS_ALL ) $right+=RIGHTS_ADD;
     if ( $new_right['b_edit']   and ($right + RIGHTS_EDIT)   <= RIGHTS_ALL ) $right+=RIGHTS_EDIT;
@@ -735,17 +736,12 @@ class Flexy_auth extends Ion_auth {
       $user_info = $this->get_user();
     else
       $user_info = $this->get_user($user_id);
-    
-    // Rechten van de gebruiker
     $rights = $user_info['rights'];
-
     // Rechten voor aanpassen van zichzelf als:
     // - cfg_users
     // - user_id === id van huidige user
     if (  $item===$this->tables['users'] and $id===$user_info['user_id'] ) return RIGHTS_EDIT;
-    
     // Anders normale rechten:
-    // trace_(['has_rights',$item,$user_info,el( array('items',$item), $rights, RIGHTS_NO )]);
     return el( array('items',$item), $rights, RIGHTS_NO );
 	}
 	
@@ -804,13 +800,14 @@ class Flexy_auth extends Ion_auth {
    * @author Jan den Besten
    */
   public function rights_to_string($rights) {
-    $string = '';
+    if ($rights===FALSE) return '';
+    $string='';
     foreach ($this->_rights_settings as $number => $description) {
       if ( $rights-$number >=0 ) {
         $string = add_string($string,$description,'|');
       }
     }
-    return $string;
+    return add_string($string,'=','|');;
   }
 
 
