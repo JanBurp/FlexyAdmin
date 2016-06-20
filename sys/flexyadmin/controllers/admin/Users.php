e5d4b3c8b191179f8e11e28a2a3303fcd3e3afb8
Jan den Besten <info@jandenbesten.net>
2016-06-20 12:34:14 +0000

    - DATA: Bug: cfg_users was declared twice
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3703 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 6b1ba6a4bc9492c4a8c4a16301c23d0c5f89c74a
    Former-commit-id: ae05acb6b28327dc3ebb54209b3a8021b2331491
    Former-commit-id: f7bfe27b94e62bb375f7251c56b46b4f5db7c62b [formerly 24aa9e116c8a513be70d3cdab11ccad21dc4be22]
    Former-commit-id: accb3ab5159168e18b4637c6818cf26331070a7b

---
+++ sys/flexyadmin/controllers/admin/Users.php
@@ -0,0 +1,158 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once(APPPATH."core/AdminController.php");
+
+
+/**
+	* Verzorgt het activeren van gebruikers
+	*
+	* @author Jan den Besten
+	**/
+class Users extends AdminController {
+   
+   /**
+     */
+	public function __construct() {
+		parent::__construct();
+    $this->load->language('help');
+    $this->config->load('login');
+    if (file_exists(SITEPATH.'views/login/'.$this->language))
+      $this->config->set_item(array('ion_auth','email_templates'),'login/'.$this->language.'/');
+    elseif (file_exists(SITEPATH.'cfg_users/'.$this->language))
+      $this->config->set_item(array('ion_auth','email_templates'),'cfg_users/'.$this->language.'/');
+	}
+	
+  /**
+   * Weiger gebruiker(s)
+   *
+   * @param string $user_id 
+   * @return void
+   * @author Jan den Besten
+   */
+	public function deny($user_id=false) {
+    return $this->_do_action('deny',$user_id);
+	}
+
+  /**
+   * Accepteer gebruiker(s) en stuur de inlog
+   *
+   * @param string $user_id 
+   * @return void
+   * @author Jan den Besten
+   */
+	public function accept($user_id=false) {
+    return $this->_do_action('accept',$user_id);
+	}
+
+  /**
+   * Stuur een uitnodiging (eerste mail)
+   *
+   * @param string $user_id 
+   * @return void
+   * @author Jan den Besten
+   */
+  public function invite($user_id=false) {
+      return $this->_do_action('invite',$user_id);
+  }
+
+
+  /**
+   * Stuur gebruiker(s) nieuw wachtwoord
+   *
+   * @param string $user_id 
+   * @return void
+   * @author Jan den Besten
+   */
+  public function send_new_password($user_id=false) {
+    return $this->_do_action('send_new_password',$user_id);
+  }
+
+
+  private function _do_action($action,$user_id=false) {
+		if ($this->_can_activate_users()) {
+      if (!$user_id) {
+        $users_ids=$this->input->get('users');
+        if (!$users_ids) {
+          $users_ids=$this->_get_inactive_user_ids();
+          if (!$users_ids and $action=='send_new_password') {
+            $users_ids=$this->_get_inactive_user_ids(true);
+          } 
+        }
+      }
+      else {
+        $users_ids=array($user_id);
+      }
+      foreach ($users_ids as $user_id) {
+        $user = $this->flexy_auth->get_user($user_id);
+        $extra_emails=$this->_extra_emails($user_id);
+        switch ($action) {
+          case 'deny':
+      			$message='user_removed';
+      			$this->flexy_auth->user_denied_mail( $user_id );
+            $this->flexy_auth->delete_user($user_id);
+            break;
+          case 'accept':
+      			$message='user_accepted';
+      			$this->flexy_auth->user_accepted_mail( $user_id );
+      			$this->flexy_auth->activate_user($user_id);
+            break;
+          case 'invite':
+            $message='send_invitation';
+            $this->flexy_auth->send_new_account($user_id);
+            $this->flexy_auth->activate_user($user_id);
+            break;
+          case 'send_new_password':
+            $message='user_send_password';
+            if (! $this->flexy_auth->send_new_password($user_id)) {
+              $message='user_send_password_error';
+            }
+            break;
+        }
+        if (isset($message)) $this->message->add(langp($message,$user['username'].'('.$user['email'].','.$user['extra_email_string'].')'));
+      }
+		}
+    redirect(api_uri('API_view_grid','cfg_users'));
+  }
+	
+  
+  /**
+   * Geeft extra email van gebruiker
+   *
+   * @param string $user_id 
+   * @return string
+   * @author Jan den Besten
+   */
+  private function _extra_emails($user_id) {
+    $extra_emails='';
+    $table=$this->config->item('extra_email_table');
+    if (!empty($table)) {
+      $this->data->table( $table );
+      $fields = $this->data->list_fields();
+      $fields = filter_by($fields,'email');
+      $this->data->select($fields);
+      $this->data->where( 'id_user',$user_id );
+      $user = $this->data->get_row();
+      if ($user) $extra_emails=trim(implode(', ',$user),', ');
+    }
+    return $extra_emails;
+  }
+
+
+  private function _get_inactive_user_ids($active=false) {
+    $user_ids=array();
+    $users=$this->flexy_auth->get_users();
+    $this_user=$this->flexy_auth->user();
+    $this_user_id = $this_user->id;
+    foreach ($users as $user) {
+      if (($active and $user->id!=$this_user_id) or (!$user->b_active or empty($user->last_login))) $user_ids[$user->id]=$user->id;
+    }
+    return $user_ids;
+  }
+
+  private function _can_activate_users() {
+    return ($this->flexy_auth->allowed_to_edit_users());
+  }
+  
+}
+
+?>
