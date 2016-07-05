589f6a84a1b4f044afee7a675ee4b0da6fad0fdb
Jan den Besten <info@jandenbesten.net>
2016-07-05 12:26:24 +0000

    - DataOptions: table/fields alleen die waar rechten voor zijn
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3731 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 80bf4d96137e727dd2969f4cc58cd8cd2072d098
    Former-commit-id: 89ed2a65896226a2df0eabcbef1afbab8051629b
    Former-commit-id: 4ca0c92fb4bfbad79b52ccae5c48c67ec38c163e [formerly 547f3a109e08d1bb18487f38c2f5097029baa177]
    Former-commit-id: 2fc91cb432c252c4d5ee1def2564adf10f989d32

--- sys/flexyadmin/models/data/Options_Fields.php
+++ sys/flexyadmin/models/data/Options_Fields.php
@@ -18,15 +18,24 @@ Class Options_Fields extends CI_Model {
   public function get_options( $info=array() ) {
     $options=array();
     $tables = $this->data->list_tables();
+    if ( !$this->flexy_auth->is_super_admin() ) {
+      $tables=not_filter_by($tables,"log_");
+      $tables=not_filter_by($tables,"rel_users");
+    }
     foreach ($tables as $table) {
-      $table_fields=$this->db->list_fields($table);
-      // Speciale velden filter
-      if ( el('field',$info)==='fields_media_fields') {
-        $table_fields = filter_by($table_fields,'media');
-      }
-      // Maak er opties van
-      foreach ($table_fields as $value) {
-        $options[]=$table.'.'.$value;
+      if ($this->flexy_auth->has_rights($table)) {
+        $table_fields=$this->db->list_fields($table);
+        // Speciale velden filter
+        if ( el('field',$info)==='fields_media_fields') {
+          $table_fields = filter_by($table_fields,'media');
+        }
+        if ( $table==='cfg_users' ) {
+          $table_fields=array('str_username','email_email');
+        }
+        // Maak er opties van
+        foreach ($table_fields as $value) {
+          $options[]=$table.'.'.$value;
+        }
       }
     }
     array_unshift($options,'');
