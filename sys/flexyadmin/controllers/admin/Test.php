2bf4b49fd3ba2d3a6056e97e6a81c49287e3ea7a
Jan den Besten <info@jandenbesten.net>
2016-08-15 14:06:38 +0000

    - BUG: FlexyAuth was niet bekend bij __test
    git-svn-id: file:///Users/jan/Dropbox/svn/FlexyAdmin/trunk@3763 0c02b2c5-a8bd-4e35-85b2-cf8cacb96951
    
    Former-commit-id: 96368e6b019305dd76be7e56bbaa1d4600db2050
    Former-commit-id: 7eda46c9c48af2ce1f8d432393809b158dbe2712
    Former-commit-id: 81dabfe40de9955800a40ddc7c1cbfbbc2b171fa [formerly eb83dd03ae576c6d62b4b2d8a0c16788b02fc43a]
    Former-commit-id: 1444ddea9ad36b1a031241720d033cf437c82ad4

--- sys/flexyadmin/controllers/admin/Test.php
+++ sys/flexyadmin/controllers/admin/Test.php
@@ -1,193 +1,30 @@
-<?php require_once(APPPATH."core/MY_Controller.php");
+<?php require_once(APPPATH."core/AjaxController.php");
+
+/**
+ * Laad Main Angular
+ *
+ * @author Jan den Besten
+ */
 
 class Test extends MY_Controller {
 	
-	public function __construct()	{
+	public function __construct() {
 		parent::__construct();
-    $this->load->library('table');
     $this->load->library('flexy_auth');
-    
-    $template = array(
-      'table_open'            => '<table border="1" cellpadding="4" cellspacing="0">',
-      'thead_open'            => '<thead>',
-      'thead_close'           => '</thead>',
-      'heading_row_start'     => '<tr>',
-      'heading_row_end'       => '</tr>',
-      'heading_cell_start'    => '<th align="left">',
-      'heading_cell_end'      => '</th>',
-      'tbody_open'            => '<tbody>',
-      'tbody_close'           => '</tbody>',
-      'row_start'             => '<tr>',
-      'row_end'               => '</tr>',
-      'cell_start'            => '<td valign="top" align="left"><pre><code>',
-      'cell_end'              => '</code></pre></td>',
-      'row_alt_start'         => '<tr>',
-      'row_alt_end'           => '</tr>',
-      'cell_alt_start'        => '<td valign="top" align="left"><pre><code>',
-      'cell_alt_end'          => '</code></pre></td>',
-      'table_close'           => '</table>'
-    );
-    $this->table->set_template($template);
 	}
   
-  public function index() {
-    if (!IS_LOCALHOST) return;
-    
-    $this->data->table('res_menu_result');
-    $this->data->path('uri');
-    $menu = $this->data->get_result();
-    
-    trace_sql($this->data->last_query());
-    trace_($this->data->get_query_info());
-    trace_($menu);
-    
-  }
-
-
-  public function options() {
-    if (!IS_LOCALHOST) return;
-    
-    $tables = $this->data->list_tables();
-    $tables = array('cfg_admin_menu');
-    foreach ($tables as $table) {
-      $this->data->table( $table );
-      $options_settings = $this->data->get_setting('options');
-      if ($options_settings) {
-        echo h($table);
-        trace_( $this->data->get_setting('options') );
-        trace_( $this->data->get_options());
-      }
-    }
-    
-  }
-
-  
-  
-  public function users( $user_id=FALSE ) {
-    if (!IS_LOCALHOST) return;
-
-    $this->load->library('flexy_auth');
-    if ($user_id) {
-      $user = $this->flexy_auth->get_user($user_id);
-      $users[$user_id] = $user;
-    }
-    else {
-      $users = $this->flexy_auth->get_users();
-    }
-    
-    foreach ($users as $id=>$user) {
-      $user['groups.description']='';
-      foreach ( $user['groups'] as $group ) {
-        $user['groups.description'] = add_string( $user['groups.description'], $group['description'],' | ');
-      }
-      $user['rights.description']='<table>';
-      foreach ($user['rights']['items'] as $item=>$rights) {
-        $user['rights.description'] .= '<tr><td><b>'.$item.'<b></td><td>'.$this->flexy_auth->rights_to_string( $rights ). '</td></tr>';
-      }
-      $user['rights.description'] .= '</table>';
-      $users[$id] = array_keep_keys( $user, array('id','str_username','groups.description','rights.description'));
-    }
-
-    echo( '<h1>User rights</h1>' );
-    $this->table->set_heading( array('id','user_name','groups.description','rights') );
-    foreach ($users as $user) {
-      $this->table->add_row( $user );
-    }
-    echo $this->table->generate();
-  }
-  
-  
-  
-  public function relations() {
-    if (!IS_LOCALHOST) return;
-    
-    // many_to_one
-    $this->data->table( 'tbl_leerlingen' );
-    $many_to_one['without']  = "";
-    $many_to_one['normal']   = "->with( 'many_to_one' )";
-    $many_to_one['specific'] = "->with( 'many_to_one', ['id_adressen'=>['str_zipcode','str_city']] )";
-    $many_to_one['abstract'] = "->with( 'many_to_one', ['id_adressen'=>'abstract'] )";
-    $many_to_one['json']  = "->with_json( 'many_to_one', ['id_adressen'] )";
-    $many_to_one['flat']     = "->with_flat_many_to_one( ['id_adressen'] )";
-    $this->eval_table('many_to_one',$many_to_one);
-
-    // one_to_many
-    $this->data->table( 'tbl_adressen' );
-    $one_to_many['without']  = "";
-    $one_to_many['normal']   = "->with( 'one_to_many' )";
-    $one_to_many['specific'] = "->with( 'one_to_many', ['tbl_leerlingen'=>['str_first_name','str_last_name']] )";
-    $one_to_many['abstract'] = "->with( 'one_to_many', ['tbl_leerlingen'=>'abstract'] )";
-    $one_to_many['json']  = "->with_json( 'one_to_many', ['tbl_leerlingen'] )";
-    $this->eval_table('one_to_many',$one_to_many);
-
-    // many_to_many
-    $this->data->table( 'tbl_groepen' );
-    $many_to_many['without']  = "";
-    $many_to_many['normal']   = "->with( 'many_to_many' )";
-    $many_to_many['specific'] = "->with( 'many_to_many', ['rel_groepen__adressen'=>['str_zipcode']] )";
-    $many_to_many['abstract'] = "->with( 'many_to_many', ['rel_groepen__adressen'=>'abstract'] )";
-    $many_to_many['json']  = "->with_json( 'many_to_many', ['rel_groepen__adressen'] )";
-    $this->eval_table('many_to_many',$many_to_many);
-    
-  }
-  
-  
-  private function eval_table( $caption, $with ) {
-
-    $get=array();
-    $result=array();
-    $sql=array();
-    
-    foreach ($with as $key => $value) {
-      // With
-      $with[$key] = highlight_code( str_replace(')->',")\n->",$value) );
-
-      // Get
-      $eval = 'return $this->data'.$value.'->get( 2 );';
-      $query = eval($eval);
-      if ($query) {
-        $num_rows = $query->num_rows();
-        $array = array_slice( $query->result_array(),0,2 );
-        $get[$key] = 'num_rows = '.$num_rows.' (2)'.br().highlight_code(array2php( $array ));
-      }
-      else {
-        $get[$key] = span(array('style'=>'color:#F00;')).'Database ERROR:<br>'.$this->db->error()['message']._span();
-      }
-      
-      // Result
-      $eval = 'return $this->data'.$value.'->get_result( 2 );';
-      $array = eval($eval);
-      $num_rows = $this->data->num_rows();
-      $result[$key] = 'num_rows = '.$num_rows.' (2)'.br().highlight_code(array2php( array_slice($array,0,2) ));
-      // Query
-      $sql[$key] = highlight_code(nice_sql( $this->data->last_query() ));
-    }
-    array_unshift($with,"\n<strong>with()</strong>");
-    array_unshift($get,"<strong>get()->result_array()</strong>");
-    array_unshift($result,"<strong>get_result()</strong>");
-    array_unshift($sql,"\n<strong>->last_query()</strong>");
-    
-    echo( '<h1>'.$caption.'</h1>' );
-    $this->table->set_heading( array_keys($with) );
-    $this->table->add_row( $with );
-    $this->table->add_row( $sql );
-    $this->table->add_row( $get );
-    $this->table->add_row( $result );
-    echo $this->table->generate();
-  }
-  
-  
-
-  public function menu() {
-    $menu = $this->data->table('tbl_menu')
-                        ->unselect('txt_text')
-                        ->path('full_uri','uri')
-                        ->where_path( 'full_uri','een_pagina/een_pagina')
-                        ->get_result();
-    trace_($menu);
-    trace_($this->data->get_query_info());
-  }
-  
+  /**
+   * Main view
+   *
+   * @return void
+   * @author Jan den Besten
+   */
+	public function index() {
+    $site = $this->data->table('tbl_site')
+                              ->select('str_title,url_url')
+                              ->get_row();
+    $this->load->view('admin/__test/main_app',array('title'=>$site['str_title'],'language'=>$this->flexy_auth->get_user()['str_language']),false);
+	}
 
 }
 
