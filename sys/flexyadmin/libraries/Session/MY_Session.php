8d1301068da0ddec43940a098162094f38d7e403
Jan den Besten <info@jandenbesten.net>
2016-10-26 06:04:51 +0200

    CherryPick from feature/3.5 (Session bug)
    Former-commit-id: 0977399c604df051fa6342443544adb25fdcc89e
    Former-commit-id: 274b60f43a697ff7b41c271abd03c47b9198495f
    Former-commit-id: 59d49fbaf790dafaae2c9a185746d187a9405740 [formerly 5d22705f9b2568e688bcbd5d0c7d8df6ec7783f8]
    Former-commit-id: a5dcb3199dda960f559b8314ec0a9442cf0cd7cc

---
+++ sys/flexyadmin/libraries/Session/MY_Session.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Session extends CI_Session {
+
+	/**
+	 * Session destroy
+	 *
+	 * Legacy CI_Session compatibility method
+	 *
+	 * @return	void
+	 */
+	public function sess_destroy()
+	{
+    if (session_status() === PHP_SESSION_ACTIVE) parent::sess_destroy();
+	}
+
+
+}
