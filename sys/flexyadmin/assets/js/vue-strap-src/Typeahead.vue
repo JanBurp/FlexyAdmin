fe2f50d26ab954abbfc81d08841e8c4414deced4
Jan den Besten <info@jandenbesten.net>
2016-11-03 21:48:34 +0100

    Added vue-strap-src
    Former-commit-id: aa6498937a263050f5ffc3ae050cfba844971419
    Former-commit-id: 58c11ed582c028c0f4ced8e526b768123d462487
    Former-commit-id: a5cae04378eb0b43e6cea3620f1a9555a5e1d79b [formerly b068a17f735ae957057adb9308d7c2cb35e50bca]
    Former-commit-id: e5812c7227c904f7d8f6547a969a2ac8578acd88

---
+++ sys/flexyadmin/assets/js/vue-strap-src/Typeahead.vue
@@ -0,0 +1,139 @@
+<template>
+  <div style="position: relative" :class="{'open':showDropdown}">
+    <input type="text" class="form-control" autocomplete="off"
+      :placeholder="placeholder"
+      :value="value"
+      @blur="showDropdown = false"
+      @input="update"
+      @keydown.down="down"
+      @keydown.enter= "hit"
+      @keydown.esc="reset"
+      @keydown.up="up"
+    />
+    <ul class="dropdown-menu" ref="dropdown">
+      <li v-for="(item, i) in items" :class="{'active': isActive(i)}">
+        <a @mousedown.prevent="hit" @mousemove="setActive(i)">
+          <partial :name="templateName"></partial>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {getJSON} from './utils/utils.js'
+import {Vue} from 'vue'
+
+// let coerce = {
+//     matchCase: 'boolean',
+//     matchStart: 'boolean'
+// }
+
+// let Vue = window.Vue
+
+export default {
+  partials: {
+    default: '<span v-html="highlight(item,query)"></span>'
+  },
+  props: {
+    async: {type: String},
+    data: {type: Array},
+    key: {type: String, default: null},
+    limit: {type: Number, default: 8},
+    matchCase: {type: Boolean, default: false},
+    matchStart: {type: Boolean, default: false},
+    onHit: {
+      type: Function,
+      default (items) {
+        this.reset()
+        this.value = items
+      }
+    },
+    placeholder: {type: String},
+    template: {type: String},
+    templateName: {type: String, default: 'default'},
+    value: {type: String, default: ''}
+  },
+  data () {
+    return {
+      showDropdown: false,
+      noResults: true,
+      current: 0,
+      items: []
+    }
+  },
+  computed: {
+    primitiveData () {
+      if (this.data) {
+        return this.data.filter(value => {
+          value = this.matchCase ? value : value.toLowerCase()
+          var query = this.matchCase ? this.value : this.value.toLowerCase()
+          return this.matchStart ? value.indexOf(query) === 0 : value.indexOf(query) !== -1
+        }).slice(0, this.limit)
+      }
+    }
+  },
+  watch: {
+    value (val) {
+      this.$emit('input', val)
+    }
+  },
+  created () {
+    this.items = this.primitiveData
+  },
+  mounted () {
+    // register a partial:
+    /*if (this.templateName && this.templateName !== 'default') {
+      Vue.partial(this.templateName, this.template)
+    }*/
+  },
+  methods: {
+    highlight (value, phrase) { return value.replace(new RegExp('(' + phrase + ')', 'gi'), '<strong>$1</strong>') },
+    update (e) {
+      this.$emit('input', this.value, e)
+      if (!this.value) {
+        this.reset()
+        return false
+      }
+      if (this.data) {
+        this.items = this.primitiveData
+        this.showDropdown = this.items.length > 0
+      }
+      if (this.async) {
+        getJSON(this.async + this.value).then(data => {
+          this.items = (this.key ? data[this.key] : data).slice(0, this.limit)
+          this.showDropdown = this.items.length > 0
+        })
+      }
+    },
+    reset () {
+      this.items = []
+      this.value = ''
+      this.loading = false
+      this.showDropdown = false
+    },
+    setActive (index) {
+      this.current = index
+    },
+    isActive (index) {
+      return this.current === index
+    },
+    hit (e) {
+      e.preventDefault()
+      this.onHit(this.items[this.current], this)
+    },
+    up () {
+      if (this.current > 0) this.current--
+    },
+    down () {
+      if (this.current < this.items.length - 1) this.current++
+    }
+  }
+}
+</script>
+
+<style>
+.dropdown-menu > li > a {
+  cursor: pointer;
+}
+</style>
