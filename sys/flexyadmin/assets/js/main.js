c3d81be21cdd28f60ee25784b340b12e5b8b42fd
Jan den Besten <info@jandenbesten.net>
2016-11-07 17:13:22 +0100

    Global Vue methods for calling API
    Former-commit-id: d21f85870de1a396a18786558d709057db91dab5
    Former-commit-id: 54a70533128c2748903c197de24d4076d577402c
    Former-commit-id: f043d2f7447d5a19849313c1d3d8678bef5f7385 [formerly 8bb17a432bc506c4437d9e379081436dc7d151d0]
    Former-commit-id: cafc8f9e481f5c6edbfac37cc5c8ec108fd2aefa

--- sys/flexyadmin/assets/js/main.js
+++ sys/flexyadmin/assets/js/main.js
@@ -7,18 +7,89 @@
  * @author: Jan den Besten
  */
 
+var AUTH_TOKEN = _flexy.auth_token;
+
 var _ = require('lodash');
-
 import Vue              from 'vue'
-// import tab              from './vue-strap-src/components/Tab.vue'
-// import tabs             from './vue-strap-src/components/Tabs.vue'
-// import tabGroup         from './vue-strap-src/components/TabGroup.vue'
-
+import Axios            from 'axios'
 import FlexyBlocks      from './components/flexy-blocks.vue'
 import FlexyPagination  from './components/flexy-pagination.vue'
 import FlexyGrid        from './components/grid/flexy-grid.vue'
 import FlexyForm        from './components/form/flexy-form.vue'
 
+
+Vue.mixin({
+  methods: {
+    
+    /**
+     * Global method om Api aan te roepen. Options Object bevat de volgende properties:
+     * - url, de url van de api (auth,table,row, etc)
+     * - data, de mee te geven parameters
+     */
+    api : function(options) {
+      var self = this;
+      var method = 'GET';
+      if (options.url==='row' && !_.isUndefined(options.data.where)) method = 'POST';
+      var request = {
+        method  : 'POST',
+        url     : '_api/'+options.url,
+        data    : options.data,
+        headers : {
+          'Authorization': AUTH_TOKEN,
+          'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        transformRequest: [function (data) {
+          var requestString='';
+          if (data) {
+            requestString = self.flexySerializeJSON(data);
+          }
+          return requestString;
+        }],
+      };
+      console.log('api',request);
+      return Axios.request( request );
+    },
+    
+    /* Maakt normale POST data (string) van meegegeven Object */
+    flexySerializeJSON : function(data) {
+      var serializeString='';
+      if ( !_.isUndefined(data) ) {
+        // sort the keys, so the returned string has always same order of keys
+        var keys = Object.keys(data).sort();
+        // Loop the keys
+        for (var i = 0; i < keys.length; i++) {
+          var key=keys[i];
+          if (serializeString!=='') serializeString+='&';
+          // array
+          if (_.isArray(data[key])) {
+            data[key].forEach(function(el,index) {
+              if (serializeString!=='') serializeString+='&';
+              serializeString += encodeURIComponent(key) + '[]=' + encodeURIComponent(el);
+            });
+          }
+          // object
+          if (_.isObject(data[key])) {
+            _.forEach(data[key], function(el,index) {
+              if (serializeString!=='') serializeString+='&';
+              serializeString += encodeURIComponent(key) + '['+index+']=' + encodeURIComponent(el);
+            });
+          }
+          // normal
+          else {
+            serializeString += encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
+          }
+        }
+      }
+      return serializeString;
+    },
+    
+  },
+})
+
+
+/**
+ Main Vue Instance
+ */
 var vm = new Vue({
   el:'#main',
   components: {
@@ -28,3 +99,5 @@ var vm = new Vue({
     FlexyForm,
   },
 });
+
+
