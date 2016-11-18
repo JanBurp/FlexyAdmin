da89d417d275e80dfa2903ee4445e9e0ca33cd07
Jan den Besten <info@jandenbesten.net>
2016-11-18 11:01:26 +0100

    Cleanup
    Former-commit-id: 627dac34a343f75e80bbeb4dcb0f11ade3634e64 [formerly 1242f71bbf47a5265319dda056ba5bac34aef821] [formerly 42703ee5ee1916f07df07863b003570fd13b324e [formerly de7f358db99c56e22524eb8d29a7a2f4ce14bc52]] [formerly cced2e4df5529cb375512b537f7b6bbe6904d529 [formerly 59c253c81faa417e3b33bdeb7bcb40e72ae1400f] [formerly 25fbdacfaa1723848b1cad33793d3d0ca1350200 [formerly 2eea317efade0ef9c0a1e63ca70642f1984041d8]]]
    Former-commit-id: 09927ef68ed7ca55fef7b56db8954b965840292e [formerly c4663c454fa2e1753b698d201e9862da47f73100] [formerly cb4f8271c895f14ba2344fa95c56d53bde86dcd5 [formerly 58ea2abb3d59d7d1304ad173e9a536e99c2e0723]]
    Former-commit-id: 658bdc3b1c2ee6c6884f0ede6fd2c51e7af84eab [formerly ee4b9dae1c6d19423ca2cb63f27e4d094b52a224]
    Former-commit-id: 55e9738fef3dfac733d8548ec85dfe5a9dbbd533

--- plugins/add_log_cronjobs.sql
+++
@@ -1,8 +0,0 @@
-# This adds log_cronjobs, for simulating and calling cronjobs
-
-CREATE TABLE `log_cronjobs` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `str_job` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
-  `tme_last_run` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
