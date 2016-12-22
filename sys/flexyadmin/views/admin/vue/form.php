72c212ceb2ad287efa9d693c274cec8d541ad6b5
Jan den Besten <info@jandenbesten.net>
2016-12-22 12:26:55 +0100

    GRID edit -> not ready yet… needs concept
    Former-commit-id: 00b4a035bc4854c551895196db94fb9bcde78c68

--- sys/flexyadmin/views/admin/vue/form.php
+++ sys/flexyadmin/views/admin/vue/form.php
@@ -1,6 +1,7 @@
 <flexy-form
   title="<?=$title?>"
   name="<?=$name?>"
+  path="<?=$path?>"
   :primary="<?=$id?>"
   :fields='<?=array2json($fields)?>'
   :fieldsets='<?=htmlentities(array2json($fieldsets),ENT_QUOTES, 'UTF-8')?>'
