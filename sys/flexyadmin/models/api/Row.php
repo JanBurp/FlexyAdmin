c09cef815412336989183c0485ec768cdd7d3267
Jan den Besten <info@jandenbesten.net>
2017-01-08 08:52:10 +0100

    BUG: Api row eenvoudiger: geen check op dubbele data meer, dat doet DataCore al wel
    Former-commit-id: 584b0bfa670b4099c3c6823d9bb61658c6a21763

--- sys/flexyadmin/models/api/Row.php
+++ sys/flexyadmin/models/api/Row.php
@@ -271,18 +271,8 @@ class Row extends Api_Model {
   private function _update_row() {
     $args=$this->_clean_args(array('table','where','data'));
     $data=$this->args['data'];
+    $fields=array_keys($data);
     $this->data->table( $args['table'] );
-    $relations = $this->data->get_setting('relations');
-    // Alleen de velden die anders zijn dan huidige data
-    $this->data->select(array_keys($data))->where( $args['where'] );
-    if ($relations) {
-      foreach ($relations as $key => $info) {
-        $this->data->with($key);
-      }
-    }
-    $old_data = $this->data->get_row();
-    unset($old_data['id']);
-    $data = array_diff_multi($old_data,$data);
     if ($data) {
       // Save
       $this->data->set( $data );
