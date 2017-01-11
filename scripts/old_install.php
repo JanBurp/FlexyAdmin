552c59d8008eb78db14f875ef70501a05f75764b
Jan den Besten <jan@Laptop-van-Jan-2.local>
2017-01-11 16:00:12 +0100

    BUSY with old_install script

--- scripts/old_install.php
+++ scripts/old_install.php
@@ -1,18 +1,38 @@
 #!/usr/bin/php -q
 <?php
 
-echo "Old folder structer.\n\n";
+echo "Old folder structere.\n\n";
 
 // Check if it is the safe folder structure
-if (file_exists('.htaccess')) {
-  echo ".htaccess exists, so it is allready the old folder structure!\n";
+if (file_exists('index.php')) {
+  echo "index.php exists, so it is allready the old folder structure!\n";
   die();
 }
 
-echo "Move files from `public` to root\n";
+echo "Move files from `public` to root\n\n";
+
+$path = 'public';
+$files = scandir($path);
+foreach ($files as $file) {
+  
+  if (is_file($path.'/'.$file)) {
+    // Just move the file
+    if (rename($path.'/'.$file, $file)) {
+      echo "- moved $file\n";
+    }
+    else {
+      echo "ERROR while moving $file\n";
+    }
+  }
+  else {
+    if (is_dir($path.'/'.$file) and $file=='assets') {
+      // Merge assets folders
+      echo "- merged Assets folders\n";
+    }
+  }
+}
 
 
-// echo "Merge `public/assets` to `site/assets`\n";
 // echo "Use htaccess.htaccess\n";
 // echo "Set `SAFE_INSTALL` in `index.php[83]` to `FALSE`\n";
 // echo "Change `var assets` in `gulpfile.js[50]`\n";
