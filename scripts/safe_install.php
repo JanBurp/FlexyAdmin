44a7d4cccef7a4da43b3aebad66d2cacabf860d4
Jan den Besten <jan@LaptopvanJan2.fritz.box>
2017-01-12 06:23:02 +0100

    Safe install script
    Former-commit-id: 4c923557855788fe60bb6c194646db68a4045726

---
+++ scripts/safe_install.php
@@ -0,0 +1,71 @@
+<?php
+if (!isset($_SERVER['SHELL'])) die('Only available from Terminal.');
+echo "\e[4;32mFlexyAdmin\n\n\e[0m";
+
+echo "Safe folder structure.\n\n";
+
+// Check if it is the safe folder structure
+if (!file_exists('index.php')) {
+  echo "ERROR `index.php` does not exists, so it is allready the safe folder structure!\n\n";
+}
+else {
+  $path = 'public';
+  $root_files = array(
+    '.htaccess',
+    'htaccess.htaccess',
+    'htaccess_safe.htaccess',
+    'temp.htaccess',
+    'index_temp.html',
+    'index.php',
+    'robots.txt',
+    'sitemap.xml',
+  );
+  $public_assets = array(
+    'css',
+    'fonts',
+    'js',
+    'less-bootstrap',
+    'less-default',
+  );
+  
+  $files = scandir($path);
+  foreach ($files as $file) {
+
+    if (is_file($path.'/'.$file)) {
+      // Just move the file
+      if (rename($path.'/'.$file, $file)) {
+        echo "- moved $file\n";
+      }
+      else {
+        echo "ERROR while moving $file\n";
+      }
+    }
+    else {
+      if (is_dir($path.'/'.$file) and $file=='assets') {
+        // Merge assets folders
+        if (rename($path.'/'.$file, $file)) {
+          echo "- Merged `assets` folders: $file\n";
+        }
+        else {
+          echo "ERROR while merging `assets` folders.\n";
+        }
+      }
+    }
+  }
+}
+
+if ( copy('htaccess.htaccess','.htaccess') ) {
+  echo "- `.htaccess` changed\n";
+}
+
+$index = file_get_contents('index.php');
+$index = preg_replace( "/\bdefine\(\'SAFE_INSTALL\', true\)/u","define('SAFE_INSTALL', false)", $index);
+file_put_contents('index.php',$index);
+echo "- Set `SAFE_INSTALL` in `index.php` to `FALSE`\n";
+
+$gulp = file_get_contents('gulpfile.js');
+$gulp = preg_replace("/\bvar assets\s=\s\'public\/assets\'/u", "var assets = 'site/assets'", $gulp);
+file_put_contents('gulpfile.js',$gulp);
+echo "- Set `assets` in `gulpfile.js` to `site/assets`\n";
+  
+?>
