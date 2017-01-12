277a57f4a58a800bc26bd2e5c5697b01c1d63c55
Jan den Besten <jan@LaptopvanJan2.fritz.box>
2017-01-12 06:49:40 +0100

    Install scripts
    Former-commit-id: 444f3efad0f202dfc0719be6da67c28bbe4fe3e0

--- scripts/old_install.php
+++ scripts/old_install.php
@@ -9,13 +9,14 @@ if (file_exists('index.php')) {
   echo "ERROR `index.php` exists, so it is allready the old folder structure!\n\n";
 }
 else {
-  $path = 'public';
-  $files = scandir($path);
+  $public = 'public';
+  $site = 'site';
+  $files = scandir($public);
   foreach ($files as $file) {
 
-    if (is_file($path.'/'.$file)) {
+    if (is_file($public.'/'.$file)) {
       // Just move the file
-      if (rename($path.'/'.$file, $file)) {
+      if (rename($public.'/'.$file, $file)) {
         echo "- moved $file\n";
       }
       else {
@@ -23,31 +24,56 @@ else {
       }
     }
     else {
-      if (is_dir($path.'/'.$file) and $file=='assets') {
+      if (is_dir($public.'/'.$file) and $file=='assets') {
         // Merge assets folders
-        if (rename($path.'/'.$file, $file)) {
-          echo "- Merged `assets` folders: $file\n";
-        }
-        else {
-          echo "ERROR while merging `assets` folders.\n";
-        }
+        recurse_copy($public.'/'.$file, $site.'/'.$file);
+        recurse_rmdir($public.'/'.$file);
+        echo "- Merged `assets` folders: $file\n";
       }
     }
   }
 }
 
+// Choose right .htaccess
 if ( copy('htaccess.htaccess','.htaccess') ) {
   echo "- `.htaccess` changed\n";
 }
 
+// Set SAFE_INSTALL in index.php
 $index = file_get_contents('index.php');
 $index = preg_replace( "/\bdefine\(\'SAFE_INSTALL\', true\)/u","define('SAFE_INSTALL', false)", $index);
 file_put_contents('index.php',$index);
 echo "- Set `SAFE_INSTALL` in `index.php` to `FALSE`\n";
 
+// Set asstes in gulpfile.js
 $gulp = file_get_contents('gulpfile.js');
 $gulp = preg_replace("/\bvar assets\s=\s\'public\/assets\'/u", "var assets = 'site/assets'", $gulp);
 file_put_contents('gulpfile.js',$gulp);
 echo "- Set `assets` in `gulpfile.js` to `site/assets`\n";
+
+
+function recurse_copy($src,$dst) { 
+  $dir = opendir($src);
+  if (!file_exists($dst)) @mkdir($dst); 
+  while(false !== ( $file = readdir($dir)) ) { 
+    if (( $file != '.' ) && ( $file != '..' )) { 
+      if ( is_dir($src . '/' . $file) ) { 
+        recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+      } 
+      else { 
+        copy($src . '/' . $file,$dst . '/' . $file); 
+      } 
+    } 
+  } 
+  closedir($dir);
+}
+
+function recurse_rmdir($dir) { 
+  $files = array_diff(scandir($dir), array('.','..')); 
+  foreach ($files as $file) { 
+    (is_dir("$dir/$file")) ? recurse_rmdir("$dir/$file") : unlink("$dir/$file"); 
+  }
+  return rmdir($dir); 
+} 
   
 ?>
