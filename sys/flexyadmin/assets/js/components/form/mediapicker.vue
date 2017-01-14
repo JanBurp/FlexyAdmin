c30deb4526c2246c0e1b50a650341b760823a111
Jan den Besten <info@jandenbesten.net>
2017-01-14 18:03:23 +0100

    Dragging media in mediapicker
    Former-commit-id: d38b8553dece6b8b7cec7d8b174d879d6d3bc820

--- sys/flexyadmin/assets/js/components/form/mediapicker.vue
+++ sys/flexyadmin/assets/js/components/form/mediapicker.vue
@@ -1,17 +1,34 @@
 <script>
 
+import draggable        from 'vuedraggable'
+
 import flexyButton      from '../flexy-button.vue'
 import flexyThumb       from '../flexy-thumb.vue'
 
 export default {
   name: 'MediaPicker',
-  components: {flexyButton,flexyThumb},
+  components: {draggable,flexyButton,flexyThumb},
   props:{
     'value'   : String,
     'name'    : String,
     'path'    : String,
   },
   computed : {
+    
+    draggableOptions : function() {
+      return {
+        // group         : { name:'tree', pull:true},
+        draggable     : 'div',
+        forceFallback : true,
+        // handle        : '.draggable-handle',
+        // scroll        : true,
+        // scrollFn      : function(offsetX, offsetY, originalEvent) {
+        //   console.log(offsetY);
+        //
+        // },
+      }
+    },
+    
   },
   
   data : function() {
@@ -27,11 +44,22 @@ export default {
       var array = media.split('|');
       for (var i = 0; i < array.length; i++) {
         array[i] = {
-          src : '_media/thumb/' + this.path +'/'+ array[i],
-          alt : array[i],
+          value : array[i],
+          src   : '_media/thumb/' + this.path +'/'+ array[i],
+          alt   : array[i],
         }
       }
       return array;
+    },
+    
+    dragEnd : function(event) {
+      var oldIndex = event.oldIndex;
+      var newIndex = event.newIndex;
+      var currentMedia = this.media.split('|');
+      var newMedia = _.clone(currentMedia);
+      newMedia.splice(newIndex, 0, newMedia.splice(oldIndex,1)[0] );
+      newMedia = _.join(newMedia,'|');
+      this.changeMedia(newMedia);
     },
     
     changeMedia : function(media) {
@@ -47,7 +75,13 @@ export default {
 <template>
   <div class="mediapicker">
     <flexy-button icon="plus" class="btn-outline-warning" size="xlg" />
-    <flexy-thumb v-for="img in thumbs()" size="lg" :src="img.src" :alt="img.alt" />
+    <draggable :list="thumbs()" :options="draggableOptions" @end="dragEnd($event)">
+      <flexy-thumb v-for="img in thumbs()" size="lg" :src="img.src" :alt="img.alt" :value="img.value" />
+    </draggable>
   </div>
 </template>
 
+<style>
+  .mediapicker .flexy-button {float:left;margin-right:.5rem;}
+  .mediapicker .flexy-thumb {cursor:move;}
+</style>
