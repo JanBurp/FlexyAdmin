96898be9b6c0e1722b0b8e42ce4039f1f73b7997
Jan den Besten <info@jandenbesten.net>
2017-01-31 21:33:06 +0100

    Removed GoogleAnalytics

--- site/views/site.php
+++ site/views/site.php
@@ -39,12 +39,6 @@
 <?php endif ?>
   <!--[if lte IE 8]><style type="text/css" media="screen">@import url(<?=$publicassets;?>css/ie8.css);</style><![endif]-->
   <!--[if IE 9]><style type="text/css" media="screen">@import url(<?=$publicassets;?>css/ie9.css);</style><![endif]-->
-  <?php if (isset($str_google_analytics) and !empty($str_google_analytics)): ?><script type="text/javascript">
-  // Google Analytics
-  var _gaq = _gaq || [];_gaq.push(['_setAccount', '<?=$str_google_analytics?>']);_gaq.push(['_trackPageview']);(function() {
-  	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-  </script><?php endif ?>
 </head>
 
 <body class="<?=$class?>">
