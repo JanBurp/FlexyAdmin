5c795082f55e8977052f81289e1e8f1c00a017ce
Jan den Besten <info@jandenbesten.net>
2017-02-06 15:27:56 +0100

    Required fields in schemaform & standard tables
    Former-commit-id: a124a5125a6daa04233f5838f0457a4180fe568e

---
+++ site/config/data/tbl_site.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['table'] = 'tbl_site';
+
+$config['field_info']['str_title']['validation']   = 'required';
+$config['field_info']['str_author']['validation']  = 'required';
+$config['field_info']['url_url']['validation']     = 'required';
+$config['field_info']['email_email']['validation'] = 'required';
