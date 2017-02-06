10c1a7c4856fafed3f33525d292c37dc5f14200f
Jan den Besten <info@jandenbesten.net>
2017-02-06 15:34:08 +0100

    Users all have ‘small’ as file view
    Former-commit-id: d9dff3002c7876b893dd1f5133d3a3dca2e96a41

--- db/flexyadmin_unittest_v3.5.0.sql
+++ db/flexyadmin_unittest_v3.5.0.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.6.33)
 # Database: flexyadmin_test
-# Generation Time: 2017-01-31 20:42:42 +0000
+# Generation Time: 2017-02-06 14:33:20 +0000
 # ************************************************************
 
 
@@ -147,7 +147,7 @@ CREATE TABLE `cfg_users` (
   `last_login` int(11) unsigned NOT NULL,
   `b_active` tinyint(1) unsigned DEFAULT '1',
   `str_language` char(3) NOT NULL DEFAULT 'nl',
-  `str_filemanager_view` varchar(10) NOT NULL DEFAULT '',
+  `str_filemanager_view` varchar(10) NOT NULL DEFAULT 'small',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -156,9 +156,9 @@ LOCK TABLES `cfg_users` WRITE;
 
 INSERT INTO `cfg_users` (`id`, `str_username`, `gpw_password`, `email_email`, `ip_address`, `salt`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `b_active`, `str_language`, `str_filemanager_view`)
 VALUES
-	(1,'admin','$2y$08$rKzbNchBsinmWva3UnPsZOMKbaTFdpOgjxNv.PYKn1BjWgrOznhZa','info@flexyadmin.com','','','','',0,'',0,1485895335,1,'nl','list'),
-	(2,'user','$2y$08$.18vvqlz24ldRDJ4AcnPR.AVYFBGOv9YbnvEw/dLRfn88KBd2E/iG','jan@burp.nl','','','','0',0,'',0,1485895335,1,'nl','list'),
-	(3,'test','$2y$08$OfDssFUdFL3mqwzlg4mFJeDrmwCRrzc.9sEQj0uVbM7MRxTpX/pZC','test@flexyadmin.com','',NULL,NULL,NULL,0,NULL,0,1485895335,1,'nl','');
+	(1,'admin','$2y$08$rKzbNchBsinmWva3UnPsZOMKbaTFdpOgjxNv.PYKn1BjWgrOznhZa','info@flexyadmin.com','','','','',0,'',0,1485895335,1,'nl','small'),
+	(2,'user','$2y$08$.18vvqlz24ldRDJ4AcnPR.AVYFBGOv9YbnvEw/dLRfn88KBd2E/iG','jan@burp.nl','','','','0',0,'',0,1485895335,1,'nl','small'),
+	(3,'test','$2y$08$OfDssFUdFL3mqwzlg4mFJeDrmwCRrzc.9sEQj0uVbM7MRxTpX/pZC','test@flexyadmin.com','',NULL,NULL,NULL,0,NULL,0,1485895335,1,'nl','small');
 
 /*!40000 ALTER TABLE `cfg_users` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -1533,7 +1533,7 @@ VALUES
 	(1,0,0,'gelukt','Gelukt!','<p>Als je dit ziet is het je gelukt om FlexyAdmin te installeren en werkend te krijgen. <br />Je hebt nu een standaard-installatie van een zeer eenvoudige basis-site.</p>\n<h2>Hoe verder</h2>\n<ul>\n<li>Pas de HTML aan in de map <em>site/views</em>. <em>site.php</em> is de basis view van je site en <em>page.php</em> de afzonderlijke pagina\'s.</li>\n<li>Pas de Stylesheets aan. Deze vindt je in de map <em>site/assets/css</em>.</li>\n<li>Handiger is om <a href=\"http://lesscss.org/\" target=\"_blank\">LESS</a> te gebruiken.</li>\n</ul>\n<h2>LESS</h2>\n<p>FlexyAdmin ondersteund <a href=\"http://lesscss.org/\" target=\"_blank\">LESS</a> in combinatie met een Gulp die het compileren verzorgd.</p>\n<ul>\n<li>Je vindt de <a href=\"http://lesscss.org/\" target=\"_blank\">LESS</a> bestanden voor de standaard template in <em>site/assets/less-default.</em></li>\n<li>Om <a href=\"http://lesscss.org/\" target=\"_blank\">LESS</a> te compileren tot CSS heeft FlexyAdmin een handige Gulpfile, zie hierna.<em><br /></em></li>\n</ul>\n<h2>Gulp</h2>\n<p>Als je gebruikt maakt van LESS heb je een compiler nodig om LESS om te zetten in CSS. FlexyAdmin maakt daarvoor gebruik van <a href=\"http://gulpjs.com/\" target=\"_blank\">Gulp</a>.<br />Gulp is een zogenaamde \'taskmanager\' en verzorgt automatisch een aantal taken. De bij FlexyAdmin geleverde Gulpfile verzorgt deze taken voor LESS en CSS:</p>\n<ul>\n<li>Compileren van LESS naar CSS</li>\n<li>Samenvoegen van alle CSS bestanden tot &eacute;&eacute;n CSS bestand</li>\n<li>Automatisch prefixen van CSS regels voor diverse browser (moz-, o-, webkit- e.d.)</li>\n<li>Rem units omzetten naar px units zodat browser die geen rem units kennen terugvallen op px (met name IE8)</li>\n<li>Minificeren van het CSS bestand.</li>\n</ul>\n<p>En deze taken voor Javascript:</p>\n<ul>\n<li>Javascript testen op veel voorkomende fouten met <a href=\"http://jshint.com/\" target=\"_blank\">JSHint</a></li>\n<li>Alle Javascript bestanden samenvoegen tot &eacute;&eacute;n bestand en deze minificeren.</li>\n</ul>\n<h2>Bower</h2>\n<p>Naast Gulp wordt FlexyAdmin ook geleverd met <a href=\"http://bower.io/\" target=\"_blank\">Bower</a>. Daarmee kun je je al je externe plugins handig installeren en updaten (zoals jQuery en Bootstrap).</p>\n<h2>gulpfile.js</h2>\n<p>Hoe je Gulp en Bower aan de praat kunt krijgen en welke gulp commando\'s er allemaal zijn lees je aan het begin van de gulpfile in de root: <em>gulpfile.js</em></p>\n<h2>Bootstrap</h2>\n<p>In plaats van het standaard minimale template kun je ook gebruik maken van <a href=\"http://getbootstrap.com/\">Bootstrap:</a></p>\n<ul>\n<li>Je vindt de Bootstrap bestanden in <em>site/assets/less-bootstrap</em></li>\n<li>Stel in <em>site/config/config.php:</em> <code>$config[\'framework\']=\'bootstrap\';</code></li>\n<li>Stel in <em>gulpfile.js: </em><code>var framework = \'bootstrap\';</code></li>\n<li>Bootstrap kun je alleen gebruiken in combinatie met LESS en Gulp.</li>\n</ul>','',1,'','',''),
 	(2,1,0,'een_pagina','Een pagina','','test_01.jpg',1,'','',''),
 	(3,2,2,'subpagina','Subpagina','<p>Een subpagina</p>','test_02.jpg|test_01.jpg',1,'','',''),
-	(5,3,2,'een_pagina','Nog een subpagina','<p>Met plaatje:</p><p><img src="_media/pictures/test_01.jpg" alt="test_01" /></p>','',1,'example','',''),
+	(5,3,2,'een_pagina','Nog een subpagina','<p>Met plaatje:</p><p><img src=\"_media/pictures/test_01.jpg\" alt=\"test_01\" /></p>','',1,'example','',''),
 	(4,4,0,'contact','Contact','<p>Hier een voorbeeld van een eenvoudig <a href=\"mailto:info@flexyadmin.com\">contactformulier</a>.</p>','',1,'forms.contact','','');
 
 /*!40000 ALTER TABLE `tbl_menu` ENABLE KEYS */;
