6c97a45f2622010d6f330ab4f77784fe5deea55c
Jan den Besten <info@jandenbesten.net>
2017-02-06 15:34:08 +0100

    Users all have ‘small’ as file view

--- db/update_v3.5.0_alpha_6.sql
+++ db/update_v3.5.0_alpha_6.sql
@@ -2,3 +2,4 @@
 UPDATE `cfg_configurations` SET `str_revision` = '3.5.0';
 
 ALTER TABLE `cfg_users` CHANGE `str_filemanager_view` `str_filemanager_view` VARCHAR(10)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT 'small';
+UPDATE `cfg_users` SET `str_filemanager_view`='small' WHERE `str_filemanager_view`='';
