48adfdc09a21c45f64a37f438fd9e200046b2f92
Jan den Besten <info@jandenbesten.net>
2017-02-13 12:45:06 +0100

    Plugin_uri

--- sys/flexyadmin/config/plugins/plugin_uri.php
+++ sys/flexyadmin/config/plugins/plugin_uri.php
@@ -10,31 +10,6 @@
 */
 
 $config['admin_api_method'] = '_admin_api';
-$config['after_update_method'] = '_after_update';
-
-
-/*
-|--------------------------------------------------------------------------
-| Plugin Update/Delete Triggers
-| Here you need to set when the update and delete methods of you're plugin are called
-|--------------------------------------------------------------------------
-|
-*/
-
-$config['trigger'] = array(
-	'fields'					=> array('uri')
-);
-
-
-/*
-|--------------------------------------------------------------------------
-| Prefix per table
-|--------------------------------------------------------------------------
-*/
-
-$config['prefix'] = array(
-  // 'tbl_menu'  => 'menu_'
-)
 
 
 ?>
