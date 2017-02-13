18a0b340a6df6ad40eaf79345054f60564dc5f2c
Jan den Besten <info@jandenbesten.net>
2017-02-13 18:15:58 +0100

    Betere LinkList

--- sys/flexyadmin/models/api/Get_link_list.php
+++ sys/flexyadmin/models/api/Get_link_list.php
@@ -22,19 +22,35 @@ class Get_link_list extends Api_Model {
     $site_links = $this->data->table('tbl_site')->select('str_title,url_url,email_email')->cache()->get_row();
     
     $links = array(
-      array( 'title'=>$site_links['str_title'].' ('.str_replace(array('http://','https://'),'',$site_links['url_url']).')',   'value' => $site_links['url_url'] ),
-      array( 'title'=>$site_links['email_email']. ' ('.$site_links['email_email'].')', 'value' => 'mailto:'.$site_links['email_email'] ),
-      
-      array( 'title'=>'Menu',        'menu' => $this->_menu() ),
+      array( 'title'=>'Algemeen',    'menu' => array(
+        array( 'title'=>$site_links['str_title'].' ('.str_replace(array('http://','https://'),'',$site_links['url_url']).')',   'value' => $site_links['url_url'] ),
+        array( 'title'=>$site_links['email_email']. ' ('.$site_links['email_email'].')', 'value' => 'mailto:'.$site_links['email_email'] ),
+      )),
+      array( 'title'=>'Pagina\'s',   'menu' => $this->_menu() ),
       array( 'title'=>'Links',       'menu' => $this->_links() ),
       array( 'title'=>'Downloads',   'menu' => $this->_downloads() ),
     );
+    header("Content-Type: application/json");
     return json_encode($links);
   }
   
   private function _menu() {
     $this->data->table('tbl_menu')->select('uri,str_title AS title,order,self_parent')->tree('uri');
     $result = $this->data->cache()->get_result();
+    $parents = array();
+    $level = 0;
+    foreach ($result as $key => $item) {
+      if ($item['self_parent']!==0) {
+        if (isset($parents[$item['self_parent']])) {
+          $level = $parents[$item['self_parent']];
+        }
+        else {
+          $level++;
+          $parents[$item['self_parent']] = $level;
+        }
+      }
+      $result[$key]['title'] = str_repeat("-",$level-1) . $item['title'];
+    }
     return $this->_result_as_links($result);
   }
   
