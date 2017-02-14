cdb643d71963e74a0bb39e8ba97d4a6fab75f6e1
Jan den Besten <info@jandenbesten.net>
2017-02-14 16:06:18 +0100

    Vergeten wachtwoord procedure bij inlog
    Former-commit-id: ac9cc2843244def33cc8d8c6a92b1a98f495cd2d

--- sys/flexyadmin/language/nl/login_lang.php
+++ sys/flexyadmin/language/nl/login_lang.php
@@ -5,4 +5,14 @@ $lang['login_password'] = "Wachtwoord";
 $lang['login_submit']   = "Inloggen";
 $lang['login_error']    = "Onbekende gebruiker!";
 
+$lang['login_forgot']            = "Wachtwoord vergeten?";
+$lang['login_login']             = "Terug naar inlogscherm";
+$lang['login_email']             = "Uw email-adres";
+$lang['login_send_new_password'] = "Stuur nieuw wachtwoord";
+
+$lang['login_forgot_mail_send'] = "Een email is onderweg.";
+$lang['login_forgot_password_send'] = "Nieuw wachtwoord is onderweg.";
+
+
+
 ?>
