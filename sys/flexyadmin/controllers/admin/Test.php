514fded1e55b7a82ff2685745f63835fcd84b1a8
Jan den Besten <info@jandenbesten.net>
2017-02-14 16:05:58 +0100

    Some small bugs
    - data->where_path() -> where_tree()
    - typos in flexy_auth
    
    
    Former-commit-id: 3ca689a91e2b08fe1e166a26a40b2cd956399f27

--- sys/flexyadmin/controllers/admin/Test.php
+++ sys/flexyadmin/controllers/admin/Test.php
@@ -208,7 +208,7 @@ class Test extends MY_Controller {
     $menu = $this->data->table('tbl_menu')
                         ->unselect('txt_text')
                         ->tree('full_uri','uri')
-                        ->where_path( 'full_uri','een_pagina/een_pagina')
+                        ->where_tree( 'full_uri','een_pagina/een_pagina')
                         ->get_result();
     trace_($menu);
     trace_($this->data->get_query_info());
