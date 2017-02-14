a5b60c70888bb59db0a4dfc6c2053eaa28403368
Jan den Besten <info@jandenbesten.net>
2017-02-14 16:45:25 +0100

    TinyMCE img_list nu met verdeling in vandaag, afgelopen week, op datum, op alfabet
    Former-commit-id: 99ecd8899f600d6ca47e2571c2ad28dcc259bc46

--- sys/flexyadmin/models/api/Get_image_list.php
+++ sys/flexyadmin/models/api/Get_image_list.php
@@ -19,17 +19,37 @@ class Get_image_list extends Api_Model {
   public function index() {
     if (!$this->logged_in()) return $this->_result_status401();
 
-    // $images = $this->_folder('pictures');
-    $images = array(
-      array( 'title'=>'Afbeeldingen',  'menu' => $this->_folder('pictures') ),
-    );
+    // $images = array(
+    //   array( 'title'=>'Afbeeldingen',  'menu' => $this->_folder('pictures') ),
+    // );
+    $images = $this->_images( 'pictures' );
     header("Content-Type: application/json");
     return json_encode($images);
   }
   
-  private function _folder($path) {
+  private function _images($path) {
+    $filters = array(
+      array( 'title' => 'Vandaag toegevoegd',         'order' => 'date', 'filter' => ' `date` > SUBDATE( CURDATE(), INTERVAL 1 DAY ) ' ),
+      array( 'title' => 'Afgelopen week toegevoegd',  'order' => 'date', 'filter' => ' `date` > SUBDATE( CURDATE(), INTERVAL 7 DAY ) AND `date` < SUBDATE( CURDATE(), INTERVAL 1 DAY )' ),
+      array( 'title' => 'Op datum',                   'order' => 'date', 'filter' => '' ),
+      array( 'title' => 'Op alfabet',                 'order' => 'alt',  'filter' => '' ),
+    );
+    
+    $images = array();
+    foreach ($filters as $filter) {
+      $items = $this->_folder($path,$filter['filter'],$filter['order']);
+      if ($items) {
+        $images[] = array( 'title'=>$filter['title'], 'menu'=>$items );
+      }
+    }
+    return $images;
+  }
+  
+  private function _folder($path,$filter='',$order='') {
     $this->data->table('res_assets')->select('CONCAT_WS("/","_media/'.$path.'",`file`) AS link, CONCAT(`alt`," (",`file`,")") AS title')->where('path',$path);
-    $result = $this->data->cache()->get_result();
+    if ($filter) $this->data->where($filter);
+    if ($order) $this->data->order_by($order);
+    $result = $this->data->get_result();
     return $this->_result_as_links($result);
   }
   
