ac9cc2843244def33cc8d8c6a92b1a98f495cd2d
Jan den Besten <info@jandenbesten.net>
2017-02-14 16:06:18 +0100

    Vergeten wachtwoord procedure bij inlog

--- sys/flexyadmin/assets/js/main.js
+++ sys/flexyadmin/assets/js/main.js
@@ -9,6 +9,7 @@
 
 var _ = require('lodash');
 
+
 import Vue              from 'vue'
 import Lang             from 'vue-lang'
 
@@ -23,52 +24,79 @@ import FlexyPagination  from './components/flexy-pagination.vue'
 import FlexyGrid        from './components/grid/flexy-grid.vue'
 import FlexyForm        from './components/form/flexy-form.vue'
 
+if ( !_.isUndefined(_flexy.auth_token) ) {
 
-// TinyMCE Global & Set extra
-_flexy.tinymceOptions = JSON.parse(_flexy.tinymceOptions);
-_flexy.tinymceOptions['link_list'] = '_api/get_link_list?_authorization='+_flexy.auth_token,
-_flexy.tinymceOptions['image_list'] = '_api/get_image_list?_authorization='+_flexy.auth_token,
-tinymce.init( _flexy.tinymceOptions );
+  // TinyMCE Global & Set extra
+  _flexy.tinymceOptions = JSON.parse(_flexy.tinymceOptions);
+  _flexy.tinymceOptions['link_list'] = '_api/get_link_list?_authorization='+_flexy.auth_token,
+  _flexy.tinymceOptions['image_list'] = '_api/get_image_list?_authorization='+_flexy.auth_token,
+  tinymce.init( _flexy.tinymceOptions );
 
-// Language settings
-const LOCALES = {};
-// _flexy.language_keys = JSON.parse(_flexy.language_keys);
-LOCALES['lang'] = _flexy.language;
-LOCALES[_flexy.language] = _flexy.language_keys;
-window.VueStrapLang = function() { return LOCALES[_flexy.language]['strap_lang']; }
-Vue.use(Lang, {lang: _flexy.language, locales: LOCALES});
+  // Language settings
+  const LOCALES = {};
+  // _flexy.language_keys = JSON.parse(_flexy.language_keys);
+  LOCALES['lang'] = _flexy.language;
+  LOCALES[_flexy.language] = _flexy.language_keys;
+  window.VueStrapLang = function() { return LOCALES[_flexy.language]['strap_lang']; }
+  Vue.use(Lang, {lang: _flexy.language, locales: LOCALES});
 
+  /**
+     Global Vue registering
+   */
+  Vue.mixin({
+    data : function() {
+      return {
+        state : flexyState.state,
+      }
+    },
+  });
 
-
-/**
-   Global Vue registering
- */
-Vue.mixin({
-  data : function() {
-    return {
+  /**
+     Main Vue Instance
+   */
+  var vm = new Vue({
+    el:'#main',
+    components: {
+      FlexyBlocks,
+      FlexyButton,
+      FlexyModal,
+      FlexyMessages,
+      FlexyPagination,
+      FlexyGrid,
+      FlexyForm,
+      FlexyHelp
+    },
+    data : {
       state : flexyState.state,
-    }
-  },
-});
+    },
+  });
 
-/**
-   Main Vue Instance
- */
-var vm = new Vue({
-  el:'#main',
-  components: {
-    FlexyBlocks,
-    FlexyButton,
-    FlexyModal,
-    FlexyMessages,
-    FlexyPagination,
-    FlexyGrid,
-    FlexyForm,
-    FlexyHelp
-  },
-  data : {
-    state : flexyState.state,
-  },
-});
+}
 
+// LOGIN
+else {
+  
+  /**
+     Login Vue Instance
+   */
+  var vm = new Vue({
+    el:'#main',
+    components: {},
+    data : {
+      message                 : _flexy.message,
+      forgottenPasswordDialog : false,
+    },
+    methods : {
+      
+      showForgottenPasswordDialog : function(show) {
+        this.message = '';
+        this.forgottenPasswordDialog = show;
+      },
+      
+    },
+    
+  });
+  
+  
+}
 
