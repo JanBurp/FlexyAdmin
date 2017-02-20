15e38930b2f59fbd20c4746a0b54b057c3635dae
Jan den Besten <info@jandenbesten.net>
2017-02-20 15:07:49 +0100

    Geen uitbreidingen meer vanuit site (admin.css & admin.js)
    Former-commit-id: 7936ebd461e749f35f61f63772d6c1ea06aa778c

--- public/assets/js/admin.js
+++
@@ -1,7 +0,0 @@
-/**
- * Dit bestand voor in de backend geladen.
- * Je kunt hiermee dus extra features toevoegen.
- * Let op: de backed kan veranderen, dus check na elke update of je extra features nog werken
- */
-$(document).ready(function() {
-});
