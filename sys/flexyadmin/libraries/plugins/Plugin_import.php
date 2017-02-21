7f47bc58bc7a308a5441015d30aab2031e5939ea
Jan den Besten <jan@Laptop-van-Jan-2.local>
2017-02-21 17:13:12 +0100

    BUSY Import plugin
    # Conflicts:
    #	site/config/data/tbl_onderwerpen.php
    
    
    Former-commit-id: 76eb61a1d4ac4996fd489614ce24cfb3149effba

--- sys/flexyadmin/libraries/plugins/Plugin_import.php
+++ sys/flexyadmin/libraries/plugins/Plugin_import.php
@@ -13,6 +13,8 @@
 	*/
  class Plugin_import extends Plugin {
    
+   private $import_table;
+   
    private $template = array(
                         'table_open' => '<table class="table table-bordered table-sm table-responsive">',
                         'thead_open' => '<thead class="thead-default">',
@@ -33,6 +35,7 @@
     $table = $this->CI->input->post('table');
     
     if ($table and !empty($_FILES)) {
+      $this->import_table = $table;
       $this->add_message(h($table));
       
       $file = current($_FILES);
@@ -52,22 +55,63 @@
       }
       
       // Import Data
-      foreach ($data as $row) {
+      $set_data = array();
+      foreach ($data as $key=>$row) {
+        $set = array();
         foreach ($row as $field => $value) {
-          $type = get_prefix($field);
-          switch ($type) {
-            case 'str':
+          $value=trim($value);
+          if ($value) {
+            $type = get_prefix($field);
+            switch ($type) {
+
+              case 'id' :
+                $value = $this->_add_foreign($field,$value);
+                $set[remove_suffix($field,'.')] = $value;
+                break;
               
-              break;
+              // case 'rel':
+              //   $this->_add_many_to_many($field,$value);
+              //   break;
+
+              case 'txt':
+                $head = get_suffix($field,':');
+                $field = get_prefix($field,':');
+                if ($head) {
+                  $value = '<b>'.$head.':&nbsp;</b>&nbsp;'.$value.'<br>';
+                }
+                if (!isset($set[$field])) $set[$field]='';
+                $set[$field] .= $value;
+                break;
+
+              case 'str':
+                $set[$field] = $value;
+                break;
+            }
           }
         }
+        $id = '-';
+        if ($set) {
+          // Check eerst of item al bestaat
+          $this->CI->data->table($this->import_table)->where($set);
+          $exists = $this->CI->data->get_row();
+          if ($exists) {
+            $id = $exists['id'];
+          }
+          else {
+            $this->CI->data->set($set);
+            $id = $this->CI->data->insert();
+          }
+        }
+        $row = array_unshift_assoc($row,'id',$id);
+        $data[$key]=$row;
       }
       
       
+      
       // Show Data
+      array_unshift($fields,'id');
       $this->CI->table->set_heading($fields);
       $this->add_message( $this->CI->table->generate($data) );
-      
       
     }
     else {
@@ -89,10 +133,44 @@
   		$form->set_data($formData);
 			$this->add_message( $form->render() );
     }
-    
-    
     return $this->show_messages();
 	}
+  
+  
+  /**
+   * Voeg foreign data toe en geef id terug.
+   * Checkt eerst of foreign data al bestaat, zoja, geeft ook de id terug.
+   *
+   * @param string $field 
+   * @param mixed $value 
+   * @return int $id
+   * @author Jan den Besten
+   */
+  private function _add_foreign($field,$value) {
+    $id = FALSE;
+    
+    // Check de many_to_one relaties
+    $foreign_field = get_suffix($field,'.');
+    $field = remove_suffix($field,'.');
+    $this->CI->data->table( $this->import_table );
+    $relation = $this->CI->data->get_setting(array('relations','many_to_one',$field));
+    
+    // Als relatie bestaat, ga verder
+    if ($relation) {
+      $other_table = $relation['other_table'];
+      // Ok table & set known
+      $this->CI->data->table($other_table)->where($foreign_field,$value);
+      $exists = $this->CI->data->get_row();
+      if ($exists) {
+        $id = $exists['id'];
+      }
+      else {
+        $this->CI->data->set($foreign_field,$value);
+        $id = $this->CI->data->insert();
+      }
+    }
+    return $id;
+  }
 
 }
 
