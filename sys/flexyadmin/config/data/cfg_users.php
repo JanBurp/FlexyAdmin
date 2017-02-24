bf5af10f21cee8254c0f516c317696e9bf9b537c
Jan den Besten <info@jandenbesten.net>
2017-02-24 08:04:26 +0100

    Many_to_many relations back to normal

--- sys/flexyadmin/config/data/cfg_users.php
+++ sys/flexyadmin/config/data/cfg_users.php
@@ -2,9 +2,8 @@
 
 /* --- Settings for data model 'cfg_users' --- Created @ Thu 28 April 2016, 17:02 */
 
-$config['table']    = 'cfg_users';
-$config['order_by'] = 'str_username,id';
-
+$config['table']           = 'cfg_users';
+$config['order_by']        = 'str_username,id';
 $config['abstract_fields'] = array('str_username');
 
 $config['relations'] = array(
@@ -15,19 +14,19 @@ $config['relations'] = array(
       'rel_table'   => 'rel_users__groups',
       'this_key'    => 'id_user',
       'other_key'   => 'id_user_group',
-      'result_name' => 'rel_users__groups'
+      'result_name' => 'cfg_user_groups'
     ),
   ),
 );
 
 $config['grid_set'] = array(
-  'fields'    => array( 'id','str_username', 'email_email','rel_users__groups', 'str_language','b_active'),
-  'order_by'  => 'rel_users__groups.id, str_username',
+  'fields'    => array( 'id','str_username', 'email_email','cfg_user_groups', 'str_language','b_active'),
+  'order_by'  => 'cfg_user_groups.id, str_username',
   'with'      => array( 'many_to_many' ),
 );
 
 $config['form_set'] = array(
-  'fieldsets' => array('Users'=>array( 'id','str_username', 'email_email', 'gpw_password', 'rel_users__groups', 'str_language')),
+  'fieldsets' => array('Users'=>array( 'id','str_username', 'email_email', 'gpw_password', 'cfg_user_groups', 'str_language')),
   'with'      => array( 'many_to_many' ),
 );
 
