ce2a75c112b0bf5e87605dc01a0b3cec592d0666
Jan den Besten <info@jandenbesten.net>
2017-02-26 06:08:48 +0100

    Extendedsearch button & title in vertical menu
    Former-commit-id: ce6e50f824f73ce30df6e8478faf9fc5bafe4e52

--- sys/flexyadmin/views/admin/menu-vertical/item.php
+++ sys/flexyadmin/views/admin/menu-vertical/item.php
@@ -1,3 +1,3 @@
 <li id="menu_lev<?=$lev?>_pos<?=$pos?>" class="nav-item lev<?=$lev?> pos<?=$pos?>" <?=$attr?>>
-  <a href="<?=site_url($uri)?>" class="nav-link <?=$current?> <?=$class?> <?=($clickable?'':'nonClickable')?>"><?php if (isset($icon) and !empty($icon)): ?><span class="fa fa-<?=$icon?>"></span><?php endif ?><?=$title?></a><?=$submenu?>
+  <a href="<?=site_url($uri)?>" class="nav-link <?=$current?> <?=$class?> <?=($clickable?'':'nonClickable')?>" title="<?=$title?>"><?php if (isset($icon) and !empty($icon)): ?><span class="fa fa-<?=$icon?>"></span><?php endif ?><?=$title?></a><?=$submenu?>
 </li>
