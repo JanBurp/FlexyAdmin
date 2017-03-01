dea081974d23e93253233319dc255c303b2aa8da
Jan den Besten <info@jandenbesten.net>
2017-03-01 14:03:36 +0100

    Api & plugin messages in Modal

--- sys/flexyadmin/assets/js/components/flexy-modal.vue
+++ sys/flexyadmin/assets/js/components/flexy-modal.vue
@@ -51,13 +51,13 @@ export default {
 <template>
   
   <div class="modal flexy-modal" id="flexyadmin-modal" :class="{'hidden':!settings.show}">
-    <div class="modal-dialog modal-sm" @keyup.esc="close()" @keyup.enter="enter()">
+    <div class="modal-dialog" :class="options.size" @keyup.esc="close()" @keyup.enter="enter()">
       <div class="modal-content">
         <div v-show="settings.title!==''" class="modal-header bg-primary text-white">
           <flexy-button @click.native="close()" icon="remove" class="btn-danger" />
           <h4 class="modal-title">{{settings.title}}</h4>
         </div>
-        <div class="modal-body">{{settings.body}}</div>
+        <div class="modal-body" v-html="settings.body"></div>
         <div class="modal-footer">
           <button v-for="(button,index) in settings.buttons" @click="clickedButton(index)" type="button" class="btn" :class="button.class">{{button.title}}</button>
         </div>
