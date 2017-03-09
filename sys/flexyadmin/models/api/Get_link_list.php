4bac8a922652815d3235f6715f44d9d2e1583724
Jan den Besten <info@jandenbesten.net>
2017-03-09 21:30:44 +0100

    Link list flexibel, kan overruled worden in eigen tbl_links model
    Former-commit-id: bea6166b17961ed19181fa1a16b098c82c0d308b

--- sys/flexyadmin/models/api/Get_link_list.php
+++ sys/flexyadmin/models/api/Get_link_list.php
@@ -19,64 +19,11 @@ class Get_link_list extends Api_Model {
   public function index() {
     if (!$this->logged_in()) return $this->_result_status401();
 
-    $site_links = $this->data->table('tbl_site')->select('str_title,url_url,email_email')->cache()->get_row();
-    
-    $links = array(
-      array( 'title'=>'Algemeen',    'menu' => array(
-        array( 'title'=>$site_links['str_title'].' ('.str_replace(array('http://','https://'),'',$site_links['url_url']).')',   'value' => $site_links['url_url'] ),
-        array( 'title'=>$site_links['email_email']. ' ('.$site_links['email_email'].')', 'value' => 'mailto:'.$site_links['email_email'] ),
-      )),
-      array( 'title'=>'Pagina\'s',   'menu' => $this->_menu() ),
-      array( 'title'=>'Links',       'menu' => $this->_links() ),
-      array( 'title'=>'Downloads',   'menu' => $this->_downloads() ),
-    );
+    $this->data->table('tbl_links');
+    $links = $this->data->get_link_list();
+
     header("Content-Type: application/json");
     return json_encode($links);
-  }
-  
-  private function _menu() {
-    $this->data->table('tbl_menu')->select('uri,str_title AS title,order,self_parent')->tree('uri');
-    $this->data->where('uri !=""');
-    $result = $this->data->cache()->get_result();
-    $parents = array();
-    $level = 0;
-    foreach ($result as $key => $item) {
-      if ($item['self_parent']!==0) {
-        if (isset($parents[$item['self_parent']])) {
-          $level = $parents[$item['self_parent']];
-        }
-        else {
-          $level++;
-          $parents[$item['self_parent']] = $level;
-        }
-      }
-      $result[$key]['title'] = str_repeat(" ",$level-1) . $item['title'];
-    }
-    return $this->_result_as_links($result);
-  }
-  
-  private function _links() {
-    $this->data->table('tbl_links')->select('url_url AS link, CONCAT(`str_title`," - ",REPLACE(REPLACE(`url_url`,"https://",""),"http://","")) AS title');
-    $this->data->where('url_url !=""')->like('url_url','http','after');
-    $result = $this->data->cache()->get_result();
-    return $this->_result_as_links($result);
-  }
-  
-  private function _downloads() {
-    $this->data->table('res_assets')->select('CONCAT_WS("/","_media/download",`path`,`file`) AS link, alt AS title')->where('path','downloads');
-    $result = $this->data->cache()->get_result();
-    return $this->_result_as_links($result);
-  }
-  
-  private function _result_as_links($result) {
-    $links = array();
-    foreach ($result as $id => $item) {
-      $links[] = array(
-        'title' => $item['title'],
-        'value' => el('link',$item,el('uri',$item)),
-      );
-    }
-    return $links;
   }
 
 }
