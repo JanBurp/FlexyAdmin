8b12bc360951772eb53609a0d5fd2bbceafc1e4c
Jan den Besten <info@jandenbesten.net>
2017-03-09 21:30:44 +0100

    Link list flexibel, kan overruled worden in eigen tbl_links model
    Former-commit-id: 2ceca24f004c5f7f0cbc14709e1f79726d1ec099

--- sys/flexyadmin/models/data/Core_tbl_links.php
+++ sys/flexyadmin/models/data/Core_tbl_links.php
@@ -15,6 +15,99 @@ Class Core_tbl_links extends Data_Core {
     parent::__construct();
   }
   
+  /**
+   * Dit geeft de linklijst aan de API call get_link_list
+   *
+   * @return array
+   * @author Jan den Besten
+   */
+  public function get_link_list() {
+    $site_links = $this->data->table('tbl_site')->select('str_title,url_url,email_email')->cache()->get_row();
+    $links = array(
+      array( 'title'=>'Algemeen',    'menu' => array(
+        array( 'title'=>$site_links['str_title'].' ('.str_replace(array('http://','https://'),'',$site_links['url_url']).')',   'value' => $site_links['url_url'] ),
+        array( 'title'=>$site_links['email_email']. ' ('.$site_links['email_email'].')', 'value' => 'mailto:'.$site_links['email_email'] ),
+      )),
+      array( 'title'=>'Pagina\'s',   'menu' => $this->_menu_link_list() ),
+      array( 'title'=>'Links',       'menu' => $this->_links_link_list() ),
+      array( 'title'=>'Downloads',   'menu' => $this->_downloads_link_list() ),
+    );
+    $this->data->table('tbl_links');
+    return $links;
+  }
+  
+  /**
+   * Link lijst van alle uri's in het menu
+   *
+   * @return array
+   * @author Jan den Besten
+   */
+  private function _menu_link_list() {
+    $this->data->table('tbl_menu')->select('uri,str_title AS title,order,self_parent')->tree('uri');
+    $this->data->where('uri !=""');
+    $result = $this->data->cache()->get_result();
+    $parents = array();
+    $level = 0;
+    foreach ($result as $key => $item) {
+      if ($item['self_parent']!==0) {
+        if (isset($parents[$item['self_parent']])) {
+          $level = $parents[$item['self_parent']];
+        }
+        else {
+          $level++;
+          $parents[$item['self_parent']] = $level;
+        }
+      }
+      $result[$key]['title'] = str_repeat(" ",$level-1) . $item['title'];
+    }
+    return $this->_result_as_link_list($result);
+  }
+  
+  /**
+   * Link lijst van alle links uit de tabel tbl_links
+   *
+   * @return array
+   * @author Jan den Besten
+   */
+  private function _links_link_list() {
+    $this->data->table('tbl_links')->select('url_url AS link, CONCAT(`str_title`," - ",REPLACE(REPLACE(`url_url`,"https://",""),"http://","")) AS title');
+    $this->data->where('url_url !=""')->like('url_url','http','after');
+    $result = $this->data->cache()->get_result();
+    return $this->_result_as_link_list($result);
+  }
+  
+  /**
+   * Link lijst van alle download bestanden
+   *
+   * @return array
+   * @author Jan den Besten
+   */
+  private function _downloads_link_list() {
+    $this->data->table('res_assets')->select('CONCAT_WS("/","_media/download",`path`,`file`) AS link, alt AS title')->where('path','downloads');
+    $result = $this->data->cache()->get_result();
+    return $this->_result_as_link_list($result);
+  }
+  
+  /**
+   * Maakt een link lijst resultaat
+   *
+   * @param array $result 
+   * @return array
+   * @author Jan den Besten
+   */
+  private function _result_as_link_list($result) {
+    $links = array();
+    foreach ($result as $id => $item) {
+      $links[] = array(
+        'title' => $item['title'],
+        'value' => el('link',$item,el('uri',$item)),
+      );
+    }
+    return $links;
+  }
+  
+  
+  
   
   /**
    * Voeg actie toe om link te checken
