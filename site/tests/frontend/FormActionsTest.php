90d54907483c9fa889a9ca537155c8e9cd179396
Jan den Besten <info@jandenbesten.net>
2017-03-16 11:12:05 +0100

    PHP 7.1 ready (depricated function in Ion_auth_model) & bug in FormactionTest

--- site/tests/frontend/FormActionsTest.php
+++ site/tests/frontend/FormActionsTest.php
@@ -24,26 +24,28 @@ class FormActionsTest extends CITestCase {
    */
   public function test_formactions()  {
     
-    $formactions=$this->CI->config->item('formactions');
+    $formactions = $this->CI->config->item('formactions');
     
     $this->assertTrue( true ); // just to be sure some test is done...
-    
 
     $error_reporting=error_reporting();
     error_reporting(0);
     
+    // Send
     if (isset($formactions['formaction_mail'])) {
       if (! $this->CI->email->can_send()) {
         unset($formactions['formaction_mail']);
       }
     }
     
+    $this->CI->data->table('tbl_crud');
+    
     foreach ($formactions as $formaction => $info) {
-      
       $this->CI->load->model($formaction);
       $this->CI->$formaction->initialize($info['settings']);
       $this->CI->$formaction->fields($info['fields']);
-      $data=$this->CI->db->random_data($info['fields']);
+
+      $data = $this->CI->data->get_random($info['fields']);
       $result=$this->CI->$formaction->go($data);
       
       $this->assertGreaterThanOrEqual(1,$result, el('message',$info,'Formaction `'.$formaction.'` did not give success as result.') );
