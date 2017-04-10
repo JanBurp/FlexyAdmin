bb01f882364c2d358a09cc90402e983647ca5cbb
Jan den Besten <info@jandenbesten.net>
2017-04-10 13:11:20 +0200

    VueForm

--- sys/flexyadmin/views/admin/vue/form.php
+++ sys/flexyadmin/views/admin/vue/form.php
@@ -1,6 +1,14 @@
 <flexy-form
-  title="<?=$title?>"
-  name="<?=$name?>"
-  :primary="<?=$id?>"
+  <?php if (isset($action)): ?>
+  action="<?=$action?>"
+  <?php endif ?>
   formtype="single"
+  name="<?=$name?>"
+  title="<?=$title?>"
+  <?php if (isset($fields)): ?>
+  :fields="<?=htmlentities(array2json($fields),ENT_QUOTES, 'UTF-8')?>"
+  <?php endif ?>
+  <?php if (isset($id)): ?>
+  :primary="<?=$id?>"
+  <?php endif ?>
 ></flexy-form>
