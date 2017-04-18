acac3abcd80bafdba2587e6eb0aa59418070fcf9
Jan den Besten <jan@Laptop-van-Jan-2.local>
2017-04-18 13:29:35 +0200

    FA: form styling bootstrap

--- sys/flexyadmin/views/admin/form/field.php
+++ sys/flexyadmin/views/admin/form/field.php
@@ -1,7 +1,7 @@
 <div class="<?=$field['container_class']?> field_<?=$field['name']?> fieldtype_<?=get_prefix( $field['name'],'_')?> <?=$field['status']?> row" <?=$attributes?>>
   <?php if (isset($field['validation_error'])): ?><?=$field['validation_error']?><?php endif ?>
-  <label for="<?=$field['name']?>" class="<?=$field['label_class']?> <?=($field['horizontal_bootstrap'])?'col-sm-2':'';?>"><?=$field['label']?></label>
-  <?php if ($field['horizontal_bootstrap']): ?><div class="col-sm-10"><?php endif ?>
+  <label for="<?=$field['name']?>" class="<?=$field['label_class']?> <?=($field['horizontal_bootstrap'])?'col-lg-2 col-sm-3':'';?>"><?=$field['label']?></label>
+  <?php if ($field['horizontal_bootstrap']): ?><div class="col-lg-10 col-sm-9"><?php endif ?>
     <?=$field['control']?>
   <?php if ($field['horizontal_bootstrap']): ?></div><?php endif ?>
   <?php if (isset($field['html']) and !empty($field['html'])): ?>
