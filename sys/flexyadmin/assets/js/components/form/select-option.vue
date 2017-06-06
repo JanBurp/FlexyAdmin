9829a9945d8d496dd8d804d00008546fe35dbebd
Jan den Besten <jan@Laptop-van-Jan-2.local>
2017-06-06 14:57:33 +0200

    FA kleuren in selects
    Former-commit-id: 6fd48cd2ec7fd2674adab207df1f9df506439a91
    Former-commit-id: d5856a54586c635d7c1a1c502f94ab07288f2fe2 [formerly 18a06edfa0609ddb094941645cc8cc8d73e0f763]
    Former-commit-id: 6fd09a5d2639505cf52d7c1cb5c45a3886ed4bc1

---
+++ sys/flexyadmin/assets/js/components/form/select-option.vue
@@ -0,0 +1,33 @@
+<script>
+
+import jdb  from '../../jdb-tools.js'
+
+export default {
+  name: 'select-option',
+  props:{
+    'label'       : String,
+    'extraClass' : String,
+  },
+  
+  data : function() {
+    return {
+    };
+  },
+  
+  methods : {
+    style : function(value) {
+      if ( value.substr(0,1)=='#' && value.length==7 ) {
+        return 'color:'+jdb.complementColor(value)+';background-color:'+value+';padding:.25rem .5rem;';
+      }
+      return '';
+    },
+  },
+  
+}
+</script>
+
+<template>
+  <span class="select-option" :class="extraClass" :style="style(label)">
+    {{label}}
+  </span>
+</template>
