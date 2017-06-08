cb2b3c7df4c86e3568746aa05665c58d72e55cde
Jan den Besten <info@jandenbesten.net>
2017-06-08 17:27:04 +0200

    Frontend Basic Styling
    Former-commit-id: 5e737b069c2df8dc28f7063b1625f253ed54402a
    Former-commit-id: fc0dbc098d4aef5b2a5387d33fa927b600e0527e [formerly 26ab9e075af57b16f75a1c7ce304d99a4f8e237a]
    Former-commit-id: b639dde20d4a288f5f9174707a2c5fee55e924dc

--- site/views/menu/item.php
+++ site/views/menu/item.php
@@ -2,8 +2,10 @@
   <?php if (!$submenu): ?>
   	<a href="<?=site_url($uri)?>" class="lev<?=$lev?> pos<?=$pos?> _pos<?=$_pos?> <?=$order?> <?=$class_uri?> <?=$current?> <?=(!$clickable?'disabled':'')?>"><?=$title?></a>
   <?php else: ?>
-	  <a href="<?=site_url($uri)?>" class="dropdown-toggle disabled lev<?=$lev?> pos<?=$pos?> _pos<?=$_pos?> <?=$order?> <?=$class_uri?> <?=$current?>"><?=$title?></a>
-	  <a class="dropdown-toggle dropdown-caret" data-toggle="dropdown" aria-expanded="false"><b class="caret"></b></a>
+	  <a href="<?=site_url($uri)?>" class="lev<?=$lev?> pos<?=$pos?> _pos<?=$_pos?> <?=$order?> <?=$class_uri?> <?=$current?>">
+	  	<?=$title?>
+	  </a>
+	  <a href="" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
 	  <?=$submenu?>
-	  <?php endif ?>
+	 <?php endif ?>
 </li>
