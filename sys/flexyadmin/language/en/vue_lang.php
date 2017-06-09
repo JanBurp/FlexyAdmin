1aab202199128538e4203cf8d811f4d8c6423883
Jan den Besten <jan@LaptopvanJan2.fritz.box>
2017-06-09 05:32:07 +0200

    FA bugs in taal instellingen
    Former-commit-id: 7b5b1fab9e82c24b51362d714fc86cb2ca6af2c3
    Former-commit-id: ef06af4fbf4a6f366080370500f3969825430356 [formerly 521fbdf730afec201ce2a14dabd155299ea7219b]
    Former-commit-id: 5acd8403a3fed714963cdfaa0d09c620d2d8aea1

---
+++ sys/flexyadmin/language/en/vue_lang.php
@@ -0,0 +1,64 @@
+<?php 
+
+$lang['vue_api_error_401'] = 'You don\'t have rights for this item.';
+
+$lang['vue_cancel']        = "Cancel";
+$lang['vue_submit']        = "Submit";
+$lang['vue_add']           = "Add";
+$lang['vue_add_item']      = "New item in `{0}`";
+$lang['vue_ok']            = "Ok";
+$lang['vue_save']          = "Save";
+$lang['vue_remove']        = "Remove";
+$lang['vue_browse']        = "Browse";
+$lang['vue_or_drag_files'] = "Or drag files here";
+
+$lang['vue_confirm_delete_one']      = "Are you sure to remove the selected item?";
+$lang['vue_confirm_delete_multiple'] = "Are you sure to remove {0} selected items?";
+$lang['vue_deleted']                 = "{0} item(s) remove.";
+
+$lang['vue_confirm_save_default']    = "You changed nothing! Are you sure to submit?";
+
+$lang['vue_grid_search']             = "Search";
+$lang['vue_grid_fast_search']        = "Fast search";
+$lang['vue_grid_extended_search']    = "Extended search";
+$lang['vue_grid_search_and']         = "and";
+$lang['vue_grid_search_or']          = "or";
+$lang['vue_grid_search_exist']       = 'has';
+$lang['vue_grid_search_word']        = 'has word';
+$lang['vue_grid_search_exact']       = 'same as';
+
+$lang['vue_grid_edit_success']       = 'Field is changed';
+
+$lang['vue_file_select']             = 'Select file(s)';
+$lang['vue_upload']                  = 'Upload';
+$lang['vue_upload_choose']           = 'Click of drag new files.';
+$lang['vue_upload_file']             = 'File';
+$lang['vue_upload_size']             = 'Size';
+$lang['vue_upload_progress']         = 'Progress';
+$lang['vue_upload_status']           = 'Status';
+$lang['vue_upload_status_uploading'] = 'Uploading...';
+$lang['vue_upload_error']            = 'Unknown error.';
+$lang['vue_upload_ready']            = ' added.';
+
+$lang['vue_grid_total']              = "{0} items";
+$lang['vue_grid_pagination']         = "{0} items / {1} pages";
+$lang['vue_grid_pagination_max']     = "{0} items (of {1}) / {2} pages";
+$lang['vue_grid_order_save_error']   = "<b>ERROR</b> while saving new order. Order is restored, try again.";
+$lang['vue_form_validation_error']   = "Not all fields are filled correctly!";
+$lang['vue_form_save_error']         = "<b>ERROR</b> while saving. Try again.";
+$lang['vue_error_api']               = "<b>ERROR</b>. No contact with server. Try again.";
+$lang['vue_error_delete']            = "<b>ERROR</b>. While deleting item(s).";
+
+$lang['vue_strap_lang'] = array(
+  'daysOfWeek'    => array('Su', 'Mo', 'Tu', 'Wo', 'Th', 'Fr', 'Sa'),
+  'limit'         => 'Maximum reached (maximum {{limit}} items).',
+  'loading'       => 'Loading...',
+  'minLength'     => 'Min. Length',
+  'months'        => array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+  'notSelected'   => 'Nothing selected',
+  'required'      => 'Required',
+  'search'        => 'Search'
+);
+
+
+?>
