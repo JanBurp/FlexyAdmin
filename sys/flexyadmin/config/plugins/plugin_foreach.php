cb20c49f5cbffcd4bd5c244893cba85bcfa08d9e
Jan den Besten <jan@LaptopvanJan2.lan>
2017-07-11 15:29:00 +0200

    FA new plugin_foreach

---
+++ sys/flexyadmin/config/plugins/plugin_foreach.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['admin_api_method']        = '_admin_api';
+
+/*
+ *--------------------------------------------------------------------------
+ * Plugins specific config settings
+ *--------------------------------------------------------------------------
+ *
+ */
+
+$config['table'] = 'cfg_users';
+
+
+?>
