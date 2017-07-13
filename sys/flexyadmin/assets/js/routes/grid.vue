3a15de94c637e3d43681754f88e8ee1ad7004e6f
Jan den Besten <info@jandenbesten.net>
2017-07-13 15:27:18 +0200

    idem

---
+++ sys/flexyadmin/assets/js/routes/grid.vue
@@ -0,0 +1,16 @@
+<script>
+import FlexyGrid    from '../components/grid/flexy-grid.vue'
+
+export default {
+  name: 'grid',
+  components: {FlexyGrid},
+  props:{},
+}
+</script>
+
+<template>
+  <div>
+    GRID: {{ $route.params.table }}
+    <flexy-grid type="table" api="table" limit="10" :name="$route.params.table" :title="$route.params.table"></flexy-grid>
+  </div>
+</template>
