7a6594b19fb5210e54f65e5ed5585c2eda67fb86
Jan den Besten <info@jandenbesten.net>
2017-07-13 11:02:37 +0200

    Menu button & Help styling

--- sys/flexyadmin/views/admin/menu-horizontal/item.php
+++ sys/flexyadmin/views/admin/menu-horizontal/item.php
@@ -1,3 +1,7 @@
-<li id="menu_lev<?=$lev?>_pos<?=$pos?>" class="nav-item lev<?=$lev?> pos<?=$pos?>" <?=$attr?>>
-  <a href="<?=site_url($uri)?>" class="nav-link <?=$current?> <?=($clickable?'':'nonClickable')?>" <?=isset($html)?$html:'';?>><?php if (isset($icon) and !empty($icon)): ?><span class="fa fa-<?=$icon?>"></span><?php endif ?><span class="nav-title"><?=$title?></span></a><?=$submenu?>
+<li id="menu_lev<?=$lev?>_pos<?=$pos?>" class="nav-item lev<?=$lev?> pos<?=$pos?> <?=$class?>" <?=$attr?>>
+  <a href="<?=site_url($uri)?>" class="nav-link <?=$current?>" <?=isset($html)?$html:'';?>>
+  	<span class="fa fa-<?=$icon?> <?=(isset($active_icon)?'hide-when-active':'')?>"></span>
+  	<?php if ($active_icon): ?><span class="show-when-active fa fa-<?=$active_icon?>"></span><?php endif ?>
+  	<span class="nav-title"><?=$title?></span>
+  </a><?=$submenu?>
 </li>
