ca1777cc4bfe3ec91927aa2cf079dbe81f888b79
Jan den Besten <info@jandenbesten.net>
2017-07-16 21:59:19 +0200

    Simple Form

---
+++ sys/flexyadmin/assets/js/components/form/flexy-simple-form.vue
@@ -0,0 +1,46 @@
+<script>
+import FlexyFormField  from './flexy-form-field.vue'
+
+export default {
+  name: 'Search',
+  components: {FlexyFormField},
+  props:{
+    'fields':{
+      type: [Array,Object],
+      default: {},
+    },
+  },
+    
+  data : function() {
+    return {
+      data : [],
+    }
+  },
+
+  created : function() {
+    for (var field in this.fields) {
+      this.data[field] = this.fields[field].value;
+    }
+  },
+
+  methods : {
+
+    update : function(name,value) {
+      this.data[name] = value;
+    },
+
+    submit : function(event) {
+      this.$emit('submit',this.data);
+    },
+
+  },
+
+}
+</script>
+
+<template>
+  <form @submit.prevent.stop="submit($event)">
+    <flexy-form-field v-for="(field,name) in fields" :name="name" :type="field.type" :label="field.label" :value="field.value" @changed="update(name,$event)"></flexy-form-field>
+    <button class="btn btn-primary" type="submit">Submit</button>
+  </form>
+</template>
