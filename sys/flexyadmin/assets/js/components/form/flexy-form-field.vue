6b32e9af404e4d5519224f40971813845e3e7a33
Jan den Besten <info@jandenbesten.net>
2017-07-16 21:59:19 +0200

    Simple Form
    Former-commit-id: de8b028a51928b85f35eb86cb719c08c8543e8d5
    Former-commit-id: 34057ae04bc5bf21a28b7d73d43af6de34d1aa0a [formerly e61f4731174d8cfc6de75a19ce6968486cd1ed48]
    Former-commit-id: dbf0a0f3628b1a2b6886abd56a4234519bd8c61d

---
+++ sys/flexyadmin/assets/js/components/form/flexy-form-field.vue
@@ -0,0 +1,68 @@
+<script>
+
+import flexyButton      from '../flexy-button.vue'
+// import timepicker       from './timepicker.vue'
+// import datetimepicker   from './datetimepicker.vue'
+// import colorpicker      from './colorpicker.vue'
+// import mediapicker      from './mediapicker.vue'
+// import vselect           from '../../vue-strap-src/components/Select.vue'
+// import datepicker       from '../../vue-strap-src/Datepicker.vue'
+
+export default {
+  name: 'FlexyFormField',
+  components: {flexyButton},//,timepicker,datetimepicker,colorpicker,mediapicker,datepicker,vselect},
+  props:{
+    'name':String,
+    'type':{
+      type: String,
+      default: 'input',
+    },
+    'label':{
+      type: String,
+      default: '',
+    },
+    'value' :{
+      type: [Number,String,Boolean],
+      default: null,
+    },
+    'placeholder' :{
+      type: [Number,String,Boolean],
+      default: null,
+    },
+    'validation'  :{
+      type: String,
+      default: '',
+    },
+  },
+
+  data : function() {
+    return {
+      data : this.value,
+    }
+  },
+  
+  methods:{
+    
+    title : function() {
+      if (this.label==='') return this.name;
+      return this.label;
+    },
+
+    update : function(value) {
+      this.$emit('changed',value);
+    },
+
+  },
+
+}
+</script>
+
+<template>
+  <div class="form-group row">
+    <label class="col-md-3 form-control-label" :for="name">{{title()}}</label>
+    <div class="col-md-9">
+      <input v-if="type=='input'"     type="input"    class="form-control" :id="name" :name="name" v-model="data" :placeholder="placeholder" v-on:input="update($event.target.value)" />
+      <input v-if="type=='checkbox'"  type="checkbox" class="form-control" :id="name" :name="name" v-model="data" @click="update($event.target.checked)" />
+    </div>
+  </div>  
+</template>
