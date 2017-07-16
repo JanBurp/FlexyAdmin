0cf32484e9a8b99f061d9e250230553aab6a2c0e
Jan den Besten <info@jandenbesten.net>
2017-07-16 16:54:52 +0200

    Router: busy with tools
    Former-commit-id: 736f08beb28821b0b29b3004af2565fd019a4b73
    Former-commit-id: ca4e499efe4f18ad35366ba9dcfd7599407d6f72 [formerly 1a21a1ff267e5ab300415126b7bf94ea008ee19f]
    Former-commit-id: fef601244550843ae6e28d3c2693e3dd1bfe58c4

---
+++ sys/flexyadmin/assets/js/routes/tools.vue
@@ -0,0 +1,22 @@
+<script>
+import dbTool from '../tools/db.vue'
+import fillTool from '../tools/fill.vue'
+import searchTool from '../tools/search.vue'
+
+export default {
+  name: 'tools',
+  components: {dbTool,searchTool,fillTool},
+}
+</script>
+
+<template>
+	<div>
+		<div v-if="$route.params.tool==='db_export'"><db-tool action="export"></db-tool></div>
+		<div v-if="$route.params.tool==='db_import'"><db-tool action="import"></db-tool></div>
+		<div v-if="$route.params.tool==='db_backup'"><db-tool action="backup"></db-tool></div>
+		<div v-if="$route.params.tool==='db_restore'"><db-tool action="restore"></db-tool></div>
+
+		<div v-if="$route.params.tool==='fill'"><fill-tool></fill-tool></div>
+		<div v-if="$route.params.tool==='search'"><search-tool></search-tool></div>
+	</div>
+</template>
