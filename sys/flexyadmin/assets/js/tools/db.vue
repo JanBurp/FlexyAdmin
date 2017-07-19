5239212918d41e083a0b7dc75410d82c8c96762f
Jan den Besten <info@jandenbesten.net>
2017-07-19 14:52:22 +0200

    Router db restore
    Former-commit-id: b51015bb400ddcbffcffe4face310c0398afa842
    Former-commit-id: 9c7598e45b70981cf62f1da7cfb0db7450a120b4 [formerly 63b11bd7a6fbd05c8ef761e1c0e808e1517a5e5a]
    Former-commit-id: 347cf472d67ead68c3a76f2241ec003dcb18e3e0

--- sys/flexyadmin/assets/js/tools/db.vue
+++ sys/flexyadmin/assets/js/tools/db.vue
@@ -1,31 +1,103 @@
 <script>
+
+// import jdb              from './../jdb-tools.js'
+import flexyState       from './../flexy-state.js'
+import FlexySimpleForm  from '../components/form/flexy-simple-form.vue'
+
 export default {
-  name: 'Db',
-  props:['action'],
+  name: 'db',
+  components: {FlexySimpleForm},
+  props: ['action'],
   
   data :function(){
     return {
-      // helpItems   : {},
-      // visibleItem : '',
+      fields   : {},
+      filename : '',
+      sql      : '',
+      errors   : false,
+      message  : false,
     }
   },
-  
-  // created : function(){
-  //   this.helpItems = JSON.parse(this.items);
-  //   this.visibleItem = 'over_help'
-  // },
+
+
+  created : function() {
+    var self = this;
+
+    switch (this.action) {
+
+
+      case 'backup':
+        return flexyState.api({
+          url : 'tools/db_backup',
+        }).then(function(response){
+          self.filename = response.data.data.filename;
+          self.sql      = response.data.data.sql;
+        });
+        break
+
+      case 'restore':
+        break;
+
+
+    }
+
+  },
+
+
+  methods : {
+
+    restore : function(event) {
+      var self=this;
+      var input = event.target;
+      var reader = new FileReader();
+      reader.onloadend = function(){
+        if (reader.result) {
+          return flexyState.api({
+            method : 'POST',
+            url    : 'tools/db_restore',
+            data   : { 'sql' : reader.result},
+        }).then(function(response){
+          if (!_.isUndefined(response.data.data.errors) && response.data.data.errors.length>0) {
+            self.errors = Object.assign( {}, response.data.data.errors );
+          }
+          self.message = response.data.data.comments;
+          if (!self.errors) {
+            self.message = '<b>Succes!!</b><br><br>' + self.message;  
+          }
+        });
+        }
+      };
+      reader.readAsText(input.files[0]);
+    },
+
+  }
   
 }
 </script>
 
 <template>
-  <div class="flexy-tool">
-    <div class="card">
-      <h1 class="card-header">Database Tools - {{action}}</h1>
-      <div class="card-block">
+  <div class="flexy-tool flexy-tool-db">
+
+    <!-- BACKUP -->
+    <div v-if="action=='backup'" class="card">
+      <h1 class="card-header">Backup</h1>
+      <div v-if="filename!==''" class="card-block">
+        <a :href="'data:text/plain;charset=utf-8,' + encodeURIComponent(sql)" :download="filename" class="btn btn-warning"><span class="fa fa-download"></span>Download Backup</a>
       </div>
     </div>
 
+    <!-- RESTORE -->
+    <div v-if="action=='restore'" class="card">
+      <h1 class="card-header">Restore backup</h1>
+      <div class="card-block">
+        <input type="file" id="restore_file" name="restore_file" @change="restore($event)" />
+        <div v-if="errors!==false">
+          <div v-for="error in errors" class="alert alert-danger">{{error.message}}</div>
+        </div>
+        <div v-if="message!==false" class="alert alert-success" v-html="message"></div>
+      </div>
+    </div>
+
+
   </div>
-  
 </template>
