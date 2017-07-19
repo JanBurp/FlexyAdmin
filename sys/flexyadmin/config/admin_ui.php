fda19e02d5e0f1c7255eb95ff3eb0243f683cdaa
Jan den Besten <info@jandenbesten.net>
2017-07-19 16:33:00 +0200

    Router busy with DB
    Former-commit-id: 2dca89b882e1d329ea828ba6be405a0425f3120c
    Former-commit-id: 05e7a36157c74eb93b23d4f02b1d36f53f39dd11 [formerly 5ff985a69633e545197caf000ac090d5fb07a471]
    Former-commit-id: 0afb30800dc1c6f6793ac72de24e12f51bf101e0

--- sys/flexyadmin/config/admin_ui.php
+++ sys/flexyadmin/config/admin_ui.php
@@ -29,8 +29,8 @@ $config['side_menu']['media']  = array();
 $config['side_menu']['tools'] = array(
   'export'   => array( 'user_group'=>'super_admin', 'name'=>'db_export',          'uri'=>'tools/db_export',   'icon'=>'database', 'class'=>'text-muted' ),
   'import'   => array( 'user_group'=>'super_admin', 'name'=>'db_import',          'uri'=>'tools/db_import',   'icon'=>'database', 'class'=>'text-muted' ),
-  'backup'   => array( 'user_group'=>'[b_backup]',  'name'=>'db_backup',          'uri'=>'tools/db_backup',   'icon'=>'database', 'class'=>'text-muted' ),
-  'restore'  => array( 'user_group'=>'[b_backup]',  'name'=>'db_restore',         'uri'=>'tools/db_restore',  'icon'=>'database', 'class'=>'text-muted' ),
+  'backup'   => array( 'user_group'=>'[b_backup]',  'name'=>'db_backup',          'uri'=>'tools/db_backup',   'icon'=>'download', 'class'=>'text-muted' ),
+  'restore'  => array( 'user_group'=>'[b_backup]',  'name'=>'db_restore',         'uri'=>'tools/db_restore',  'icon'=>'upload', 'class'=>'text-muted' ),
   '-'        => array( 'type'=>'seperator' ),
   'search'   => array( 'user_group'=>'super_admin',   'name'=>'sr_search_replace',  'uri'=>'tools/search',      'icon'=>'search', 'class'=>'text-muted' ),
   'fill'     => array( 'user_group'=>'super_admin',   'name'=>'fill_fill',          'uri'=>'tools/fill',        'icon'=>'arrow-circle-o-down', 'class'=>'text-muted' ),
