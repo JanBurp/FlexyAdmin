8ab76b60d517af1a998e0e3838b7265d23a08cda
Jan den Besten <info@jandenbesten.net>
2017-08-22 15:18:27 +0200

    BUG API table when no grid order could not be set
    Former-commit-id: 5745223988eb144d35cf0a23354ea0354369ffbd
    Former-commit-id: fcefd8912873d8678b953e770324a4782d6dedbd [formerly a7efcd27ebdd6f2f90ca5df606a9fa0749e63c45]
    Former-commit-id: d135aa95ee179a3a32f81d3fbdd1eb00493b347d

--- sys/flexyadmin/models/api/Table.php
+++ sys/flexyadmin/models/api/Table.php
@@ -176,9 +176,9 @@ class Table extends Api_Model {
       if (!isset($this->args['where'])) $this->args['where'] = '';
       // txt_abstract, options
       if ( isset($this->args['txt_abstract'])) $this->data->select_txt_abstract( $this->args['txt_abstract'] );
-      $this->data->order_by( $this->args['order'] );
       $this->data->find( $this->args['filter'] );
       if ($this->args['where']) $this->data->where( $this->args['where'] );
+      $this->data->order_by( $this->args['order'] );
       $items = $this->data->get_grid( $this->args['limit'], $this->args['offset'] );
     }
     else {
@@ -196,6 +196,7 @@ class Table extends Api_Model {
         else {
           if ( isset($this->args['where']) ) $this->data->where( $this->args['where'] );
           if ( isset($this->args['txt_abstract'])) $this->data->select_txt_abstract( $this->args['txt_abstract'] );
+          $this->data->order_by( $this->args['order'] );
           $items = $this->data->get_result( $this->args['limit'], $this->args['offset'] );
         }
       }
