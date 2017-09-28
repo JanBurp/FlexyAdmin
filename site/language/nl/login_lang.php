ceca1676c8d5cb3e3fb646a647c056a0df2cc28d
Jan den Besten <info@jandenbesten.net>
2017-09-28 09:29:59 +0200

    Typos
    # Conflicts:
    #	site/language/nl/login_lang.php
    
    
    Former-commit-id: 6b455129edb4adf7d1d0a35573f5c29041acea3e
    Former-commit-id: 5fbf0cfc870ea246473c839fb403d33cb963167a [formerly 89027eb52cffd458267f63df942676e4b788828b]
    Former-commit-id: bcf5d1fc834e6ac67b50483938bde5da61c5342c

---
+++ site/language/nl/login_lang.php
@@ -0,0 +1,46 @@
+<?php
+
+// REGISTER
+$lang['username']								= 'Gebruikersnaam';
+$lang['username_used']					= 'Deze naam wordt al gebruikt.';
+$lang['password']								= 'Wachtwoord';
+$lang['password2']							= 'Wachtwoord 2x';
+$lang['password_mismatch']			= 'Het wachtwoord is anders dan het controle-wachtwoord. Vul 2 keer hetzelfde in.';
+$lang['email']									= 'Email adres';
+$lang['email_used']							= 'Dit email adres is al in gebruik.';
+$lang['register_submit']				= 'Aanmelden!';
+$lang['register_completed']			= 'Klik nu op inloggen om verder te gaan.';
+$lang['register_wait']					= 'Je aanvraag wordt beoordeeld. Je krijgt daar binnenkort per email uitsluitsel over.';
+$lang['register_succes']				= 'Je aanmelding is gelukt. Je kunt nu <a href="%s">inloggen</a>.';
+$lang['register_fail']					= 'Je aanmelding is mislukt.';
+$lang['register_mail_subject']	= 'Aanmelding.';
+
+// LOGIN
+$lang['forgot_password']				= 'Klik hier als je je wachtwoord vergeten bent.';
+$lang['register']								= 'Maak account aan.';
+$lang['login_register']					= 'Maak account aan.';
+$lang['remember']								= 'Laat me ingelogd blijven';
+$lang['login_submit']						= 'Inloggen';
+$lang['login_error']						= 'Verkeerde gebruikersnaam/wachtwoord combinatie.';
+$lang['login_already']					= 'Je bent (al) ingelogd.';
+
+// LOGOUT
+$lang['logout_done']						= 'Je bent uitgelogd.';
+
+// FORGOT PASSWORD
+$lang['forgot_password_intro']				= 'Je bent je wachtwoord vergeten, dat kan gebeuren.<br/>Vul je email in dan sturen wij je een mail waarmee je je wachtwoord kunt resetten.';
+$lang['forgot_password_submit']				= 'Stuur email';
+$lang['forgot_password_completed']		= 'Check je email om je wachtwoord te resetten.';
+$lang['forgot_password_mail_subject']	= 'Vergeten wachtwoord reset.';
+$lang['email_not_found']	            = 'Email adres is niet bekend bij ons.';
+// RESET PASSWORD
+$lang['reset_password_succes'] 				= 'Het opnieuw aanvragen van je wachtwoord is gelukt.<br/>Per mail heb je een nieuw wachtwoord ontvangen.<br/><br/><a href="%s">Login</a>';
+$lang['reset_password_error'] 				= 'Het opnieuw aanvragen van je wachtwoord is mislukt of is al eerder gebeurd.';
+$lang['reset_password_mail_subject']	= 'Nieuw wachtwoord.';
+
+// EDIT
+$lang['edit_submit']                  = 'Pas aan';
+$lang['edit_changed']                 = 'Uw gegevens zijn aangepast';
+
+
+?>
