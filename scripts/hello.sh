b39c2ef87d758129b9dc8d61cbe63dbe640959f7
Jan den Besten <info@jandenbesten.net>
2018-01-02 12:34:25 +0100

    Update script added
    Former-commit-id: fa6b827ca0590fda5042cefc94f74e5e00a0332b
    Former-commit-id: 6917bb171c4b824e3b4acadf25ad662c8200a3fe [formerly 5924580b548100861f4ee8422005afd76685933a]
    Former-commit-id: a77aee16c90db93eebd627038a1156aa9ea6fc52

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-RESET=`tput sgr0`
-BUILD=`grep '^[^\s]*' sys/build.txt | cut -d " " -f1`
+export RED=`tput setaf 1`
+export GREEN=`tput setaf 2`
+export RESET=`tput sgr0`
+export BUILD=`grep '^[^\s]*' sys/build.txt | cut -d " " -f1`
 
-echo "${GREEN}FlexyAdmin (${BUILD})\n==========\n${RESET}"
+echo "${GREEN}FlexyAdmin (${BUILD})\n==================\n${RESET}"
