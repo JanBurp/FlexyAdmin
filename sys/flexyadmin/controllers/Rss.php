dd48c005d3aaebf2a41baa634f88decd2a6de089
Jan den Besten <info@jandenbesten.net>
2018-01-05 09:45:17 +0100

    Better RSS feed
    Former-commit-id: c577d4c97ac9f72d5b71c0b9ed5b03e0c202be0b
    Former-commit-id: 2b607f13d5c66554dfa393121e5ca7792269dc12 [formerly c8055d76703fd8a0b4db4006c4e1abf08d6769b0]
    Former-commit-id: 25a9f3099f5f9fb5d372474175b212ba53010c8f

--- sys/flexyadmin/controllers/Rss.php
+++ sys/flexyadmin/controllers/Rss.php
@@ -27,25 +27,26 @@ class Rss extends FrontEndController {
       if (isset($row['uri']) and isset($row['str_title'])) {
         $post=array();
         $post['url']   = $row['uri'];
-        $post['title'] = $row['str_title'];
+        $post['title'] = preg_replace('/[\x00-\x09\x0B-\x1F\x7F]/u', '', ascii_to_entities($row['str_title']) );
         $post['date']  = date('r');
         $post['body']  = '';
-        if (isset($row['dat_date'])) $post['date']  = date(DATE_ATOM, human_to_unix( $row['dat_date']));
-        if (isset($row['txt_text'])) $post['body']  = character_limiter( strip_tags($row['txt_text']), 250);
+        if (isset($row['dat_date'])) $post['date']  = date(DATE_ATOM, date_to_unix( $row['dat_date']));
+        if (isset($row['txt_text'])) $post['body']  = preg_replace('/[\x00-\x09\x0B-\x1F\x7F]/u', '', ascii_to_entities(character_limiter( strip_tags($row['txt_text']), 250)) );
         $feed[]=$post;
       }
 		}
     
 		$siteInfo = $this->data->table('tbl_site')->get_row();
-    $data=array();
-		$data['encoding'] = 'utf-8';
-		$data['feed_name'] = $siteInfo['str_title'];
-		$data['feed_url'] = site_url();
-    $data['updated'] = date(DATE_ATOM);
-		$data['page_description'] = 'RSS: '.$siteInfo['stx_description'];
-		$data['page_language'] = $this->site['language'];
-		$data['creator_email'] = str_replace(array('@','.'),array('-at-','-dot-'),$siteInfo['email_email']);
-		$data['posts'] = $feed;
+    $data = array();
+    $data['encoding']         = 'utf-8';
+    $data['feed_name']        = $siteInfo['str_title'];
+    $data['feed_url']         = site_url();
+    $data['updated']          = date(DATE_ATOM);
+    $data['page_description'] = 'RSS: '.$siteInfo['stx_description'];
+    $data['page_language']    = $this->site['language'];
+    $data['creator_email']    = str_replace(array('@','.'),array('-at-','-dot-'),$siteInfo['email_email']);
+    $data['author']           = $siteInfo['email_email'].' ('.$siteInfo['str_author'].')';
+    $data['posts']            = $feed;
 
 		header("Content-Type: application/rss+xml");
 		$this->load->view('rss/feed', $data);
