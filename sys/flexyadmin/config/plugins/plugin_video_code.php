b58c0143f504e73c921e7928cda06723025e79eb
Jan den Besten <info@jandenbesten.net>
2018-01-11 11:44:47 +0100

    Plugin video works now also with field ‘video_’

--- sys/flexyadmin/config/plugins/plugin_video_code.php
+++ sys/flexyadmin/config/plugins/plugin_video_code.php
@@ -22,6 +22,7 @@ $config['after_update_method'] = '_after_update';
 */
 
 $config['trigger'] = array(
+	'field_types'			=> array('video'),
   'fields'          => array('str_video','str_youtube','str_vimeo'),
 );
 
