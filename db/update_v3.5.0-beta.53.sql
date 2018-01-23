e8cae16405df740a100c3748e2c216e048bf685b
Jan den Besten <info@jandenbesten.net>
2018-01-23 16:13:02 +0100

    TAG 3.5.0-beta.53
    Former-commit-id: e321c50e193e70711876c38fac558f7a9ca5cbf2
    Former-commit-id: 0c557be5f509bfdebdaf20f394ab5a7f09f0ac29 [formerly de20de402dd93d4ec57d18245ad96416d54dd49f]
    Former-commit-id: 90d446721c8b2a781e2bce7ef8bdf605f0d41267

--- db/update_v3.5.0-beta.53.sql
+++ db/update_v3.5.0-beta.53.sql
@@ -1,3 +1,6 @@
 # Keep version of system when user logged in
 ALTER TABLE `cfg_users` ADD `str_last_version` VARCHAR(20)  NOT NULL  DEFAULT ''  AFTER `str_filemanager_view`;
 ALTER TABLE `cfg_version` CHANGE `str_version` `str_version` VARCHAR(20)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT '3.5.0';
+
+# Change db revision
+UPDATE `cfg_version` SET `str_version` = '3.5.0-beta.53';
