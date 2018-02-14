ee553697bf5e2446f39688dde47e942c246c1d69
Jan den Besten <info@jandenbesten.net>
2018-02-14 10:21:48 +0100

    Added ‘inputtype_’ class to form_groups

--- sys/flexyadmin/views/admin/form/field.php
+++ sys/flexyadmin/views/admin/form/field.php
@@ -1,4 +1,4 @@
-<div class="<?=$field['container_class']?> field_<?=$field['name']?> fieldtype_<?=get_prefix( $field['name'],'_')?> <?=$field['status']?> row" <?=$attributes?>>
+<div class="<?=$field['container_class']?> field_<?=$field['name']?> fieldtype_<?=get_prefix($field['name'],'_')?> inputtype_<?=$field['type']?> <?=$field['status']?> row" <?=$attributes?>>
 	<?php if (isset($field['validation_error'])): ?><?=$field['validation_error']?><?php endif ?>
 	<?php if (!el('label_after_input',$field,false)): ?><label for="<?=$field['name']?>" class="<?=$field['label_class']?> <?=($field['horizontal_bootstrap'])?'col-lg-2 col-sm-3':'';?>"><?=$field['label']?></label><?php endif ?>
 	<?php if ($field['horizontal_bootstrap']): ?><div class="col-lg-10 col-sm-9 <?=el('label_after_input',$field,false)?'col-lg-offset-2 col-sm-offset-3':''?>"><?php endif ?>
