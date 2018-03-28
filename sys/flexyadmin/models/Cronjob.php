37e6895d91b0e5319dea7245d6d10ceb44a22b6f
Jan den Besten <info@jandenbesten.net>
2018-03-28 21:43:05 +0200

    Cronjob unit-test & rewrite (marges & daylight saving compensation)
    # Conflicts:
    #	sys/build.txt
    
    
    Former-commit-id: 8e7e730203114eaedf9e6d95432bc07b7bea83ae
    Former-commit-id: 38ead4234f53379f1eb795556df6112b10319ade [formerly 32a426e95ae9016bcd916ee5b59479a4eec28820]
    Former-commit-id: 041f883e5e53b96e7de2d1112e00037bbc377624

--- sys/flexyadmin/models/Cronjob.php
+++ sys/flexyadmin/models/Cronjob.php
@@ -7,7 +7,7 @@
  */
 
 class Cronjob extends CI_Model {
-	
+
 	public function __construct()  {
     parent::__construct();
     $this->load->model('log_activity');
@@ -17,18 +17,28 @@ class Cronjob extends CI_Model {
     ini_set('max_execution_time', 600); // 10 minuten mag het script erover doen.
   }
 
+
+  private function get_jobs_info() {
+    $jobs = $this->config->item('cronjobs');
+    foreach ($jobs as $key=>$job) {
+      if (substr($job['every'],0,5)=='model') {
+        $model_job = $this->_every_model($job['every'],$key);
+        $job = array_merge($job,$model_job);
+      }
+      $jobs[$key] = $job;
+    }
+    return $jobs;
+  }
+
+
 	public function go() {
     if ($this->data->table_exists('log_cronjobs')) {
       
-      $this->jobs = $this->config->item('cronjobs');
+      $this->jobs = $this->get_jobs_info();
       
       // Eerst alle informatie verzamelen
       foreach ($this->jobs as $key=>$job) {
-        // Every van models
-        if (substr($job['every'],0,5)=='model') {
-          $job['every'] = $this->_every_model($job['every'],$key);
-        }
-
+        
         // Laatste run
         $last = $this->data->table('log_cronjobs')->where('str_job',$job['name'])->get_field('tme_last_run');
         if ($last)
@@ -40,8 +50,7 @@ class Cronjob extends CI_Model {
         $job['next'] = $this->_calc_next( $job['every'], $job['last'] );
 
         // Moet de job gebeuren?
-        $job['run'] = false;
-        if ($job['next']) $job['run']  = ( time()>=$job['next'] );
+        $job['run'] = $this->job_needs_run($job);
         
         $this->jobs[$key] = $job;
       }
@@ -76,12 +85,7 @@ class Cronjob extends CI_Model {
       
       // echo results
       foreach ($this->jobs as $job) {
-        $job['nu'] = unix_to_normal(time());
-        $job['last'] = unix_to_normal($job['last']);
-        $job['next'] = unix_to_normal($job['next']);
-        if (isset($job['at'])) {
-          $job['at'] = unix_to_normal($job['at']);
-        }
+        $job = $this->human_job($job);
 
         // Log informatie
         trace_($job);
@@ -100,13 +104,41 @@ class Cronjob extends CI_Model {
   }
 
 
+  
+  /**
+   * Logged cronjob
+   */
   public function log_last_run($name,$last) {
     $job_exists = $this->data->table('log_cronjobs')->where('str_job',$name)->get_row();
+    // if ($this->daylight_saving) $last=$last-TIME_HOUR;
     $this->data->set( array( 'str_job'=>$name, 'tme_last_run'=>unix_to_mysql($last)) );
     if ( !$job_exists )
       $this->data->insert();
     else
       $this->data->where('str_job',$name)->update();
+  }
+
+  public function human_job($job) {
+    $job['nu'] = unix_to_normal(time());
+    $job['last_human'] = unix_to_normal($job['last']);
+    $job['next_human'] = unix_to_normal($job['next']);
+    if (isset($job['at'])) {
+      $job['at_human'] = unix_to_normal($job['at']);
+    }
+    return $job;
+  }
+
+
+  /**
+   * Zorgt ervoor dat de startpositie is ingesteld. Als je bv een cronjob (dynamisch) aanzet.
+   */
+  public function log_start_setting() {
+    $cronjobs = $this->get_jobs_info();
+    foreach ($cronjobs as $name => $job) {
+      $last  = $this->cronjob->_calc_next( $job['every'], time() );
+      if (isset($job['interval'])) $last = $last - $job['interval'];
+      $this->cronjob->log_last_run($name,$last);
+    }
   }
   
   
@@ -146,16 +178,18 @@ class Cronjob extends CI_Model {
       $hour = (int) $time[0];
       $min  = (int) $time[1];
 
-      // if ($daylight_saving) $hour -= TIME_HOUR;
-
       switch ($type) {
         
         // per dag: startmoment voor next is laatste run dag om 00:00, next is dan de uren en minuten erbij op deze dag
         case 'day':
           $start = mktime( 0,0,0, date('n',$last), date('j',$last), date('Y',$last) );
           $next  = (int) $start + $hour*TIME_HOUR + $min*TIME_MINUTE;
-          // Next kan nooit kleiner zijn dan laatste keer, tel er dan een dag bij op
-          if ($next<$last) $next+=TIME_DAY;
+          // Next (met marge van 61 minuten) kan nooit kleiner zijn dan laatste keer, tel er dan een dag bij op
+          $marge = TIME_HOUR;
+          if (($next-$marge)<$last) $next+=TIME_DAY;
+          // Compenseer overgang van zomer/winter tijd
+          $daylight_saving_difference = date('I',$last) - date('I',$next);
+          $next += $daylight_saving_difference * TIME_HOUR;
           break;
         
         // per week: - start is zondag van laatste runweek om 0:00, - next is start + de dag vd week en het tijdstip
@@ -163,8 +197,12 @@ class Cronjob extends CI_Model {
           $day_of_week = date('w',$last); // 0..7
           $start = mktime( 0,0,0, date('n',$last), date('j',$last), date('Y',$last) ) - ($day_of_week * TIME_DAY);
           $next  = (int) $start + ($day * TIME_DAY) + $hour*TIME_HOUR + $min*TIME_MINUTE;
-          // Next kan nooit kleiner zijn dan laatste keer, tel er dan een week bij op
-          if ($next<$last) $next+=TIME_WEEK;
+          // Next kan nooit kleiner zijn dan laatste keer, en marge van 6 dagen, tel er dan een week bij op
+          $marge = 6 * TIME_DAY;
+          if (($next-$marge)<$last) $next+=TIME_WEEK;
+          // Compenseer overgang van zomer/winter tijd
+          $daylight_saving_difference = date('I',$last) - date('I',$next);
+          $next += $daylight_saving_difference * TIME_HOUR;
           break;
 
         // per maand: startmoment is 1e dag van de volgende maand om 0:00, next wordt dan start + de dag en tijd van de maand
@@ -173,13 +211,23 @@ class Cronjob extends CI_Model {
           $day_of_month  = date('j',$last); // 1..31
           $start = mktime( 0,0,0, date('n',$last), date('j',$last), date('Y',$last) ) + (-$day_of_month+$days_in_month) * TIME_DAY;
           $next  = (int) $start + $day * TIME_DAY + $hour*TIME_HOUR + $min*TIME_MINUTE;
-          // Next kan nooit kleiner zijn dan laatste keer, tel er dan een maand bij op
-          if ($next<$last) $next+=($days_in_month * TIME_DAY);
+          // Next kan nooit kleiner zijn dan laatste keer, tel er dan een maand bij op, marge een drie weken
+          $marge = 3* TIME_WEEK;
+          if (($next-$marge)<$last) $next+=($days_in_month * TIME_DAY);
+          // Compenseer overgang van zomer/winter tijd
+          $daylight_saving_difference = date('I',$last) - date('I',$next);
+          $next += $daylight_saving_difference * TIME_HOUR;
           break;
       }
       
     }
     return $next;
+  }
+
+  public function job_needs_run($job) {
+    $run = false;
+    if ($job['next']) $run = ( time()>=$job['next'] );
+    return $run;
   }
   
   
