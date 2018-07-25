e042fb178b49126a84f41616e289f5c3d8c40af4
Jan den Besten <info@jandenbesten.net>
2018-07-25 16:25:36 +0200

    TEST AES
    Former-commit-id: df481982ec2245440586eeac5dccc1d61d832710
    Former-commit-id: da510e2ed379a9928b609ce2ff356ed6cd96d43a [formerly c60852e1020bc2d200e856d6fcf7bba1951c8509]
    Former-commit-id: ef8931d41122ae34636799473b84bb9111aadce5

---
+++ site/config/data/tbl_adressen.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['table']           = 'tbl_adressen';
+
+$config['field_info']['str_address']['encrypted'] = TRUE;
+$config['field_info']['str_zipcode']['encrypted'] = TRUE;
+$config['field_info']['str_city']['encrypted'] = TRUE;
+
