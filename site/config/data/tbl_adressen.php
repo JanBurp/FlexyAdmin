b0c991eb9d614fe08abfa888c2667ae4e01179bd
Jan den Besten <info@jandenbesten.net>
2018-08-04 11:40:38 +0200

    AES encryption ready, with tests and plugins

--- site/config/data/tbl_adressen.php
+++ site/config/data/tbl_adressen.php
@@ -1,5 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+// Only for test AES testing with flexyadmin_unittest_...sql database
+
 $config['table']           = 'tbl_adressen';
 
 $config['field_info']['str_address']['encrypted'] = TRUE;
