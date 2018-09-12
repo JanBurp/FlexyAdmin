6bb859d6f7949c7ced3fd619853ca3c910be80f1
Jan den Besten <info@jandenbesten.net>
2018-09-12 13:13:15 +0200

    DB Export can be used as tool rights

--- sys/flexyadmin/libraries/plugins/Plugin_db.php
+++ sys/flexyadmin/libraries/plugins/Plugin_db.php
@@ -14,10 +14,10 @@ class Plugin_db extends Plugin {
 
 	public function __construct() {
 		parent::__construct();
-    $this->CI->load->dbutil();
-    $this->CI->load->driver('cache', array('adapter' => 'file'));
-    $this->CI->load->helper('download');
-    $this->CI->load->library('zip');
+		$this->CI->load->dbutil();
+		$this->CI->load->driver('cache', array('adapter' => 'file'));
+		$this->CI->load->helper('download');
+		$this->CI->load->library('zip');
 	}
 
   /**
@@ -27,96 +27,96 @@ class Plugin_db extends Plugin {
 		if (method_exists($this,$action)) {
 			return $this->$action($args);
 		}
-    return '';
+		return '';
 	}
 
   private function _filename() {
-    $name=$this->CI->data->table('tbl_site')->get_field("url_url");
-    $name=str_replace(array('http://','https://','www.'),'',$name);
-    $name=explode(".",$name);
-    $name=$name[0];
-    return date("Y-m-d").'_'.$name;
+		$name=$this->CI->data->table('tbl_site')->get_field("url_url");
+		$name=str_replace(array('http://','https://','www.'),'',$name);
+		$name=explode(".",$name);
+		$name=$name[0];
+		return date("Y-m-d").'_'.$name;
   }
 
 
 	private function backup($args=NULL) {
-    if (!$this->CI->flexy_auth->can_backup()) return 'No rights';
+		if (!$this->CI->flexy_auth->can_backup()) return 'No rights';
 
-    $tablesWithRights=$this->CI->flexy_auth->get_table_rights(RIGHTS_DELETE);
+		$tablesWithRights=$this->CI->flexy_auth->get_table_rights(RIGHTS_DELETE);
 
-    // select all tbl_, res_ and most of the cfg_ tables
-    $tablesWithRights=array_combine($tablesWithRights,$tablesWithRights);
-    $tablesWithRights=not_filter_by($tablesWithRights,"log");
-    unset($tablesWithRights["cfg_sessions"]);
+		// select all tbl_, res_ and most of the cfg_ tables
+		$tablesWithRights=array_combine($tablesWithRights,$tablesWithRights);
+		$tablesWithRights=not_filter_by($tablesWithRights,"log");
+		unset($tablesWithRights["cfg_sessions"]);
 
-    // create backup
-    $prefs = array('tables'=> $tablesWithRights,'format'=>'sql');
-    $sql = $this->CI->dbutil->backup($prefs);
-    $sql = $this->CI->dbutil->clean_sql($sql);
-    $sql = "# FlexyAdmin backup\n# User: '".$this->CI->flexy_auth->get_user(null,'str_username')."'  \n# Date: ".date("d F Y")."\n\n".$sql;
-    $filename = $this->_filename().'_backup'.'.sql';
+		// create backup
+		$prefs = array('tables'=> $tablesWithRights,'format'=>'sql');
+		$sql = $this->CI->dbutil->backup($prefs);
+		$sql = $this->CI->dbutil->clean_sql($sql);
+		$sql = "# FlexyAdmin backup\n# User: '".$this->CI->flexy_auth->get_user(null,'str_username')."'  \n# Date: ".date("d F Y")."\n\n".$sql;
+		$filename = $this->_filename().'_backup'.'.sql';
 
-    // Ecnrypt
-    $key = Key::loadFromAsciiSafeString( $this->CI->config->item('encryption_key') );
-    $sql = Crypto::encrypt($sql, $key);
-    $filename .= '.txt';
+		// Ecnrypt
+		$key = Key::loadFromAsciiSafeString( $this->CI->config->item('encryption_key') );
+		$sql = Crypto::encrypt($sql, $key);
+		$filename .= '.txt';
 
-    $this->CI->zip->add_data($filename, $sql);
-    $this->CI->zip->download($filename);
+		$this->CI->zip->add_data($filename, $sql);
+		$this->CI->zip->download($filename);
 	}
 
 
   public function export($args=NULL) {
-    if (!$this->CI->flexy_auth->is_super_admin()) return 'No rights';
+		if (!$this->CI->flexy_auth->can_use_tools() or !$this->CI->flexy_auth->can_backup()) return 'No rights';
 
-    $sql = '';
-    $backup_prefs = array('format' => 'sql');
-    $type = array_shift($args);
-    $file = array_shift($args);
-    $hash = array_shift($args);
-    switch ($type) {
-      case 'complete':
-        $sql = $this->CI->dbutil->backup($backup_prefs);
-        break;
-      case 'all':
-        $tables = $this->CI->data->list_tables();
-        $tablesWithData = not_filter_by($tables,array('log','cfg_sessions'));
-        $backup_prefs = array('tables'=> $tablesWithData, 'format'=>'sql');
-        $sql = $this->CI->dbutil->backup($backup_prefs);
-        $tablesWithStructure  = array_diff($tables,$tablesWithData);
-        $backup_prefs = array( 'tables'=> $tablesWithStructure, 'format'=>'sql' ,'add_insert'  => FALSE);
-        $sql .= $this->CI->dbutil->backup($backup_prefs);
-        break;
-      case 'data':
-        $tables = $this->CI->data->list_tables();
-        $tables = not_filter_by($tables,array('log','cfg'));
-        $backup_prefs = array('tables'=> $tables, 'format'=>'sql');
-        $sql = $this->CI->dbutil->backup($backup_prefs);
-        break;
-      case 'select':
-        $tables = $args;
-        $backup_prefs = array('tables'=> $tables, 'format'=>'sql');
-        $sql = $this->CI->dbutil->backup($backup_prefs);
-        break;
-    }
-    $sql = "# FlexyAdmin backup\n# User: '".$this->CI->flexy_auth->get_user(null,'str_username')."'  \n# Date: ".date("d F Y")."\n\n" . $sql;
-    $filename = $this->_filename().'_'.$type.'.sql';
+		$sql = '';
+		$backup_prefs = array('format' => 'sql');
+		$type = array_shift($args);
+		$file = array_shift($args);
+		$hash = array_shift($args);
+		switch ($type) {
+		  case 'complete':
+			$sql = $this->CI->dbutil->backup($backup_prefs);
+			break;
+		  case 'all':
+			$tables = $this->CI->data->list_tables();
+			$tablesWithData = not_filter_by($tables,array('log','cfg_sessions'));
+			$backup_prefs = array('tables'=> $tablesWithData, 'format'=>'sql');
+			$sql = $this->CI->dbutil->backup($backup_prefs);
+			$tablesWithStructure  = array_diff($tables,$tablesWithData);
+			$backup_prefs = array( 'tables'=> $tablesWithStructure, 'format'=>'sql' ,'add_insert'  => FALSE);
+			$sql .= $this->CI->dbutil->backup($backup_prefs);
+			break;
+		  case 'data':
+			$tables = $this->CI->data->list_tables();
+			$tables = not_filter_by($tables,array('log','cfg'));
+			$backup_prefs = array('tables'=> $tables, 'format'=>'sql');
+			$sql = $this->CI->dbutil->backup($backup_prefs);
+			break;
+		  case 'select':
+			$tables = $args;
+			$backup_prefs = array('tables'=> $tables, 'format'=>'sql');
+			$sql = $this->CI->dbutil->backup($backup_prefs);
+			break;
+		}
+		$sql = "# FlexyAdmin backup\n# User: '".$this->CI->flexy_auth->get_user(null,'str_username')."'  \n# Date: ".date("d F Y")."\n\n" . $sql;
+		$filename = $this->_filename().'_'.$type.'.sql';
 
-    if ($hash==='true') {
-      $key = Key::loadFromAsciiSafeString( $this->CI->config->item('encryption_key') );
-      $sql = Crypto::encrypt($sql, $key);
-      $filename .= '.txt';
-    }
+		if ($hash==='true') {
+		  $key = Key::loadFromAsciiSafeString( $this->CI->config->item('encryption_key') );
+		  $sql = Crypto::encrypt($sql, $key);
+		  $filename .= '.txt';
+		}
 
-    switch ($file) {
-      case 'zip':
-        $this->CI->zip->add_data($filename, $sql);
-        $this->CI->zip->download($filename);
-        break;
-      case 'sql':
-        force_download($filename, $sql);
-        break;
-    }
+		switch ($file) {
+		  case 'zip':
+			$this->CI->zip->add_data($filename, $sql);
+			$this->CI->zip->download($filename);
+			break;
+		  case 'sql':
+			force_download($filename, $sql);
+			break;
+		}
   }
 
 
