bb5017408b960a6fe11568c7cfa84621296e19a1
Jan den Besten <info@jandenbesten.net>
2018-11-21 12:30:42 +0100

    Users with no rights can be set as visitor
    # Conflicts:
    #	sys/build.txt

--- sys/flexyadmin/models/api/User.php
+++ sys/flexyadmin/models/api/User.php
@@ -34,24 +34,32 @@ class User extends Api_Model {
     
     $action = $this->args['action'];
     $email  = $this->args['email'];
+    $visitor = $this->data->table('cfg_user_groups')->get_field('id',array('name'=>'visitor'));
 
     // Multiple users
     if (isset($this->args['where']) && is_array($this->args['where'])) {
       foreach ($this->args['where'] as $user_id) {
-        $user  = $this->flexy_auth->get_user($user_id);      
+        $user  = $this->flexy_auth->get_user($user_id);
         if (!$user) {
           $this->result['data']=FALSE;
           $this->_set_error('NO USER FOUND');
         }
         else {
-          $send = $this->flexy_auth->send_new_password( $user );
-          if (!$send) {
-            $this->result['data']=FALSE;
-            $this->_set_error( langp('user_send_error',$user['str_username']));
+          if ($action=='setgroup') {
+            $this->flexy_auth->add_to_group($visitor, $user['id']);
+            $message = lang('users_setas_visitor');
+            $this->_set_message( $message );
           }
           else {
-            $message = lang('users_send_passwords');
-            $this->_set_message( $message );
+            $send = $this->flexy_auth->send_new_password( $user );
+            if (!$send) {
+              $this->result['data']=FALSE;
+              $this->_set_error( langp('user_send_error',$user['str_username']));
+            }
+            else {
+              $message = lang('users_send_passwords');
+              $this->_set_message( $message );
+            }
           }
         }
       }
@@ -64,20 +72,27 @@ class User extends Api_Model {
         $this->_set_error('NO USER FOUND');
       }
       else {
-        if ($action==='invite') {
-          $send = $this->flexy_auth->send_new_account( $user );
-        }
-        else {
-          $send = $this->flexy_auth->send_new_password( $user );
-        }
-        // Error when sending
-        if (!$send) {
-          $this->result['data']=FALSE;
-          $this->_set_error( langp('user_send_error',$user['str_username']));
-        }
-        else {
-          $message = ($action==='invite')?langp('send_invitation',$user['str_username']):langp('user_send_password',$user['str_username']);
+        if ($action=='setgroup') {
+          $this->flexy_auth->add_to_group($visitor, $user['id']);
+          $message = langp('user_setas_visitor',$user['str_username']);
           $this->_set_message( $message );
+        }
+        else {
+          if ($action==='invite') {
+            $send = $this->flexy_auth->send_new_account( $user );
+          }
+          else {
+            $send = $this->flexy_auth->send_new_password( $user );
+          }
+          // Error when sending
+          if (!$send) {
+            $this->result['data']=FALSE;
+            $this->_set_error( langp('user_send_error',$user['str_username']));
+          }
+          else {
+            $message = ($action==='invite')?langp('send_invitation',$user['str_username']):langp('user_send_password',$user['str_username']);
+            $this->_set_message( $message );
+          }
         }
       }
     }
