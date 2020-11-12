1d5baf6d7ab2688806e1dbd0f422a5da7c40a6ed
Jan den Besten <info@jandenbesten.net>
2020-11-12 10:24:06 +0100

    FA coockie security update
    # Conflicts:
    #	sys/flexyadmin/config/config.php

--- sys/flexyadmin/config/config.php
+++ sys/flexyadmin/config/config.php
@@ -375,10 +375,12 @@ $config['sess_regenerate_destroy']= FALSE;
 */
 $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
+$config['cookie_path']    = '';
 $config['cookie_secure']	= FALSE;
 $config['cookie_httponly'] 	= FALSE;
 
+ini_set('session.cookie_samesite', 'None');
+ini_set('session.cookie_secure', TRUE);
 
 /*
 |--------------------------------------------------------------------------
