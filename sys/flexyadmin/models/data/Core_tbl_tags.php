d12a4cacb62cc19d0aa810199047fe5b214bac33
Jan den Besten <info@jandenbesten.net>
2021-12-20 11:31:36 +0100

    FA tag cleanup action

--- sys/flexyadmin/models/data/Core_tbl_tags.php
+++ sys/flexyadmin/models/data/Core_tbl_tags.php
@@ -29,4 +29,54 @@ Class Core_tbl_tags extends Data_Core {
     return $currentTags;
   }
 
+  public function cleanup_tags() {
+    $used_tags = [];
+    $tables = $this->data->list_tables();
+    $tables = filter_by($tables,'tbl');
+    foreach ($tables as $table) {
+      if ($table!=='tbl_tags') {
+        $fields = $this->data->table($table)->list_fields();
+        if ( in_array('str_tags',$fields) ) {
+          $result = $this->data->table($table)->select('str_tags')->get_result();
+          foreach ($result as $row) {
+            $row_tags = $row['str_tags'];
+            if (!empty($row_tags)) {
+              $row_tags = explode('|',$row_tags);
+              $used_tags = array_merge($used_tags,$row_tags);
+            }
+          }
+        }
+      }
+    }
+
+    $used_tags = array_unique($used_tags);
+
+    $this->db->truncate('tbl_tags');
+    foreach ($used_tags as $tag) {
+      $this->data->table('tbl_tags')->set('str_tag',$tag)->insert();
+    }
+
+    return $used_tags;
+  }
+
+  /**
+   * Voeg cleanup actie toe aan header
+   *
+   * @return array
+   * @author Jan den Besten
+   */
+  public function get_setting_grid_set() {
+    $grid_set = parent::get_setting_grid_set();
+    $grid_set['actions'] = array(
+      array(
+        'name'  => 'Cleanup unused tags',
+        'icon'  => 'remove',
+        'url'   => 'tags_cleanup',
+        'class' => 'text-warning',
+      ),
+    );
+
+    return $grid_set;
+  }
+
 }
